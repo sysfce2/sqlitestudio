8f9727ca828127c9ca8d7267b850511c0e24eaf7
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 13:21:42 +0200

    New translations dbsqlitemc.ts (Hungarian)
    [ci skip]

--- Plugins/DbSQLite3MC/translations/DbSqliteMc_hu_HU.ts
+++ Plugins/DbSQLite3MC/translations/DbSqliteMc_hu_HU.ts
@@ -1,46 +1,46 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="hu" sourcelanguage="en">
-<context>
+  <context>
     <name>DbSqliteMc</name>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="42"/>
-        <source>Password (key)</source>
-        <translation>Jelszó (kulcs)</translation>
+      <location filename="../dbsqlitemc.cpp" line="42"/>
+      <source>Password (key)</source>
+      <translation>Jelszó (kulcs)</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="43"/>
-        <source>Leave empty to create or connect to decrypted database.</source>
-        <translation>Titkosítás nélküli adatbázis létrehozásához vagy az ahhoz való csatlakozáshoz hagyja üresen.</translation>
+      <location filename="../dbsqlitemc.cpp" line="43"/>
+      <source>Leave empty to create or connect to decrypted database.</source>
+      <translation>Titkosítás nélküli adatbázis létrehozásához vagy az ahhoz való csatlakozáshoz hagyja üresen.</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="44"/>
-        <source>Encryption password</source>
-        <translation>Titkosítási jelszó</translation>
+      <location filename="../dbsqlitemc.cpp" line="44"/>
+      <source>Encryption password</source>
+      <translation>Titkosítási jelszó</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="50"/>
-        <source>Cipher</source>
-        <translation>Titkosító</translation>
+      <location filename="../dbsqlitemc.cpp" line="50"/>
+      <source>Cipher</source>
+      <translation>Titkosító</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="51"/>
-        <source>Cipher determines encryption algorithm used to encrypt the database.</source>
-        <translation>A titkosító határozza meg az adatbázis titkosításához használt titkosítási algoritmust.</translation>
+      <location filename="../dbsqlitemc.cpp" line="51"/>
+      <source>Cipher determines encryption algorithm used to encrypt the database.</source>
+      <translation>A titkosító határozza meg az adatbázis titkosításához használt titkosítási algoritmust.</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="61"/>
-        <source>Cipher configuration (optional)</source>
-        <translation>Titkosító konfigurációja (opcionális)</translation>
+      <location filename="../dbsqlitemc.cpp" line="61"/>
+      <source>Cipher configuration (optional)</source>
+      <translation>Titkosító konfigurációja (opcionális)</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="62"/>
-        <source>PRAGMA statements to customize SQLite3 Multiple Ciphers configuration, such as KDF iterations, legacy mode, etc.
+      <location filename="../dbsqlitemc.cpp" line="62"/>
+      <source>PRAGMA statements to customize SQLite3 Multiple Ciphers configuration, such as KDF iterations, legacy mode, etc.
 They will be executed upon each opening of the database.
 See documentation for SQLite3 Multiple Ciphers for details.</source>
-        <translation>PRAGMA utasítások az SQLite3 Multiple Ciphers konfigurációjának testreszabásához, például KDF-iterációkhoz, örökölt módhoz stb.
+      <translation>PRAGMA utasítások az SQLite3 Multiple Ciphers konfigurációjának testreszabásához, például KDF-iterációkhoz, örökölt módhoz stb.
 Ezek az adatbázis minden megnyitásakor végrehajtásra kerülnek.
 A részletekért tekintse meg az SQLite3 Multiple Ciphers dokumentációját.</translation>
     </message>
-</context>
+  </context>
 </TS>
