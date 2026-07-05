d84fbb54eb45e5dfb44cd61edab07cf7eb25b1ae
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 13:14:58 +0200

    New translations multieditorimage.ts (Slovak)
    [ci skip]

--- Plugins/MultiEditorImage/translations/MultiEditorImage_sk_SK.ts
+++ Plugins/MultiEditorImage/translations/MultiEditorImage_sk_SK.ts
@@ -1,30 +1,30 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="sk" sourcelanguage="en">
-<context>
+  <context>
     <name>MultiEditorImage</name>
     <message>
-        <location filename="../multieditorimage.cpp" line="26"/>
-        <source>Zoom in by 25%</source>
-        <translation>Priblížiť o 25 %</translation>
+      <location filename="../multieditorimage.cpp" line="26"/>
+      <source>Zoom in by 25%</source>
+      <translation>Priblížiť o 25 %</translation>
     </message>
     <message>
-        <location filename="../multieditorimage.cpp" line="27"/>
-        <source>Zoom out by 25%</source>
-        <translation>Oddialiť o 25 %</translation>
+      <location filename="../multieditorimage.cpp" line="27"/>
+      <source>Zoom out by 25%</source>
+      <translation>Oddialiť o 25 %</translation>
     </message>
     <message>
-        <location filename="../multieditorimage.cpp" line="28"/>
-        <source>Reset zoom</source>
-        <translation>Obnoviť priblíženie</translation>
+      <location filename="../multieditorimage.cpp" line="28"/>
+      <source>Reset zoom</source>
+      <translation>Obnoviť priblíženie</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>MultiEditorImagePlugin</name>
     <message>
-        <location filename="../multieditorimage.cpp" line="200"/>
-        <source>Image</source>
-        <translation>Obrázok</translation>
+      <location filename="../multieditorimage.cpp" line="200"/>
+      <source>Image</source>
+      <translation>Obrázok</translation>
     </message>
-</context>
+  </context>
 </TS>
