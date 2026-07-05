e76fd435de619f25878d1e3d7735885bdda3f98a
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 20:48:12 +0200

    New translations printing.ts (Catalan)
    [ci skip]

--- Plugins/Printing/translations/Printing_ca_ES.ts
+++ Plugins/Printing/translations/Printing_ca_ES.ts
@@ -1,40 +1,40 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="ca" sourcelanguage="en">
-<context>
+  <context>
     <name>Printing</name>
     <message>
-        <location filename="../printing.cpp" line="36"/>
-        <source>Print data</source>
-        <translation>Imprimeix les dades</translation>
+      <location filename="../printing.cpp" line="36"/>
+      <source>Print data</source>
+      <translation>Imprimeix les dades</translation>
     </message>
     <message>
-        <location filename="../printing.cpp" line="38"/>
-        <source>Print query</source>
-        <translation>Imprimeix la consulta</translation>
+      <location filename="../printing.cpp" line="38"/>
+      <source>Print query</source>
+      <translation>Imprimeix la consulta</translation>
     </message>
     <message>
-        <location filename="../printing.cpp" line="75"/>
-        <source>No data to print.</source>
-        <translation>No hi ha dades per imprimir.</translation>
+      <location filename="../printing.cpp" line="75"/>
+      <source>No data to print.</source>
+      <translation>No hi ha dades per imprimir.</translation>
     </message>
     <message>
-        <location filename="../printing.cpp" line="83"/>
-        <source>Printing data.</source>
-        <translation>S&apos;estan imprimint les dades.</translation>
+      <location filename="../printing.cpp" line="83"/>
+      <source>Printing data.</source>
+      <translation>S&apos;estan imprimint les dades.</translation>
     </message>
     <message>
-        <location filename="../printing.cpp" line="109"/>
-        <source>Printing query.</source>
-        <translation>S&apos;està imprimint la consulta.</translation>
+      <location filename="../printing.cpp" line="109"/>
+      <source>Printing query.</source>
+      <translation>S&apos;està imprimint la consulta.</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>PrintingExport</name>
     <message>
-        <location filename="../printingexport.cpp" line="27"/>
-        <source>Printing</source>
-        <translation>Impressió</translation>
+      <location filename="../printingexport.cpp" line="27"/>
+      <source>Printing</source>
+      <translation>Impressió</translation>
     </message>
-</context>
+  </context>
 </TS>
