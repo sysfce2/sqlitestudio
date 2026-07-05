baa1a97d189303e6919df2983d66c668b8783279
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 22:52:55 +0200

    New translations csvexport.ts (Arabic)
    [ci skip]

--- Plugins/CsvExport/translations/CsvExport_ar_SA.ts
+++ Plugins/CsvExport/translations/CsvExport_ar_SA.ts
@@ -1,57 +1,57 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="ar" sourcelanguage="en">
-<context>
+  <context>
     <name>CsvExport</name>
     <message>
-        <location filename="../CsvExport.ui" line="20"/>
-        <source>Column names in first row</source>
-        <translation>أسماء الأعمدة في الصف الأول</translation>
+      <location filename="../CsvExport.ui" line="20"/>
+      <source>Column names in first row</source>
+      <translation>أسماء الأعمدة في الصف الأول</translation>
     </message>
     <message>
-        <location filename="../CsvExport.ui" line="30"/>
-        <source>Column separator:</source>
-        <translation>فاصل الأعمدة:</translation>
+      <location filename="../CsvExport.ui" line="30"/>
+      <source>Column separator:</source>
+      <translation>فاصل الأعمدة:</translation>
     </message>
     <message>
-        <location filename="../CsvExport.ui" line="41"/>
-        <source>, (comma)</source>
-        <translation>, (فاصلة)</translation>
+      <location filename="../CsvExport.ui" line="41"/>
+      <source>, (comma)</source>
+      <translation>, (فاصلة)</translation>
     </message>
     <message>
-        <location filename="../CsvExport.ui" line="46"/>
-        <source>; (semicolon)</source>
-        <translation>; (فاصلة منقوطة)</translation>
+      <location filename="../CsvExport.ui" line="46"/>
+      <source>; (semicolon)</source>
+      <translation>; (فاصلة منقوطة)</translation>
     </message>
     <message>
-        <location filename="../CsvExport.ui" line="51"/>
-        <source>\t (tab)</source>
-        <translation>\t (علامة جدولة)</translation>
+      <location filename="../CsvExport.ui" line="51"/>
+      <source>\t (tab)</source>
+      <translation>\t (علامة جدولة)</translation>
     </message>
     <message>
-        <location filename="../CsvExport.ui" line="56"/>
-        <source>  (whitespace)</source>
-        <translation>  (مسافة بيضاء)</translation>
+      <location filename="../CsvExport.ui" line="56"/>
+      <source>  (whitespace)</source>
+      <translation>  (مسافة بيضاء)</translation>
     </message>
     <message>
-        <location filename="../CsvExport.ui" line="61"/>
-        <source>Custom:</source>
-        <translation>مخصص:</translation>
+      <location filename="../CsvExport.ui" line="61"/>
+      <source>Custom:</source>
+      <translation>مخصص:</translation>
     </message>
     <message>
-        <location filename="../CsvExport.ui" line="82"/>
-        <source>Export NULL values as:</source>
-        <translation>تصدير قيم NULL كـ:</translation>
+      <location filename="../CsvExport.ui" line="82"/>
+      <source>Export NULL values as:</source>
+      <translation>تصدير قيم NULL كـ:</translation>
     </message>
     <message>
-        <location filename="../CsvExport.ui" line="89"/>
-        <source>Empty string</source>
-        <translation>سلسلة فارغة</translation>
+      <location filename="../CsvExport.ui" line="89"/>
+      <source>Empty string</source>
+      <translation>سلسلة فارغة</translation>
     </message>
     <message>
-        <location filename="../csvexport.cpp" line="41"/>
-        <source>Enter the custom separator character.</source>
-        <translation>أدخل حرف الفاصل المخصص.</translation>
+      <location filename="../csvexport.cpp" line="41"/>
+      <source>Enter the custom separator character.</source>
+      <translation>أدخل حرف الفاصل المخصص.</translation>
     </message>
-</context>
+  </context>
 </TS>
