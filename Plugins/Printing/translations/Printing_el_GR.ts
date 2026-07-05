7c27d5ea4b53428f2018a93454e435e842e285ad
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 20:48:47 +0200

    New translations printing.ts (Greek)
    [ci skip]

--- Plugins/Printing/translations/Printing_el_GR.ts
+++ Plugins/Printing/translations/Printing_el_GR.ts
@@ -1,40 +1,40 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="el" sourcelanguage="en">
-<context>
+  <context>
     <name>Printing</name>
     <message>
-        <location filename="../printing.cpp" line="36"/>
-        <source>Print data</source>
-        <translation>Εκτύπωση δεδομένων</translation>
+      <location filename="../printing.cpp" line="36"/>
+      <source>Print data</source>
+      <translation>Εκτύπωση δεδομένων</translation>
     </message>
     <message>
-        <location filename="../printing.cpp" line="38"/>
-        <source>Print query</source>
-        <translation>Εκτύπωση ερωτήματος</translation>
+      <location filename="../printing.cpp" line="38"/>
+      <source>Print query</source>
+      <translation>Εκτύπωση ερωτήματος</translation>
     </message>
     <message>
-        <location filename="../printing.cpp" line="75"/>
-        <source>No data to print.</source>
-        <translation>Δεν υπάρχουν δεδομένα για εκτύπωση.</translation>
+      <location filename="../printing.cpp" line="75"/>
+      <source>No data to print.</source>
+      <translation>Δεν υπάρχουν δεδομένα για εκτύπωση.</translation>
     </message>
     <message>
-        <location filename="../printing.cpp" line="83"/>
-        <source>Printing data.</source>
-        <translation>Γίνεται εκτύπωση δεδομένων.</translation>
+      <location filename="../printing.cpp" line="83"/>
+      <source>Printing data.</source>
+      <translation>Γίνεται εκτύπωση δεδομένων.</translation>
     </message>
     <message>
-        <location filename="../printing.cpp" line="109"/>
-        <source>Printing query.</source>
-        <translation>Γίνεται εκτύπωση ερωτήματος.</translation>
+      <location filename="../printing.cpp" line="109"/>
+      <source>Printing query.</source>
+      <translation>Γίνεται εκτύπωση ερωτήματος.</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>PrintingExport</name>
     <message>
-        <location filename="../printingexport.cpp" line="27"/>
-        <source>Printing</source>
-        <translation>Εκτύπωση</translation>
+      <location filename="../printingexport.cpp" line="27"/>
+      <source>Printing</source>
+      <translation>Εκτύπωση</translation>
     </message>
-</context>
+  </context>
 </TS>
