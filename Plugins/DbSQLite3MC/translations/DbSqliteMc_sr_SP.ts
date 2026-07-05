15324349e781fb9b9f88d6927c71f9e58a8ca8e9
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 13:21:53 +0200

    New translations dbsqlitemc.ts (Serbian (Cyrillic))
    [ci skip]

--- Plugins/DbSQLite3MC/translations/DbSqliteMc_sr_SP.ts
+++ Plugins/DbSQLite3MC/translations/DbSqliteMc_sr_SP.ts
@@ -1,46 +1,46 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="sr" sourcelanguage="en">
-<context>
+  <context>
     <name>DbSqliteMc</name>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="42"/>
-        <source>Password (key)</source>
-        <translation>Лозинка (кључ)</translation>
+      <location filename="../dbsqlitemc.cpp" line="42"/>
+      <source>Password (key)</source>
+      <translation>Лозинка (кључ)</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="43"/>
-        <source>Leave empty to create or connect to decrypted database.</source>
-        <translation>Оставите празно да бисте направили или се повезали са дешифрованом базом података.</translation>
+      <location filename="../dbsqlitemc.cpp" line="43"/>
+      <source>Leave empty to create or connect to decrypted database.</source>
+      <translation>Оставите празно да бисте направили или се повезали са дешифрованом базом података.</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="44"/>
-        <source>Encryption password</source>
-        <translation>Лозинка за шифровање</translation>
+      <location filename="../dbsqlitemc.cpp" line="44"/>
+      <source>Encryption password</source>
+      <translation>Лозинка за шифровање</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="50"/>
-        <source>Cipher</source>
-        <translation>Шифра</translation>
+      <location filename="../dbsqlitemc.cpp" line="50"/>
+      <source>Cipher</source>
+      <translation>Шифра</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="51"/>
-        <source>Cipher determines encryption algorithm used to encrypt the database.</source>
-        <translation>Шифра одређује алгоритам шифровања који се користи за шифровање базе података.</translation>
+      <location filename="../dbsqlitemc.cpp" line="51"/>
+      <source>Cipher determines encryption algorithm used to encrypt the database.</source>
+      <translation>Шифра одређује алгоритам шифровања који се користи за шифровање базе података.</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="61"/>
-        <source>Cipher configuration (optional)</source>
-        <translation>Конфигурација шифре (опционо)</translation>
+      <location filename="../dbsqlitemc.cpp" line="61"/>
+      <source>Cipher configuration (optional)</source>
+      <translation>Конфигурација шифре (опционо)</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="62"/>
-        <source>PRAGMA statements to customize SQLite3 Multiple Ciphers configuration, such as KDF iterations, legacy mode, etc.
+      <location filename="../dbsqlitemc.cpp" line="62"/>
+      <source>PRAGMA statements to customize SQLite3 Multiple Ciphers configuration, such as KDF iterations, legacy mode, etc.
 They will be executed upon each opening of the database.
 See documentation for SQLite3 Multiple Ciphers for details.</source>
-        <translation>PRAGMA наредбе за прилагођавање конфигурације SQLite3 Multiple Ciphers, као што су KDF итерације, застарели режим итд.
+      <translation>PRAGMA наредбе за прилагођавање конфигурације SQLite3 Multiple Ciphers, као што су KDF итерације, застарели режим итд.
 Оне ће бити извршене при сваком отварању базе података.
 За детаље погледајте документацију за SQLite3 Multiple Ciphers.</translation>
     </message>
-</context>
+  </context>
 </TS>
