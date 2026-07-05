4bb3d660316d24a274a8f692641a91911422f97f
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 20:51:37 +0200

    New translations sqlformattersimple.ts (Greek)
    [ci skip]

--- Plugins/SqlFormatterSimple/translations/SqlFormatterSimple_el_GR.ts
+++ Plugins/SqlFormatterSimple/translations/SqlFormatterSimple_el_GR.ts
@@ -1,17 +1,17 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="el" sourcelanguage="en">
-<context>
+  <context>
     <name>SqlFormatterSimplePlugin</name>
     <message>
-        <location filename="../SqlFormatterSimple.ui" line="20"/>
-        <source>Upper case keywords</source>
-        <translation>Λέξεις-κλειδιά με κεφαλαία</translation>
+      <location filename="../SqlFormatterSimple.ui" line="20"/>
+      <source>Upper case keywords</source>
+      <translation>Λέξεις-κλειδιά με κεφαλαία</translation>
     </message>
     <message>
-        <location filename="../SqlFormatterSimple.ui" line="30"/>
-        <source>Reduce multiple whitespaces to single whitespace</source>
-        <translation>Μείωση πολλαπλών κενών διαστημάτων σε ένα μόνο κενό διάστημα</translation>
+      <location filename="../SqlFormatterSimple.ui" line="30"/>
+      <source>Reduce multiple whitespaces to single whitespace</source>
+      <translation>Μείωση πολλαπλών κενών διαστημάτων σε ένα μόνο κενό διάστημα</translation>
     </message>
-</context>
+  </context>
 </TS>
