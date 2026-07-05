80286daf51248cdb04448aed7f134f02e19dd735
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 13:24:14 +0200

    New translations letos.ts (Hungarian)
    [ci skip]

--- Letos/letos/translations/letos_hu_HU.ts
+++ Letos/letos/translations/letos_hu_HU.ts
@@ -1,97 +1,97 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="hu" sourcelanguage="en">
-<context>
+  <context>
     <name>QObject</name>
     <message>
-        <location filename="../main.cpp" line="52"/>
-        <source>GUI interface to Letos, a SQLite manager.</source>
-        <translation>Grafikus felület a Letoshoz, egy SQLite-kezelőhöz.</translation>
+      <location filename="../main.cpp" line="52"/>
+      <source>GUI interface to Letos, a SQLite manager.</source>
+      <translation>Grafikus felület a Letoshoz, egy SQLite-kezelőhöz.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="57"/>
-        <source>Starts the application in safe mode without restoring the previous session. Use this to bypass issues caused by a corrupted session.</source>
-        <translation>Csökkentett módban indítja az alkalmazást az előző munkamenet visszaállítása nélkül. Ezzel megkerülhetők a sérült munkamenet által okozott problémák.</translation>
+      <location filename="../main.cpp" line="57"/>
+      <source>Starts the application in safe mode without restoring the previous session. Use this to bypass issues caused by a corrupted session.</source>
+      <translation>Csökkentett módban indítja az alkalmazást az előző munkamenet visszaállítása nélkül. Ezzel megkerülhetők a sérült munkamenet által okozott problémák.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="58"/>
-        <source>Do not load any plugins. Can be used alongside safe mode to further isolate potential issues.</source>
-        <translation>Ne töltsön be bővítményeket. A csökkentett móddal együtt is használható a lehetséges problémák további elkülönítésére.</translation>
+      <location filename="../main.cpp" line="58"/>
+      <source>Do not load any plugins. Can be used alongside safe mode to further isolate potential issues.</source>
+      <translation>Ne töltsön be bővítményeket. A csökkentett móddal együtt is használható a lehetséges problémák további elkülönítésére.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="59"/>
-        <source>Enables debug messages in console (accessible with F12).</source>
-        <translation>Engedélyezi a hibakeresési üzeneteket a konzolban (az F12 billentyűvel érhető el).</translation>
+      <location filename="../main.cpp" line="59"/>
+      <source>Enables debug messages in console (accessible with F12).</source>
+      <translation>Engedélyezi a hibakeresési üzeneteket a konzolban (az F12 billentyűvel érhető el).</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="60"/>
-        <source>Redirects debug messages into standard output (forces debug mode).</source>
-        <translation>Átirányítja a hibakeresési üzeneteket a szabványos kimenetre (kényszeríti a hibakeresési módot).</translation>
+      <location filename="../main.cpp" line="60"/>
+      <source>Redirects debug messages into standard output (forces debug mode).</source>
+      <translation>Átirányítja a hibakeresési üzeneteket a szabványos kimenetre (kényszeríti a hibakeresési módot).</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="61"/>
-        <source>Redirects debug messages into given file (forces debug mode).</source>
-        <translation>Átirányítja a hibakeresési üzeneteket a megadott fájlba (kényszeríti a hibakeresési módot).</translation>
+      <location filename="../main.cpp" line="61"/>
+      <source>Redirects debug messages into given file (forces debug mode).</source>
+      <translation>Átirányítja a hibakeresési üzeneteket a megadott fájlba (kényszeríti a hibakeresési módot).</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="61"/>
-        <source>log file</source>
-        <translation>naplófájl</translation>
+      <location filename="../main.cpp" line="61"/>
+      <source>log file</source>
+      <translation>naplófájl</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="62"/>
-        <source>Enables Lemon parser debug messages for SQL code assistant.</source>
-        <translation>Engedélyezi a Lemon elemző hibakeresési üzeneteit az SQL-kódasszisztenshez.</translation>
+      <location filename="../main.cpp" line="62"/>
+      <source>Enables Lemon parser debug messages for SQL code assistant.</source>
+      <translation>Engedélyezi a Lemon elemző hibakeresési üzeneteit az SQL-kódasszisztenshez.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="63"/>
-        <source>Enables debugging of every single SQL query being sent to any database.</source>
-        <translation>Engedélyezi a bármely adatbázisnak elküldött minden egyes SQL-lekérdezés hibakeresését.</translation>
+      <location filename="../main.cpp" line="63"/>
+      <source>Enables debugging of every single SQL query being sent to any database.</source>
+      <translation>Engedélyezi a bármely adatbázisnak elküldött minden egyes SQL-lekérdezés hibakeresését.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="64"/>
-        <source>Limits SQL query messages to only the given &lt;database&gt;.</source>
-        <translation>Az SQL-lekérdezési üzeneteket csak a megadott &lt;adatbázis&gt; értékre korlátozza.</translation>
+      <location filename="../main.cpp" line="64"/>
+      <source>Limits SQL query messages to only the given &lt;database&gt;.</source>
+      <translation>Az SQL-lekérdezési üzeneteket csak a megadott &lt;adatbázis&gt; értékre korlátozza.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="64"/>
-        <source>database</source>
-        <translation>adatbázis</translation>
+      <location filename="../main.cpp" line="64"/>
+      <source>database</source>
+      <translation>adatbázis</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="56"/>
-        <source>Points to the master configuration file. Read manual at wiki page for more details.</source>
-        <translation>A fő konfigurációs fájlra mutat. További részletekért olvassa el a kézikönyvet a wikioldalon.</translation>
+      <location filename="../main.cpp" line="56"/>
+      <source>Points to the master configuration file. Read manual at wiki page for more details.</source>
+      <translation>A fő konfigurációs fájlra mutat. További részletekért olvassa el a kézikönyvet a wikioldalon.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="56"/>
-        <source>settings file</source>
-        <translation>beállításfájl</translation>
+      <location filename="../main.cpp" line="56"/>
+      <source>settings file</source>
+      <translation>beállításfájl</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="65"/>
-        <source>Enables debugging of Letos&apos;s query executor.</source>
-        <translation>Engedélyezi a Letos&apos;s lekérdezés-végrehajtójának hibakeresését.</translation>
+      <location filename="../main.cpp" line="65"/>
+      <source>Enables debugging of Letos&apos;s query executor.</source>
+      <translation>Engedélyezi a Letos&apos;s lekérdezés-végrehajtójának hibakeresését.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="66"/>
-        <source>Lists plugins installed in the Letos and quits.</source>
-        <translation>Felsorolja a Letosban telepített bővítményeket, majd kilép.</translation>
+      <location filename="../main.cpp" line="66"/>
+      <source>Lists plugins installed in the Letos and quits.</source>
+      <translation>Felsorolja a Letosban telepített bővítményeket, majd kilép.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="79"/>
-        <source>file</source>
-        <translation>fájl</translation>
+      <location filename="../main.cpp" line="79"/>
+      <source>file</source>
+      <translation>fájl</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="79"/>
-        <source>Database file to open</source>
-        <translation>Megnyitandó adatbázisfájl</translation>
+      <location filename="../main.cpp" line="79"/>
+      <source>Database file to open</source>
+      <translation>Megnyitandó adatbázisfájl</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="152"/>
-        <source>Select configuration directory</source>
-        <translation>Konfigurációs könyvtár kiválasztása</translation>
+      <location filename="../main.cpp" line="163"/>
+      <source>Select configuration directory</source>
+      <translation>Konfigurációs könyvtár kiválasztása</translation>
     </message>
-</context>
+  </context>
 </TS>
