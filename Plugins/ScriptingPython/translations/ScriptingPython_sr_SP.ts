cfcd7af9c2f1076c0f8617235176ff78c52de9d5
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 13:20:37 +0200

    New translations scriptingpython.ts (Serbian (Cyrillic))
    [ci skip]

--- Plugins/ScriptingPython/translations/ScriptingPython_sr_SP.ts
+++ Plugins/ScriptingPython/translations/ScriptingPython_sr_SP.ts
@@ -1,50 +1,50 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="sr" sourcelanguage="en">
-<context>
+  <context>
     <name>QObject</name>
     <message>
-        <location filename="../scriptingpython.cpp" line="730"/>
-        <source>Invalid use of %1 function. Expected %2 arguments, but got %3.</source>
-        <translation>Неисправна употреба функције %1. Очекивано је %2 аргумената, али је добијено %3.</translation>
+      <location filename="../scriptingpython.cpp" line="730"/>
+      <source>Invalid use of %1 function. Expected %2 arguments, but got %3.</source>
+      <translation>Неисправна употреба функције %1. Очекивано је %2 аргумената, али је добијено %3.</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="739"/>
-        <source>Unknown error from function %1.</source>
-        <translation>Непозната грешка из функције %1.</translation>
+      <location filename="../scriptingpython.cpp" line="739"/>
+      <source>Unknown error from function %1.</source>
+      <translation>Непозната грешка из функције %1.</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="780"/>
-        <source>Could not calculate string representation of the Python object passed as argument to the function %1.</source>
-        <translation>Није могуће израчунати ниску представу Python објекта прослеђеног као аргумент функцији %1.</translation>
+      <location filename="../scriptingpython.cpp" line="780"/>
+      <source>Could not calculate string representation of the Python object passed as argument to the function %1.</source>
+      <translation>Није могуће израчунати ниску представу Python објекта прослеђеног као аргумент функцији %1.</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="802"/>
-        <source>Could not find execution context for function %1. This is a bug of Python plugin. Please report it.</source>
-        <translation>Није могуће пронаћи контекст извршавања за функцију %1. Ово је грешка у Python прикључку. Пријавите је.</translation>
+      <location filename="../scriptingpython.cpp" line="802"/>
+      <source>Could not find execution context for function %1. This is a bug of Python plugin. Please report it.</source>
+      <translation>Није могуће пронаћи контекст извршавања за функцију %1. Ово је грешка у Python прикључку. Пријавите је.</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="827"/>
-        <source>Error from Python function %1: %2</source>
-        <translation>Грешка из Python функције %1: %2</translation>
+      <location filename="../scriptingpython.cpp" line="827"/>
+      <source>Error from Python function %1: %2</source>
+      <translation>Грешка из Python функције %1: %2</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>ScriptingPython</name>
     <message>
-        <location filename="../scriptingpython.ui" line="20"/>
-        <source>Python library path</source>
-        <translation>Путања Python библиотеке</translation>
+      <location filename="../scriptingpython.ui" line="20"/>
+      <source>Python library path</source>
+      <translation>Путања Python библиотеке</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.ui" line="29"/>
-        <source>Choose a Python library file</source>
-        <translation>Изаберите датотеку Python библиотеке</translation>
+      <location filename="../scriptingpython.ui" line="29"/>
+      <source>Choose a Python library file</source>
+      <translation>Изаберите датотеку Python библиотеке</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="370"/>
-        <source>The plugin is not configured properly.</source>
-        <translation>Прикључак није правилно конфигурисан.</translation>
+      <location filename="../scriptingpython.cpp" line="370"/>
+      <source>The plugin is not configured properly.</source>
+      <translation>Прикључак није правилно конфигурисан.</translation>
     </message>
-</context>
+  </context>
 </TS>
