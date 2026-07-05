ed4b6bdbb87d001e1a8216640724a12f8e3b2754
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 22:55:58 +0200

    New translations sqlenterpriseformatter.ts (Finnish)
    [ci skip]

--- Plugins/SqlEnterpriseFormatter/translations/SqlEnterpriseFormatter_fi_FI.ts
+++ Plugins/SqlEnterpriseFormatter/translations/SqlEnterpriseFormatter_fi_FI.ts
@@ -1,228 +1,228 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="fi" sourcelanguage="en">
-<context>
+  <context>
     <name>QObject</name>
     <message>
-        <location filename="../sqlenterpriseformatter.cpp" line="89"/>
-        <location filename="../sqlenterpriseformatter.cpp" line="96"/>
-        <source>name</source>
-        <comment>example name wrapper</comment>
-        <translation>nimi</translation>
+      <location filename="../sqlenterpriseformatter.cpp" line="89"/>
+      <location filename="../sqlenterpriseformatter.cpp" line="96"/>
+      <source>name</source>
+      <comment>example name wrapper</comment>
+      <translation>nimi</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>SqlEnterpriseFormatter</name>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="46"/>
-        <source>Indentation</source>
-        <translation>Sisennys</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="46"/>
+      <source>Indentation</source>
+      <translation>Sisennys</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="58"/>
-        <source>Line up keywords in multi-line queries</source>
-        <translation>Tasaa avainsanat monirivisissä kyselyissä</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="58"/>
+      <source>Line up keywords in multi-line queries</source>
+      <translation>Tasaa avainsanat monirivisissä kyselyissä</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="68"/>
-        <source>Indent contents of parenthesis block</source>
-        <translation>Sisennä sulkulohkon sisältö</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="68"/>
+      <source>Indent contents of parenthesis block</source>
+      <translation>Sisennä sulkulohkon sisältö</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="91"/>
-        <source>Tab size:</source>
-        <translation>Sarkaimen koko:</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="91"/>
+      <source>Tab size:</source>
+      <translation>Sarkaimen koko:</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="106"/>
-        <source>New lines</source>
-        <translation>Rivinvaihdot</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="106"/>
+      <source>New lines</source>
+      <translation>Rivinvaihdot</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="160"/>
-        <source>Before opening parenthesis in column definitions</source>
-        <translation>Ennen avaavaa suljetta sarakemäärittelyissä</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="160"/>
+      <source>Before opening parenthesis in column definitions</source>
+      <translation>Ennen avaavaa suljetta sarakemäärittelyissä</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="170"/>
-        <source>After opening parenthesis in column definitions</source>
-        <translation>Avaavan sulkeen jälkeen sarakemäärittelyissä</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="170"/>
+      <source>After opening parenthesis in column definitions</source>
+      <translation>Avaavan sulkeen jälkeen sarakemäärittelyissä</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="180"/>
-        <source>Before closing parenthesis in column definitions</source>
-        <translation>Ennen sulkevaa suljetta sarakemäärittelyissä</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="180"/>
+      <source>Before closing parenthesis in column definitions</source>
+      <translation>Ennen sulkevaa suljetta sarakemäärittelyissä</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="190"/>
-        <source>After closing parenthesis in column definitions</source>
-        <translation>Sulkevan sulkeen jälkeen sarakemäärittelyissä</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="190"/>
+      <source>After closing parenthesis in column definitions</source>
+      <translation>Sulkevan sulkeen jälkeen sarakemäärittelyissä</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="200"/>
-        <source>Before opening parenthesis in expressions</source>
-        <translation>Ennen avaavaa suljetta lausekkeissa</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="200"/>
+      <source>Before opening parenthesis in expressions</source>
+      <translation>Ennen avaavaa suljetta lausekkeissa</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="210"/>
-        <source>After opening parenthesis in expressions</source>
-        <translation>Avaavan sulkeen jälkeen lausekkeissa</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="210"/>
+      <source>After opening parenthesis in expressions</source>
+      <translation>Avaavan sulkeen jälkeen lausekkeissa</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="220"/>
-        <source>Before closing parenthesis in expressions</source>
-        <translation>Ennen sulkevaa suljetta lausekkeissa</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="220"/>
+      <source>Before closing parenthesis in expressions</source>
+      <translation>Ennen sulkevaa suljetta lausekkeissa</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="230"/>
-        <source>After closing parenthesis in expressions</source>
-        <translation>Sulkevan sulkeen jälkeen lausekkeissa</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="230"/>
+      <source>After closing parenthesis in expressions</source>
+      <translation>Sulkevan sulkeen jälkeen lausekkeissa</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="240"/>
-        <source>After JOIN keywords in FROM clause</source>
-        <translation>JOIN-avainsanojen jälkeen FROM-lausekkeessa</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="240"/>
+      <source>After JOIN keywords in FROM clause</source>
+      <translation>JOIN-avainsanojen jälkeen FROM-lausekkeessa</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="250"/>
-        <source>Put each column constraint in CREATE TABLE into new line</source>
-        <translation>Sijoita jokainen sarakerajoite CREATE TABLE -lauseessa uudelle riville</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="250"/>
+      <source>Put each column constraint in CREATE TABLE into new line</source>
+      <translation>Sijoita jokainen sarakerajoite CREATE TABLE -lauseessa uudelle riville</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="260"/>
-        <source>After comma</source>
-        <translation>Pilkun jälkeen</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="260"/>
+      <source>After comma</source>
+      <translation>Pilkun jälkeen</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="270"/>
-        <source>After comma in expressions</source>
-        <translation>Pilkun jälkeen lausekkeissa</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="270"/>
+      <source>After comma in expressions</source>
+      <translation>Pilkun jälkeen lausekkeissa</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="280"/>
-        <source>After semicolon</source>
-        <translation>Puolipisteen jälkeen</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="280"/>
+      <source>After semicolon</source>
+      <translation>Puolipisteen jälkeen</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="290"/>
-        <location filename="../sqlenterpriseformatter.ui" line="512"/>
-        <source>Never before semicolon</source>
-        <translation>Ei koskaan ennen puolipistettä</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="290"/>
+      <location filename="../sqlenterpriseformatter.ui" line="512"/>
+      <source>Never before semicolon</source>
+      <translation>Ei koskaan ennen puolipistettä</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="338"/>
-        <source>White spaces</source>
-        <translation>Välilyönnit</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="338"/>
+      <source>White spaces</source>
+      <translation>Välilyönnit</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="392"/>
-        <source>Before comma in lists</source>
-        <translation>Ennen pilkkua luetteloissa</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="392"/>
+      <source>Before comma in lists</source>
+      <translation>Ennen pilkkua luetteloissa</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="402"/>
-        <source>After comma in lists</source>
-        <translation>Pilkun jälkeen luetteloissa</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="402"/>
+      <source>After comma in lists</source>
+      <translation>Pilkun jälkeen luetteloissa</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="412"/>
-        <source>Before opening parenthesis</source>
-        <translation>Ennen avaavaa suljetta</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="412"/>
+      <source>Before opening parenthesis</source>
+      <translation>Ennen avaavaa suljetta</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="422"/>
-        <source>After opening parenthesis</source>
-        <translation>Avaavan sulkeen jälkeen</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="422"/>
+      <source>After opening parenthesis</source>
+      <translation>Avaavan sulkeen jälkeen</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="432"/>
-        <source>Before closing parenthesis</source>
-        <translation>Ennen sulkevaa suljetta</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="432"/>
+      <source>Before closing parenthesis</source>
+      <translation>Ennen sulkevaa suljetta</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="442"/>
-        <source>After closing parenthesis</source>
-        <translation>Sulkevan sulkeen jälkeen</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="442"/>
+      <source>After closing parenthesis</source>
+      <translation>Sulkevan sulkeen jälkeen</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="452"/>
-        <source>No space between SQL function name and opening parenthesis</source>
-        <translation>Ei välilyöntiä SQL-funktion nimen ja avaavan sulkeen väliin</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="452"/>
+      <source>No space between SQL function name and opening parenthesis</source>
+      <translation>Ei välilyöntiä SQL-funktion nimen ja avaavan sulkeen väliin</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="462"/>
-        <source>Before dot operator (in path to database object)</source>
-        <translation>Ennen pisteoperaattoria (tietokantaobjektin polussa)</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="462"/>
+      <source>Before dot operator (in path to database object)</source>
+      <translation>Ennen pisteoperaattoria (tietokantaobjektin polussa)</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="472"/>
-        <source>After dot operator (in path to database object)</source>
-        <translation>Pisteoperaattorin jälkeen (tietokantaobjektin polussa)</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="472"/>
+      <source>After dot operator (in path to database object)</source>
+      <translation>Pisteoperaattorin jälkeen (tietokantaobjektin polussa)</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="482"/>
-        <source>Before mathematical operator</source>
-        <translation>Ennen matemaattista operaattoria</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="482"/>
+      <source>Before mathematical operator</source>
+      <translation>Ennen matemaattista operaattoria</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="492"/>
-        <source>After mathematical operator</source>
-        <translation>Matemaattisen operaattorin jälkeen</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="492"/>
+      <source>After mathematical operator</source>
+      <translation>Matemaattisen operaattorin jälkeen</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="502"/>
-        <source>Never before comma</source>
-        <translation>Ei koskaan ennen pilkkua</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="502"/>
+      <source>Never before comma</source>
+      <translation>Ei koskaan ennen pilkkua</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="558"/>
-        <source>Names</source>
-        <translation>Nimet</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="558"/>
+      <source>Names</source>
+      <translation>Nimet</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="583"/>
-        <source>Preferred name wrapper</source>
-        <translation>Ensisijainen nimen ympäröintimerkki</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="583"/>
+      <source>Preferred name wrapper</source>
+      <translation>Ensisijainen nimen ympäröintimerkki</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="600"/>
-        <source>Always use name wrapping</source>
-        <translation>Käytä nimien ympäröintiä aina</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="600"/>
+      <source>Always use name wrapping</source>
+      <translation>Käytä nimien ympäröintiä aina</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="610"/>
-        <source>Uppercase data type names</source>
-        <translation>Muuta tietotyyppien nimet suuraakkosiksi</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="610"/>
+      <source>Uppercase data type names</source>
+      <translation>Muuta tietotyyppien nimet suuraakkosiksi</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="620"/>
-        <source>Uppercase keywords</source>
-        <translation>Muuta avainsanat suuraakkosiksi</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="620"/>
+      <source>Uppercase keywords</source>
+      <translation>Muuta avainsanat suuraakkosiksi</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="631"/>
-        <source>Comments</source>
-        <translation>Kommentit</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="631"/>
+      <source>Comments</source>
+      <translation>Kommentit</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="637"/>
-        <source>Preferred comment marker (where possible):</source>
-        <translation>Ensisijainen kommenttimerkki (jos mahdollista):</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="637"/>
+      <source>Preferred comment marker (where possible):</source>
+      <translation>Ensisijainen kommenttimerkki (jos mahdollista):</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="654"/>
-        <source>Move all comments to the line end</source>
-        <translation>Siirrä kaikki kommentit rivin loppuun</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="654"/>
+      <source>Move all comments to the line end</source>
+      <translation>Siirrä kaikki kommentit rivin loppuun</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="664"/>
-        <source>Line up comments at the line end</source>
-        <translation>Tasaa kommentit rivin lopussa</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="664"/>
+      <source>Line up comments at the line end</source>
+      <translation>Tasaa kommentit rivin lopussa</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="695"/>
-        <source>Preview</source>
-        <translation>Esikatselu</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="695"/>
+      <source>Preview</source>
+      <translation>Esikatselu</translation>
     </message>
-</context>
+  </context>
 </TS>
