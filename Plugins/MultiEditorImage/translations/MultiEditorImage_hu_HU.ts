d46886c54c703273a562f185b3f7bfadbbc25e5f
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 22:56:19 +0200

    New translations multieditorimage.ts (Hungarian)
    [ci skip]

--- Plugins/MultiEditorImage/translations/MultiEditorImage_hu_HU.ts
+++ Plugins/MultiEditorImage/translations/MultiEditorImage_hu_HU.ts
@@ -1,30 +1,30 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="hu" sourcelanguage="en">
-<context>
+  <context>
     <name>MultiEditorImage</name>
     <message>
-        <location filename="../multieditorimage.cpp" line="26"/>
-        <source>Zoom in by 25%</source>
-        <translation>Nagyítás 25%-kal</translation>
+      <location filename="../multieditorimage.cpp" line="26"/>
+      <source>Zoom in by 25%</source>
+      <translation>Nagyítás 25%-kal</translation>
     </message>
     <message>
-        <location filename="../multieditorimage.cpp" line="27"/>
-        <source>Zoom out by 25%</source>
-        <translation>Kicsinyítés 25%-kal</translation>
+      <location filename="../multieditorimage.cpp" line="27"/>
+      <source>Zoom out by 25%</source>
+      <translation>Kicsinyítés 25%-kal</translation>
     </message>
     <message>
-        <location filename="../multieditorimage.cpp" line="28"/>
-        <source>Reset zoom</source>
-        <translation>Nagyítás visszaállítása</translation>
+      <location filename="../multieditorimage.cpp" line="28"/>
+      <source>Reset zoom</source>
+      <translation>Nagyítás visszaállítása</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>MultiEditorImagePlugin</name>
     <message>
-        <location filename="../multieditorimage.cpp" line="200"/>
-        <source>Image</source>
-        <translation>Kép</translation>
+      <location filename="../multieditorimage.cpp" line="200"/>
+      <source>Image</source>
+      <translation>Kép</translation>
     </message>
-</context>
+  </context>
 </TS>
