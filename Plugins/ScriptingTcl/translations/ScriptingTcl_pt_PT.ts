868310274d2983c9cf76b7f2e153c2ec56f460a4
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 20:50:20 +0200

    New translations scriptingtcl.ts (Portuguese)
    [ci skip]

--- Plugins/ScriptingTcl/translations/ScriptingTcl_pt_PT.ts
+++ Plugins/ScriptingTcl/translations/ScriptingTcl_pt_PT.ts
@@ -1,27 +1,27 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="pt-PT" sourcelanguage="en">
-<context>
+  <context>
     <name>ScriptingTcl</name>
     <message>
-        <location filename="../scriptingtcl.cpp" line="428"/>
-        <source>No database available in current context, while called Tcl&apos;s &apos;%1&apos; command.</source>
-        <translation>Nenhuma base de dados disponível no contexto atual ao chamar o comando &apos;%1&apos; do Tcl.</translation>
+      <location filename="../scriptingtcl.cpp" line="428"/>
+      <source>No database available in current context, while called Tcl&apos;s &apos;%1&apos; command.</source>
+      <translation>Nenhuma base de dados disponível no contexto atual ao chamar o comando &apos;%1&apos; do Tcl.</translation>
     </message>
     <message>
-        <location filename="../scriptingtcl.cpp" line="450"/>
-        <source>Invalid &apos;%1&apos; command syntax. Should be: %2</source>
-        <translation>Sintaxe inválida do comando &apos;%1&apos;. Deve ser: %2</translation>
+      <location filename="../scriptingtcl.cpp" line="450"/>
+      <source>Invalid &apos;%1&apos; command syntax. Should be: %2</source>
+      <translation>Sintaxe inválida do comando &apos;%1&apos;. Deve ser: %2</translation>
     </message>
     <message>
-        <location filename="../scriptingtcl.cpp" line="462"/>
-        <source>Error from Tcl&apos;s &apos;%1&apos; command: %2</source>
-        <translation>Erro do comando &apos;%1&apos; do Tcl: %2</translation>
+      <location filename="../scriptingtcl.cpp" line="462"/>
+      <source>Error from Tcl&apos;s &apos;%1&apos; command: %2</source>
+      <translation>Erro do comando &apos;%1&apos; do Tcl: %2</translation>
     </message>
     <message>
-        <location filename="../scriptingtcl.cpp" line="593"/>
-        <source>Error from Tcl&apos;s&apos; &apos;%1&apos; command: %2</source>
-        <translation>Erro do comando &apos;%1&apos; do Tcl: %2</translation>
+      <location filename="../scriptingtcl.cpp" line="593"/>
+      <source>Error from Tcl&apos;s&apos; &apos;%1&apos; command: %2</source>
+      <translation>Erro do comando &apos;%1&apos; do Tcl: %2</translation>
     </message>
-</context>
+  </context>
 </TS>
