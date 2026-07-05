9db17173405ea72fe3e774228767db441ada34a1
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 20:49:52 +0200

    New translations scriptingtcl.ts (Greek)
    [ci skip]

--- Plugins/ScriptingTcl/translations/ScriptingTcl_el_GR.ts
+++ Plugins/ScriptingTcl/translations/ScriptingTcl_el_GR.ts
@@ -1,27 +1,27 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="el" sourcelanguage="en">
-<context>
+  <context>
     <name>ScriptingTcl</name>
     <message>
-        <location filename="../scriptingtcl.cpp" line="428"/>
-        <source>No database available in current context, while called Tcl&apos;s &apos;%1&apos; command.</source>
-        <translation>Δεν υπάρχει διαθέσιμη βάση δεδομένων στο τρέχον περιβάλλον, κατά την κλήση της εντολής &apos;%1&apos; του Tcl.</translation>
+      <location filename="../scriptingtcl.cpp" line="428"/>
+      <source>No database available in current context, while called Tcl&apos;s &apos;%1&apos; command.</source>
+      <translation>Δεν υπάρχει διαθέσιμη βάση δεδομένων στο τρέχον περιβάλλον, κατά την κλήση της εντολής &apos;%1&apos; του Tcl.</translation>
     </message>
     <message>
-        <location filename="../scriptingtcl.cpp" line="450"/>
-        <source>Invalid &apos;%1&apos; command syntax. Should be: %2</source>
-        <translation>Μη έγκυρη σύνταξη της εντολής &apos;%1&apos;. Πρέπει να είναι: %2</translation>
+      <location filename="../scriptingtcl.cpp" line="450"/>
+      <source>Invalid &apos;%1&apos; command syntax. Should be: %2</source>
+      <translation>Μη έγκυρη σύνταξη της εντολής &apos;%1&apos;. Πρέπει να είναι: %2</translation>
     </message>
     <message>
-        <location filename="../scriptingtcl.cpp" line="462"/>
-        <source>Error from Tcl&apos;s &apos;%1&apos; command: %2</source>
-        <translation>Σφάλμα από την εντολή &apos;%1&apos; του Tcl&apos;s: %2</translation>
+      <location filename="../scriptingtcl.cpp" line="462"/>
+      <source>Error from Tcl&apos;s &apos;%1&apos; command: %2</source>
+      <translation>Σφάλμα από την εντολή &apos;%1&apos; του Tcl&apos;s: %2</translation>
     </message>
     <message>
-        <location filename="../scriptingtcl.cpp" line="593"/>
-        <source>Error from Tcl&apos;s&apos; &apos;%1&apos; command: %2</source>
-        <translation>Σφάλμα από την εντολή &apos;%1&apos; του Tcl&apos;s&apos;: %2</translation>
+      <location filename="../scriptingtcl.cpp" line="593"/>
+      <source>Error from Tcl&apos;s&apos; &apos;%1&apos; command: %2</source>
+      <translation>Σφάλμα από την εντολή &apos;%1&apos; του Tcl&apos;s&apos;: %2</translation>
     </message>
-</context>
+  </context>
 </TS>
