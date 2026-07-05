6ff55fc10ae01e2fa98b80e4513a4b81a4cddf6b
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 23:01:44 +0200

    New translations dbsqlitemc.ts (Ukrainian)
    [ci skip]

--- Plugins/DbSQLite3MC/translations/DbSqliteMc_uk_UA.ts
+++ Plugins/DbSQLite3MC/translations/DbSqliteMc_uk_UA.ts
@@ -1,46 +1,46 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="uk" sourcelanguage="en">
-<context>
+  <context>
     <name>DbSqliteMc</name>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="42"/>
-        <source>Password (key)</source>
-        <translation>Пароль (ключ)</translation>
+      <location filename="../dbsqlitemc.cpp" line="42"/>
+      <source>Password (key)</source>
+      <translation>Пароль (ключ)</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="43"/>
-        <source>Leave empty to create or connect to decrypted database.</source>
-        <translation>Залиште порожнім, щоб створити або підключитися до розшифрованої бази даних.</translation>
+      <location filename="../dbsqlitemc.cpp" line="43"/>
+      <source>Leave empty to create or connect to decrypted database.</source>
+      <translation>Залиште порожнім, щоб створити або підключитися до розшифрованої бази даних.</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="44"/>
-        <source>Encryption password</source>
-        <translation>Пароль шифрування</translation>
+      <location filename="../dbsqlitemc.cpp" line="44"/>
+      <source>Encryption password</source>
+      <translation>Пароль шифрування</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="50"/>
-        <source>Cipher</source>
-        <translation>Шифр</translation>
+      <location filename="../dbsqlitemc.cpp" line="50"/>
+      <source>Cipher</source>
+      <translation>Шифр</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="51"/>
-        <source>Cipher determines encryption algorithm used to encrypt the database.</source>
-        <translation>Шифр визначає алгоритм шифрування, що використовується для шифрування бази даних.</translation>
+      <location filename="../dbsqlitemc.cpp" line="51"/>
+      <source>Cipher determines encryption algorithm used to encrypt the database.</source>
+      <translation>Шифр визначає алгоритм шифрування, що використовується для шифрування бази даних.</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="61"/>
-        <source>Cipher configuration (optional)</source>
-        <translation>Конфігурація шифру (необов’язково)</translation>
+      <location filename="../dbsqlitemc.cpp" line="61"/>
+      <source>Cipher configuration (optional)</source>
+      <translation>Конфігурація шифру (необов’язково)</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="62"/>
-        <source>PRAGMA statements to customize SQLite3 Multiple Ciphers configuration, such as KDF iterations, legacy mode, etc.
+      <location filename="../dbsqlitemc.cpp" line="62"/>
+      <source>PRAGMA statements to customize SQLite3 Multiple Ciphers configuration, such as KDF iterations, legacy mode, etc.
 They will be executed upon each opening of the database.
 See documentation for SQLite3 Multiple Ciphers for details.</source>
-        <translation>Інструкції PRAGMA для налаштування конфігурації SQLite3 Multiple Ciphers, як-от ітерації KDF, застарілий режим тощо.
+      <translation>Інструкції PRAGMA для налаштування конфігурації SQLite3 Multiple Ciphers, як-от ітерації KDF, застарілий режим тощо.
 Вони виконуватимуться під час кожного відкриття бази даних.
 Докладні відомості див. у документації SQLite3 Multiple Ciphers.</translation>
     </message>
-</context>
+  </context>
 </TS>
