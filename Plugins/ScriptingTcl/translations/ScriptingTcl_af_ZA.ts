e1fccc0de2f496e64f5bad17752fafd4f46ca336
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 22:52:39 +0200

    New translations scriptingtcl.ts (Afrikaans)
    [ci skip]

--- Plugins/ScriptingTcl/translations/ScriptingTcl_af_ZA.ts
+++ Plugins/ScriptingTcl/translations/ScriptingTcl_af_ZA.ts
@@ -1,27 +1,27 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="af" sourcelanguage="en">
-<context>
+  <context>
     <name>ScriptingTcl</name>
     <message>
-        <location filename="../scriptingtcl.cpp" line="428"/>
-        <source>No database available in current context, while called Tcl&apos;s &apos;%1&apos; command.</source>
-        <translation>Geen databasis is in die huidige konteks beskikbaar terwyl Tcl&apos;s &apos;%1&apos;-opdrag aangeroep is nie.</translation>
+      <location filename="../scriptingtcl.cpp" line="428"/>
+      <source>No database available in current context, while called Tcl&apos;s &apos;%1&apos; command.</source>
+      <translation>Geen databasis is in die huidige konteks beskikbaar terwyl Tcl&apos;s &apos;%1&apos;-opdrag aangeroep is nie.</translation>
     </message>
     <message>
-        <location filename="../scriptingtcl.cpp" line="450"/>
-        <source>Invalid &apos;%1&apos; command syntax. Should be: %2</source>
-        <translation>Ongeldige sintaksis vir die &apos;%1&apos;-opdrag. Moet wees: %2</translation>
+      <location filename="../scriptingtcl.cpp" line="450"/>
+      <source>Invalid &apos;%1&apos; command syntax. Should be: %2</source>
+      <translation>Ongeldige sintaksis vir die &apos;%1&apos;-opdrag. Moet wees: %2</translation>
     </message>
     <message>
-        <location filename="../scriptingtcl.cpp" line="462"/>
-        <source>Error from Tcl&apos;s &apos;%1&apos; command: %2</source>
-        <translation>Fout vanaf Tcl&apos;s &apos;%1&apos;-opdrag: %2</translation>
+      <location filename="../scriptingtcl.cpp" line="462"/>
+      <source>Error from Tcl&apos;s &apos;%1&apos; command: %2</source>
+      <translation>Fout vanaf Tcl&apos;s &apos;%1&apos;-opdrag: %2</translation>
     </message>
     <message>
-        <location filename="../scriptingtcl.cpp" line="593"/>
-        <source>Error from Tcl&apos;s&apos; &apos;%1&apos; command: %2</source>
-        <translation>Fout vanaf Tcl&apos;s&apos; &apos;%1&apos;-opdrag: %2</translation>
+      <location filename="../scriptingtcl.cpp" line="593"/>
+      <source>Error from Tcl&apos;s&apos; &apos;%1&apos; command: %2</source>
+      <translation>Fout vanaf Tcl&apos;s&apos; &apos;%1&apos;-opdrag: %2</translation>
     </message>
-</context>
+  </context>
 </TS>
