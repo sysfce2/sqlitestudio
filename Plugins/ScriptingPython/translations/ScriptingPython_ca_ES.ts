18a8366a39251e49ffd6e62f135eb704c6c99ae9
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 20:52:34 +0200

    New translations scriptingpython.ts (Catalan)
    [ci skip]

--- Plugins/ScriptingPython/translations/ScriptingPython_ca_ES.ts
+++ Plugins/ScriptingPython/translations/ScriptingPython_ca_ES.ts
@@ -1,50 +1,50 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="ca" sourcelanguage="en">
-<context>
+  <context>
     <name>QObject</name>
     <message>
-        <location filename="../scriptingpython.cpp" line="730"/>
-        <source>Invalid use of %1 function. Expected %2 arguments, but got %3.</source>
-        <translation>Ús no vàlid de la funció %1. S&apos;esperaven %2 arguments, però se n&apos;han rebut %3.</translation>
+      <location filename="../scriptingpython.cpp" line="730"/>
+      <source>Invalid use of %1 function. Expected %2 arguments, but got %3.</source>
+      <translation>Ús no vàlid de la funció %1. S&apos;esperaven %2 arguments, però se n&apos;han rebut %3.</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="739"/>
-        <source>Unknown error from function %1.</source>
-        <translation>Error desconegut de la funció %1.</translation>
+      <location filename="../scriptingpython.cpp" line="739"/>
+      <source>Unknown error from function %1.</source>
+      <translation>Error desconegut de la funció %1.</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="780"/>
-        <source>Could not calculate string representation of the Python object passed as argument to the function %1.</source>
-        <translation>No s&apos;ha pogut calcular la representació en cadena de l&apos;objecte Python passat com a argument a la funció %1.</translation>
+      <location filename="../scriptingpython.cpp" line="780"/>
+      <source>Could not calculate string representation of the Python object passed as argument to the function %1.</source>
+      <translation>No s&apos;ha pogut calcular la representació en cadena de l&apos;objecte Python passat com a argument a la funció %1.</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="802"/>
-        <source>Could not find execution context for function %1. This is a bug of Python plugin. Please report it.</source>
-        <translation>No s&apos;ha pogut trobar el context d&apos;execució per a la funció %1. Això és un error del connector de Python. Informeu-ne.</translation>
+      <location filename="../scriptingpython.cpp" line="802"/>
+      <source>Could not find execution context for function %1. This is a bug of Python plugin. Please report it.</source>
+      <translation>No s&apos;ha pogut trobar el context d&apos;execució per a la funció %1. Això és un error del connector de Python. Informeu-ne.</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="827"/>
-        <source>Error from Python function %1: %2</source>
-        <translation>Error de la funció Python %1: %2</translation>
+      <location filename="../scriptingpython.cpp" line="827"/>
+      <source>Error from Python function %1: %2</source>
+      <translation>Error de la funció Python %1: %2</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>ScriptingPython</name>
     <message>
-        <location filename="../scriptingpython.ui" line="20"/>
-        <source>Python library path</source>
-        <translation>Camí de la biblioteca Python</translation>
+      <location filename="../scriptingpython.ui" line="20"/>
+      <source>Python library path</source>
+      <translation>Camí de la biblioteca Python</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.ui" line="29"/>
-        <source>Choose a Python library file</source>
-        <translation>Trieu un fitxer de biblioteca Python</translation>
+      <location filename="../scriptingpython.ui" line="29"/>
+      <source>Choose a Python library file</source>
+      <translation>Trieu un fitxer de biblioteca Python</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="370"/>
-        <source>The plugin is not configured properly.</source>
-        <translation>El connector no està configurat correctament.</translation>
+      <location filename="../scriptingpython.cpp" line="370"/>
+      <source>The plugin is not configured properly.</source>
+      <translation>El connector no està configurat correctament.</translation>
     </message>
-</context>
+  </context>
 </TS>
