87e983ab8aabfa702e5f651ffe563488ebbcfa6c
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 20:47:43 +0200

    New translations multieditorimage.ts (Korean)
    [ci skip]

--- Plugins/MultiEditorImage/translations/MultiEditorImage_ko_KR.ts
+++ Plugins/MultiEditorImage/translations/MultiEditorImage_ko_KR.ts
@@ -1,30 +1,30 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="ko" sourcelanguage="en">
-<context>
+  <context>
     <name>MultiEditorImage</name>
     <message>
-        <location filename="../multieditorimage.cpp" line="26"/>
-        <source>Zoom in by 25%</source>
-        <translation>25% 확대</translation>
+      <location filename="../multieditorimage.cpp" line="26"/>
+      <source>Zoom in by 25%</source>
+      <translation>25% 확대</translation>
     </message>
     <message>
-        <location filename="../multieditorimage.cpp" line="27"/>
-        <source>Zoom out by 25%</source>
-        <translation>25% 축소</translation>
+      <location filename="../multieditorimage.cpp" line="27"/>
+      <source>Zoom out by 25%</source>
+      <translation>25% 축소</translation>
     </message>
     <message>
-        <location filename="../multieditorimage.cpp" line="28"/>
-        <source>Reset zoom</source>
-        <translation>확대/축소 재설정</translation>
+      <location filename="../multieditorimage.cpp" line="28"/>
+      <source>Reset zoom</source>
+      <translation>확대/축소 재설정</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>MultiEditorImagePlugin</name>
     <message>
-        <location filename="../multieditorimage.cpp" line="200"/>
-        <source>Image</source>
-        <translation>이미지</translation>
+      <location filename="../multieditorimage.cpp" line="200"/>
+      <source>Image</source>
+      <translation>이미지</translation>
     </message>
-</context>
+  </context>
 </TS>
