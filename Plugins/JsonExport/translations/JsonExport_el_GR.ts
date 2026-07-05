de5d516422ad74159c04f2f382b76a9c7e27906a
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 13:14:07 +0200

    New translations jsonexport.ts (Greek)
    [ci skip]

--- Plugins/JsonExport/translations/JsonExport_el_GR.ts
+++ Plugins/JsonExport/translations/JsonExport_el_GR.ts
@@ -1,22 +1,22 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="el" sourcelanguage="en">
-<context>
+  <context>
     <name>JsonExportConfig</name>
     <message>
-        <location filename="../jsonexport.ui" line="20"/>
-        <source>Output format</source>
-        <translation>Μορφή εξόδου</translation>
+      <location filename="../jsonexport.ui" line="20"/>
+      <source>Output format</source>
+      <translation>Μορφή εξόδου</translation>
     </message>
     <message>
-        <location filename="../jsonexport.ui" line="26"/>
-        <source>Format document (new lines, indentation)</source>
-        <translation>Μορφοποίηση εγγράφου (νέες γραμμές, εσοχές)</translation>
+      <location filename="../jsonexport.ui" line="26"/>
+      <source>Format document (new lines, indentation)</source>
+      <translation>Μορφοποίηση εγγράφου (νέες γραμμές, εσοχές)</translation>
     </message>
     <message>
-        <location filename="../jsonexport.ui" line="42"/>
-        <source>Compress (everything in one line)</source>
-        <translation>Συμπίεση (όλα σε μία γραμμή)</translation>
+      <location filename="../jsonexport.ui" line="42"/>
+      <source>Compress (everything in one line)</source>
+      <translation>Συμπίεση (όλα σε μία γραμμή)</translation>
     </message>
-</context>
+  </context>
 </TS>
