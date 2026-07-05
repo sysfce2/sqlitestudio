38fa4808ed620888c52f1324b2de6ebe39fee0ed
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 20:53:25 +0200

    New translations dbsqlitemc.ts (Romanian)
    [ci skip]

--- Plugins/DbSQLite3MC/translations/DbSqliteMc_ro_RO.ts
+++ Plugins/DbSQLite3MC/translations/DbSqliteMc_ro_RO.ts
@@ -1,46 +1,46 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="ro" sourcelanguage="en">
-<context>
+  <context>
     <name>DbSqliteMc</name>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="42"/>
-        <source>Password (key)</source>
-        <translation>Parolă (cheie)</translation>
+      <location filename="../dbsqlitemc.cpp" line="42"/>
+      <source>Password (key)</source>
+      <translation>Parolă (cheie)</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="43"/>
-        <source>Leave empty to create or connect to decrypted database.</source>
-        <translation>Lăsați gol pentru a crea sau a vă conecta la o bază de date decriptată.</translation>
+      <location filename="../dbsqlitemc.cpp" line="43"/>
+      <source>Leave empty to create or connect to decrypted database.</source>
+      <translation>Lăsați gol pentru a crea sau a vă conecta la o bază de date decriptată.</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="44"/>
-        <source>Encryption password</source>
-        <translation>Parolă de criptare</translation>
+      <location filename="../dbsqlitemc.cpp" line="44"/>
+      <source>Encryption password</source>
+      <translation>Parolă de criptare</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="50"/>
-        <source>Cipher</source>
-        <translation>Cifru</translation>
+      <location filename="../dbsqlitemc.cpp" line="50"/>
+      <source>Cipher</source>
+      <translation>Cifru</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="51"/>
-        <source>Cipher determines encryption algorithm used to encrypt the database.</source>
-        <translation>Cifrul determină algoritmul de criptare utilizat pentru a cripta baza de date.</translation>
+      <location filename="../dbsqlitemc.cpp" line="51"/>
+      <source>Cipher determines encryption algorithm used to encrypt the database.</source>
+      <translation>Cifrul determină algoritmul de criptare utilizat pentru a cripta baza de date.</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="61"/>
-        <source>Cipher configuration (optional)</source>
-        <translation>Configurarea cifrului (opțional)</translation>
+      <location filename="../dbsqlitemc.cpp" line="61"/>
+      <source>Cipher configuration (optional)</source>
+      <translation>Configurarea cifrului (opțional)</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="62"/>
-        <source>PRAGMA statements to customize SQLite3 Multiple Ciphers configuration, such as KDF iterations, legacy mode, etc.
+      <location filename="../dbsqlitemc.cpp" line="62"/>
+      <source>PRAGMA statements to customize SQLite3 Multiple Ciphers configuration, such as KDF iterations, legacy mode, etc.
 They will be executed upon each opening of the database.
 See documentation for SQLite3 Multiple Ciphers for details.</source>
-        <translation>Instrucțiuni PRAGMA pentru a personaliza configurația SQLite3 Multiple Ciphers, cum ar fi iterațiile KDF, modul legacy etc.
+      <translation>Instrucțiuni PRAGMA pentru a personaliza configurația SQLite3 Multiple Ciphers, cum ar fi iterațiile KDF, modul legacy etc.
 Acestea vor fi executate la fiecare deschidere a bazei de date.
 Consultați documentația pentru SQLite3 Multiple Ciphers pentru detalii.</translation>
     </message>
-</context>
+  </context>
 </TS>
