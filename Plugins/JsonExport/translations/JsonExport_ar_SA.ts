77d64c867eeb1694c98cdffc6b06b3dd05b9e44f
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 20:46:56 +0200

    New translations jsonexport.ts (Arabic)
    [ci skip]

--- Plugins/JsonExport/translations/JsonExport_ar_SA.ts
+++ Plugins/JsonExport/translations/JsonExport_ar_SA.ts
@@ -1,22 +1,22 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="ar" sourcelanguage="en">
-<context>
+  <context>
     <name>JsonExportConfig</name>
     <message>
-        <location filename="../jsonexport.ui" line="20"/>
-        <source>Output format</source>
-        <translation>تنسيق الإخراج</translation>
+      <location filename="../jsonexport.ui" line="20"/>
+      <source>Output format</source>
+      <translation>تنسيق الإخراج</translation>
     </message>
     <message>
-        <location filename="../jsonexport.ui" line="26"/>
-        <source>Format document (new lines, indentation)</source>
-        <translation>تنسيق المستند (أسطر جديدة، مسافات بادئة)</translation>
+      <location filename="../jsonexport.ui" line="26"/>
+      <source>Format document (new lines, indentation)</source>
+      <translation>تنسيق المستند (أسطر جديدة، مسافات بادئة)</translation>
     </message>
     <message>
-        <location filename="../jsonexport.ui" line="42"/>
-        <source>Compress (everything in one line)</source>
-        <translation>ضغط (كل شيء في سطر واحد)</translation>
+      <location filename="../jsonexport.ui" line="42"/>
+      <source>Compress (everything in one line)</source>
+      <translation>ضغط (كل شيء في سطر واحد)</translation>
     </message>
-</context>
+  </context>
 </TS>
