05ca6d7522d6357e12177a4bf59fa12ffcf7f58b
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 22:54:27 +0200

    New translations multieditorjson.ts (Czech)
    [ci skip]

--- Plugins/MultiEditorJson/translations/MultiEditorJson_cs_CZ.ts
+++ Plugins/MultiEditorJson/translations/MultiEditorJson_cs_CZ.ts
@@ -1,74 +1,74 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="cs_CZ" sourcelanguage="en">
-<context>
+  <context>
     <name>MultiEditorJson</name>
     <message>
-        <location filename="../multieditorjson.cpp" line="27"/>
-        <source>Prettify</source>
-        <translation>Zformátovat</translation>
+      <location filename="../multieditorjson.cpp" line="27"/>
+      <source>Prettify</source>
+      <translation>Zformátovat</translation>
     </message>
     <message>
-        <location filename="../multieditorjson.cpp" line="28"/>
-        <source>Format JSON with indentation</source>
-        <translation>Formátovat JSON s odsazením</translation>
+      <location filename="../multieditorjson.cpp" line="28"/>
+      <source>Format JSON with indentation</source>
+      <translation>Formátovat JSON s odsazením</translation>
     </message>
     <message>
-        <location filename="../multieditorjson.cpp" line="30"/>
-        <source>Condense</source>
-        <translation>Zhustit</translation>
+      <location filename="../multieditorjson.cpp" line="30"/>
+      <source>Condense</source>
+      <translation>Zhustit</translation>
     </message>
     <message>
-        <location filename="../multieditorjson.cpp" line="31"/>
-        <source>Remove whitespace and format on single line</source>
-        <translation>Odstranit bílé znaky a zformátovat na jeden řádek</translation>
+      <location filename="../multieditorjson.cpp" line="31"/>
+      <source>Remove whitespace and format on single line</source>
+      <translation>Odstranit bílé znaky a zformátovat na jeden řádek</translation>
     </message>
     <message>
-        <location filename="../multieditorjson.cpp" line="35"/>
-        <source>Validate</source>
-        <translation>Ověřit</translation>
+      <location filename="../multieditorjson.cpp" line="35"/>
+      <source>Validate</source>
+      <translation>Ověřit</translation>
     </message>
     <message>
-        <location filename="../multieditorjson.cpp" line="36"/>
-        <source>Validate JSON syntax</source>
-        <translation>Ověřit syntaxi JSON</translation>
+      <location filename="../multieditorjson.cpp" line="36"/>
+      <source>Validate JSON syntax</source>
+      <translation>Ověřit syntaxi JSON</translation>
     </message>
     <message>
-        <location filename="../multieditorjson.cpp" line="39"/>
-        <source>Find or replace (%1)</source>
-        <translation>Najít nebo nahradit (%1)</translation>
+      <location filename="../multieditorjson.cpp" line="39"/>
+      <source>Find or replace (%1)</source>
+      <translation>Najít nebo nahradit (%1)</translation>
     </message>
     <message>
-        <location filename="../multieditorjson.cpp" line="169"/>
-        <source>✓ Valid JSON</source>
-        <translation>✓ Platný JSON</translation>
+      <location filename="../multieditorjson.cpp" line="169"/>
+      <source>✓ Valid JSON</source>
+      <translation>✓ Platný JSON</translation>
     </message>
     <message>
-        <location filename="../multieditorjson.cpp" line="174"/>
-        <source>✗ Invalid: %1</source>
-        <translation>✗ Neplatné: %1</translation>
+      <location filename="../multieditorjson.cpp" line="174"/>
+      <source>✗ Invalid: %1</source>
+      <translation>✗ Neplatné: %1</translation>
     </message>
     <message>
-        <location filename="../multieditorjson.cpp" line="214"/>
-        <source>JSON is valid!</source>
-        <translation>JSON je platný!</translation>
+      <location filename="../multieditorjson.cpp" line="214"/>
+      <source>JSON is valid!</source>
+      <translation>JSON je platný!</translation>
     </message>
     <message>
-        <location filename="../multieditorjson.cpp" line="218"/>
-        <source>JSON validation failed: %1</source>
-        <translation>Ověření JSON selhalo: %1</translation>
+      <location filename="../multieditorjson.cpp" line="218"/>
+      <source>JSON validation failed: %1</source>
+      <translation>Ověření JSON selhalo: %1</translation>
     </message>
     <message>
-        <location filename="../multieditorjson.cpp" line="113"/>
-        <source>JSON files (*.json, *.txt)</source>
-        <translation>Soubory JSON (*.json, *.txt)</translation>
+      <location filename="../multieditorjson.cpp" line="113"/>
+      <source>JSON files (*.json, *.txt)</source>
+      <translation>Soubory JSON (*.json, *.txt)</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>MultiEditorJsonPlugin</name>
     <message>
-        <source>JSON</source>
-        <translation>JSON</translation>
+      <source>JSON</source>
+      <translation>JSON</translation>
     </message>
-</context>
+  </context>
 </TS>
