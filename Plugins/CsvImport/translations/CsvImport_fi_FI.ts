bf36a505350c96b85b1e8ea492b8f41ed3f5492b
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 22:55:49 +0200

    New translations csvimport.ts (Finnish)
    [ci skip]

--- Plugins/CsvImport/translations/CsvImport_fi_FI.ts
+++ Plugins/CsvImport/translations/CsvImport_fi_FI.ts
@@ -1,90 +1,90 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="fi" sourcelanguage="en">
-<context>
+  <context>
     <name>CsvImport</name>
     <message>
-        <location filename="../csvimport.cpp" line="32"/>
-        <source>Cannot read file %1</source>
-        <translation>Tiedostoa %1 ei voi lukea</translation>
+      <location filename="../csvimport.cpp" line="32"/>
+      <source>Cannot read file %1</source>
+      <translation>Tiedostoa %1 ei voi lukea</translation>
     </message>
     <message>
-        <location filename="../csvimport.cpp" line="64"/>
-        <source>Could not find any data in the file %1.</source>
-        <translation>Tiedostosta %1 ei löytynyt tietoja.</translation>
+      <location filename="../csvimport.cpp" line="64"/>
+      <source>Could not find any data in the file %1.</source>
+      <translation>Tiedostosta %1 ei löytynyt tietoja.</translation>
     </message>
     <message>
-        <location filename="../csvimport.cpp" line="170"/>
-        <source>Enter the custom separator character.</source>
-        <translation>Syötä mukautettu erotinmerkki.</translation>
+      <location filename="../csvimport.cpp" line="170"/>
+      <source>Enter the custom separator character.</source>
+      <translation>Syötä mukautettu erotinmerkki.</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>csvImportOptions</name>
     <message>
-        <location filename="../CsvImportOptions.ui" line="37"/>
-        <source>, (comma)</source>
-        <translation>, (pilkku)</translation>
+      <location filename="../CsvImportOptions.ui" line="37"/>
+      <source>, (comma)</source>
+      <translation>, (pilkku)</translation>
     </message>
     <message>
-        <location filename="../CsvImportOptions.ui" line="42"/>
-        <source>; (semicolon)</source>
-        <translation>; (puolipiste)</translation>
+      <location filename="../CsvImportOptions.ui" line="42"/>
+      <source>; (semicolon)</source>
+      <translation>; (puolipiste)</translation>
     </message>
     <message>
-        <location filename="../CsvImportOptions.ui" line="47"/>
-        <source>\t (tab)</source>
-        <translation>\t (sarkain)</translation>
+      <location filename="../CsvImportOptions.ui" line="47"/>
+      <source>\t (tab)</source>
+      <translation>\t (sarkain)</translation>
     </message>
     <message>
-        <location filename="../CsvImportOptions.ui" line="52"/>
-        <source>  (whitespace)</source>
-        <translation>  (välilyönti)</translation>
+      <location filename="../CsvImportOptions.ui" line="52"/>
+      <source>  (whitespace)</source>
+      <translation>  (välilyönti)</translation>
     </message>
     <message>
-        <location filename="../CsvImportOptions.ui" line="57"/>
-        <source>Custom:</source>
-        <translation>Mukautettu:</translation>
+      <location filename="../CsvImportOptions.ui" line="57"/>
+      <source>Custom:</source>
+      <translation>Mukautettu:</translation>
     </message>
     <message>
-        <location filename="../CsvImportOptions.ui" line="65"/>
-        <source>Column separator:</source>
-        <translation>Sarake-erotin:</translation>
+      <location filename="../CsvImportOptions.ui" line="65"/>
+      <source>Column separator:</source>
+      <translation>Sarake-erotin:</translation>
     </message>
     <message>
-        <location filename="../CsvImportOptions.ui" line="108"/>
-        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;If enabled, the &amp;quot; character will be interpreted as standard CSV value quotation mark, causing the string to be treated as single value until the next (ending) &amp;quot; character. If disabled, the &amp;quot; will be treated as any other character. Usually you want this to be enabled.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Jos käytössä, &amp;quot;-merkki tulkitaan CSV-arvon vakiolainausmerkiksi, jolloin merkkijonoa käsitellään yhtenä arvona seuraavaan (päättävään) &amp;quot;-merkkiin asti. Jos ei käytössä, &amp;quot;-merkkiä käsitellään kuten mitä tahansa muuta merkkiä. Yleensä tämän kannattaa olla käytössä.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+      <location filename="../CsvImportOptions.ui" line="108"/>
+      <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;If enabled, the &amp;quot; character will be interpreted as standard CSV value quotation mark, causing the string to be treated as single value until the next (ending) &amp;quot; character. If disabled, the &amp;quot; will be treated as any other character. Usually you want this to be enabled.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+      <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Jos käytössä, &amp;quot;-merkki tulkitaan CSV-arvon vakiolainausmerkiksi, jolloin merkkijonoa käsitellään yhtenä arvona seuraavaan (päättävään) &amp;quot;-merkkiin asti. Jos ei käytössä, &amp;quot;-merkkiä käsitellään kuten mitä tahansa muuta merkkiä. Yleensä tämän kannattaa olla käytössä.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
-        <location filename="../CsvImportOptions.ui" line="111"/>
-        <source>Interpret &quot; as a value quotation mark</source>
-        <translation>Tulkitse &quot; arvon lainausmerkiksi</translation>
+      <location filename="../CsvImportOptions.ui" line="111"/>
+      <source>Interpret &quot; as a value quotation mark</source>
+      <translation>Tulkitse &quot; arvon lainausmerkiksi</translation>
     </message>
     <message>
-        <location filename="../CsvImportOptions.ui" line="20"/>
-        <source>&lt;p&gt;Enable this if the first data line in your CSV file represents column names. You don&apos;t want column names to be imported into the table as a regular data.&lt;/p&gt;</source>
-        <translation>&lt;p&gt;Ota tämä käyttöön, jos CSV-tiedostosi ensimmäinen tietorivi sisältää sarakkeiden nimet. Et halua, että sarakkeiden nimet tuodaan taulukkoon tavallisena datana.&lt;/p&gt;</translation>
+      <location filename="../CsvImportOptions.ui" line="20"/>
+      <source>&lt;p&gt;Enable this if the first data line in your CSV file represents column names. You don&apos;t want column names to be imported into the table as a regular data.&lt;/p&gt;</source>
+      <translation>&lt;p&gt;Ota tämä käyttöön, jos CSV-tiedostosi ensimmäinen tietorivi sisältää sarakkeiden nimet. Et halua, että sarakkeiden nimet tuodaan taulukkoon tavallisena datana.&lt;/p&gt;</translation>
     </message>
     <message>
-        <location filename="../CsvImportOptions.ui" line="23"/>
-        <source>First line represents CSV column names</source>
-        <translation>Ensimmäinen rivi sisältää CSV-sarakkeiden nimet</translation>
+      <location filename="../CsvImportOptions.ui" line="23"/>
+      <source>First line represents CSV column names</source>
+      <translation>Ensimmäinen rivi sisältää CSV-sarakkeiden nimet</translation>
     </message>
     <message>
-        <location filename="../CsvImportOptions.ui" line="72"/>
-        <source>If enabled, then text value provided on the right will be interpreted as NULL</source>
-        <translation>Jos käytössä, oikealla annettu tekstiarvo tulkitaan NULL-arvoksi</translation>
+      <location filename="../CsvImportOptions.ui" line="72"/>
+      <source>If enabled, then text value provided on the right will be interpreted as NULL</source>
+      <translation>Jos käytössä, oikealla annettu tekstiarvo tulkitaan NULL-arvoksi</translation>
     </message>
     <message>
-        <location filename="../CsvImportOptions.ui" line="75"/>
-        <source>NULL values:</source>
-        <translation>NULL-arvot:</translation>
+      <location filename="../CsvImportOptions.ui" line="75"/>
+      <source>NULL values:</source>
+      <translation>NULL-arvot:</translation>
     </message>
     <message>
-        <location filename="../CsvImportOptions.ui" line="85"/>
-        <source>If your CSV data contains null values, define how are they represented in the CSV.</source>
-        <translation>Jos CSV-data sisältää NULL-arvoja, määritä, miten ne esitetään CSV-tiedostossa.</translation>
+      <location filename="../CsvImportOptions.ui" line="85"/>
+      <source>If your CSV data contains null values, define how are they represented in the CSV.</source>
+      <translation>Jos CSV-data sisältää NULL-arvoja, määritä, miten ne esitetään CSV-tiedostossa.</translation>
     </message>
-</context>
+  </context>
 </TS>
