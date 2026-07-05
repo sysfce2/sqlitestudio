ae910c1e88614b112b93e14f2a196dce96125478
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 23:00:58 +0200

    New translations cellrendererimage.ts (Swedish)
    [ci skip]

--- Plugins/CellRendererImage/translations/CellRendererImage_sv_SE.ts
+++ Plugins/CellRendererImage/translations/CellRendererImage_sv_SE.ts
@@ -1,36 +1,36 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="sv-SE" sourcelanguage="en">
-<context>
+  <context>
     <name>CellRendererImage</name>
     <message>
-        <location filename="../cellrendererimage.ui" line="20"/>
-        <source>Preferred image cell size</source>
-        <translation>Föredragen cellstorlek för bild</translation>
+      <location filename="../cellrendererimage.ui" line="20"/>
+      <source>Preferred image cell size</source>
+      <translation>Föredragen cellstorlek för bild</translation>
     </message>
     <message>
-        <location filename="../cellrendererimage.ui" line="42"/>
-        <source>Height</source>
-        <translation>Höjd</translation>
+      <location filename="../cellrendererimage.ui" line="42"/>
+      <source>Height</source>
+      <translation>Höjd</translation>
     </message>
     <message>
-        <location filename="../cellrendererimage.ui" line="49"/>
-        <source>Width</source>
-        <translation>Bredd</translation>
+      <location filename="../cellrendererimage.ui" line="49"/>
+      <source>Width</source>
+      <translation>Bredd</translation>
     </message>
     <message>
-        <location filename="../cellrendererimage.ui" line="72"/>
-        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:700;&quot;&gt;Tip:&lt;/span&gt; Set these values to &lt;span style=&quot; font-weight:700;&quot;&gt;1&lt;/span&gt; if you want cells to remain as small as required for regular &lt;span style=&quot; font-weight:700;&quot;&gt;text&lt;/span&gt; rendering, without affecting the overall grid layout.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:700;&quot;&gt;Tips:&lt;/span&gt; Ställ in dessa värden på &lt;span style=&quot; font-weight:700;&quot;&gt;1&lt;/span&gt; om du vill att cellerna ska förbli så små som krävs för vanlig &lt;span style=&quot; font-weight:700;&quot;&gt;text&lt;/span&gt;rendering, utan att påverka den övergripande rutnätslayouten.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+      <location filename="../cellrendererimage.ui" line="72"/>
+      <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:700;&quot;&gt;Tip:&lt;/span&gt; Set these values to &lt;span style=&quot; font-weight:700;&quot;&gt;1&lt;/span&gt; if you want cells to remain as small as required for regular &lt;span style=&quot; font-weight:700;&quot;&gt;text&lt;/span&gt; rendering, without affecting the overall grid layout.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+      <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:700;&quot;&gt;Tips:&lt;/span&gt; Ställ in dessa värden på &lt;span style=&quot; font-weight:700;&quot;&gt;1&lt;/span&gt; om du vill att cellerna ska förbli så små som krävs för vanlig &lt;span style=&quot; font-weight:700;&quot;&gt;text&lt;/span&gt;rendering, utan att påverka den övergripande rutnätslayouten.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>CellRendererImagePlugin</name>
     <message>
-        <location filename="../cellrendererimageplugin.cpp" line="42"/>
-        <source>Image</source>
-        <comment>cell preview renderer name</comment>
-        <translation>Bild</translation>
+      <location filename="../cellrendererimageplugin.cpp" line="42"/>
+      <source>Image</source>
+      <comment>cell preview renderer name</comment>
+      <translation>Bild</translation>
     </message>
-</context>
+  </context>
 </TS>
