fd3e582cf5d534859e679d8fb44474ee9d15bcc0
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 23:01:39 +0200

    New translations sqlformattersimple.ts (Ukrainian)
    [ci skip]

--- Plugins/SqlFormatterSimple/translations/SqlFormatterSimple_uk_UA.ts
+++ Plugins/SqlFormatterSimple/translations/SqlFormatterSimple_uk_UA.ts
@@ -1,17 +1,17 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="uk" sourcelanguage="en">
-<context>
+  <context>
     <name>SqlFormatterSimplePlugin</name>
     <message>
-        <location filename="../SqlFormatterSimple.ui" line="20"/>
-        <source>Upper case keywords</source>
-        <translation>Ключові слова у верхньому регістрі</translation>
+      <location filename="../SqlFormatterSimple.ui" line="20"/>
+      <source>Upper case keywords</source>
+      <translation>Ключові слова у верхньому регістрі</translation>
     </message>
     <message>
-        <location filename="../SqlFormatterSimple.ui" line="30"/>
-        <source>Reduce multiple whitespaces to single whitespace</source>
-        <translation>Звести кілька пробілів до одного пробілу</translation>
+      <location filename="../SqlFormatterSimple.ui" line="30"/>
+      <source>Reduce multiple whitespaces to single whitespace</source>
+      <translation>Звести кілька пробілів до одного пробілу</translation>
     </message>
-</context>
+  </context>
 </TS>
