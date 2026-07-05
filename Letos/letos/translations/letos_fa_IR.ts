d33c793f3d5862b2d1f9b2b51036d410b150ea9f
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 23:04:27 +0200

    New translations letos.ts (Persian)
    [ci skip]

--- Letos/letos/translations/letos_fa_IR.ts
+++ Letos/letos/translations/letos_fa_IR.ts
@@ -1,97 +1,97 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="fa" sourcelanguage="en">
-<context>
+  <context>
     <name>QObject</name>
     <message>
-        <location filename="../main.cpp" line="52"/>
-        <source>GUI interface to Letos, a SQLite manager.</source>
-        <translation>رابط کاربری گرافیکی برای Letos، یک مدیر SQLite.</translation>
+      <location filename="../main.cpp" line="52"/>
+      <source>GUI interface to Letos, a SQLite manager.</source>
+      <translation>رابط کاربری گرافیکی برای Letos، یک مدیر SQLite.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="57"/>
-        <source>Starts the application in safe mode without restoring the previous session. Use this to bypass issues caused by a corrupted session.</source>
-        <translation>برنامه را در حالت ایمن بدون بازیابی نشست قبلی شروع می‌کند. از این گزینه برای دور زدن مشکلات ناشی از یک نشست خراب استفاده کنید.</translation>
+      <location filename="../main.cpp" line="57"/>
+      <source>Starts the application in safe mode without restoring the previous session. Use this to bypass issues caused by a corrupted session.</source>
+      <translation>برنامه را در حالت ایمن بدون بازیابی نشست قبلی شروع می‌کند. از این گزینه برای دور زدن مشکلات ناشی از یک نشست خراب استفاده کنید.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="58"/>
-        <source>Do not load any plugins. Can be used alongside safe mode to further isolate potential issues.</source>
-        <translation>هیچ افزونه‌ای را بارگذاری نکنید. می‌توان از این گزینه همراه با حالت ایمن برای جداسازی بیشتر مشکلات احتمالی استفاده کرد.</translation>
+      <location filename="../main.cpp" line="58"/>
+      <source>Do not load any plugins. Can be used alongside safe mode to further isolate potential issues.</source>
+      <translation>هیچ افزونه‌ای را بارگذاری نکنید. می‌توان از این گزینه همراه با حالت ایمن برای جداسازی بیشتر مشکلات احتمالی استفاده کرد.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="59"/>
-        <source>Enables debug messages in console (accessible with F12).</source>
-        <translation>پیام‌های اشکال‌زدایی را در کنسول فعال می‌کند (با F12 قابل دسترسی است).</translation>
+      <location filename="../main.cpp" line="59"/>
+      <source>Enables debug messages in console (accessible with F12).</source>
+      <translation>پیام‌های اشکال‌زدایی را در کنسول فعال می‌کند (با F12 قابل دسترسی است).</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="60"/>
-        <source>Redirects debug messages into standard output (forces debug mode).</source>
-        <translation>پیام‌های اشکال‌زدایی را به خروجی استاندارد هدایت می‌کند (حالت اشکال‌زدایی را اجباری می‌کند).</translation>
+      <location filename="../main.cpp" line="60"/>
+      <source>Redirects debug messages into standard output (forces debug mode).</source>
+      <translation>پیام‌های اشکال‌زدایی را به خروجی استاندارد هدایت می‌کند (حالت اشکال‌زدایی را اجباری می‌کند).</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="61"/>
-        <source>Redirects debug messages into given file (forces debug mode).</source>
-        <translation>پیام‌های اشکال‌زدایی را به فایل داده‌شده هدایت می‌کند (حالت اشکال‌زدایی را اجباری می‌کند).</translation>
+      <location filename="../main.cpp" line="61"/>
+      <source>Redirects debug messages into given file (forces debug mode).</source>
+      <translation>پیام‌های اشکال‌زدایی را به فایل داده‌شده هدایت می‌کند (حالت اشکال‌زدایی را اجباری می‌کند).</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="61"/>
-        <source>log file</source>
-        <translation>فایل گزارش</translation>
+      <location filename="../main.cpp" line="61"/>
+      <source>log file</source>
+      <translation>فایل گزارش</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="62"/>
-        <source>Enables Lemon parser debug messages for SQL code assistant.</source>
-        <translation>پیام‌های اشکال‌زدایی تجزیه‌گر Lemon را برای دستیار کد SQL فعال می‌کند.</translation>
+      <location filename="../main.cpp" line="62"/>
+      <source>Enables Lemon parser debug messages for SQL code assistant.</source>
+      <translation>پیام‌های اشکال‌زدایی تجزیه‌گر Lemon را برای دستیار کد SQL فعال می‌کند.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="63"/>
-        <source>Enables debugging of every single SQL query being sent to any database.</source>
-        <translation>اشکال‌زدایی تک‌تک پرس‌وجوهای SQL ارسال‌شده به هر پایگاه داده‌ای را فعال می‌کند.</translation>
+      <location filename="../main.cpp" line="63"/>
+      <source>Enables debugging of every single SQL query being sent to any database.</source>
+      <translation>اشکال‌زدایی تک‌تک پرس‌وجوهای SQL ارسال‌شده به هر پایگاه داده‌ای را فعال می‌کند.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="64"/>
-        <source>Limits SQL query messages to only the given &lt;database&gt;.</source>
-        <translation>پیام‌های پرس‌وجوی SQL را فقط به &lt;پایگاه داده&gt; داده‌شده محدود می‌کند.</translation>
+      <location filename="../main.cpp" line="64"/>
+      <source>Limits SQL query messages to only the given &lt;database&gt;.</source>
+      <translation>پیام‌های پرس‌وجوی SQL را فقط به &lt;پایگاه داده&gt; داده‌شده محدود می‌کند.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="64"/>
-        <source>database</source>
-        <translation>پایگاه داده</translation>
+      <location filename="../main.cpp" line="64"/>
+      <source>database</source>
+      <translation>پایگاه داده</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="56"/>
-        <source>Points to the master configuration file. Read manual at wiki page for more details.</source>
-        <translation>به فایل پیکربندی اصلی اشاره می‌کند. برای جزئیات بیشتر، راهنما را در صفحهٔ ویکی بخوانید.</translation>
+      <location filename="../main.cpp" line="56"/>
+      <source>Points to the master configuration file. Read manual at wiki page for more details.</source>
+      <translation>به فایل پیکربندی اصلی اشاره می‌کند. برای جزئیات بیشتر، راهنما را در صفحهٔ ویکی بخوانید.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="56"/>
-        <source>settings file</source>
-        <translation>فایل تنظیمات</translation>
+      <location filename="../main.cpp" line="56"/>
+      <source>settings file</source>
+      <translation>فایل تنظیمات</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="65"/>
-        <source>Enables debugging of Letos&apos;s query executor.</source>
-        <translation>اشکال‌زدایی اجراکنندهٔ پرس‌وجوی Letos&apos;s را فعال می‌کند.</translation>
+      <location filename="../main.cpp" line="65"/>
+      <source>Enables debugging of Letos&apos;s query executor.</source>
+      <translation>اشکال‌زدایی اجراکنندهٔ پرس‌وجوی Letos&apos;s را فعال می‌کند.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="66"/>
-        <source>Lists plugins installed in the Letos and quits.</source>
-        <translation>افزونه‌های نصب‌شده در Letos را فهرست می‌کند و خارج می‌شود.</translation>
+      <location filename="../main.cpp" line="66"/>
+      <source>Lists plugins installed in the Letos and quits.</source>
+      <translation>افزونه‌های نصب‌شده در Letos را فهرست می‌کند و خارج می‌شود.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="79"/>
-        <source>file</source>
-        <translation>فایل</translation>
+      <location filename="../main.cpp" line="79"/>
+      <source>file</source>
+      <translation>فایل</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="79"/>
-        <source>Database file to open</source>
-        <translation>فایل پایگاه داده برای باز کردن</translation>
+      <location filename="../main.cpp" line="79"/>
+      <source>Database file to open</source>
+      <translation>فایل پایگاه داده برای باز کردن</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="163"/>
-        <source>Select configuration directory</source>
-        <translation>انتخاب پوشهٔ پیکربندی</translation>
+      <location filename="../main.cpp" line="163"/>
+      <source>Select configuration directory</source>
+      <translation>انتخاب پوشهٔ پیکربندی</translation>
     </message>
-</context>
+  </context>
 </TS>
