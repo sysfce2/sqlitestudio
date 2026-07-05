1a85ca3b6348621c56f067625bdb2414ad2d5438
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 22:57:12 +0200

    New translations regexpimport.ts (Japanese)
    [ci skip]

--- Plugins/RegExpImport/translations/RegExpImport_ja_JP.ts
+++ Plugins/RegExpImport/translations/RegExpImport_ja_JP.ts
@@ -1,86 +1,86 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="ja" sourcelanguage="en">
-<context>
+  <context>
     <name>RegExpImport</name>
     <message>
-        <location filename="../regexpimport.cpp" line="37"/>
-        <source>Text files (*.txt);;All files (*)</source>
-        <translation>テキストファイル (*.txt);;すべてのファイル (*)</translation>
+      <location filename="../regexpimport.cpp" line="37"/>
+      <source>Text files (*.txt);;All files (*)</source>
+      <translation>テキストファイル (*.txt);;すべてのファイル (*)</translation>
     </message>
     <message>
-        <location filename="../regexpimport.cpp" line="53"/>
-        <source>Cannot read file %1</source>
-        <translation>ファイル %1 を読み取れません</translation>
+      <location filename="../regexpimport.cpp" line="53"/>
+      <source>Cannot read file %1</source>
+      <translation>ファイル %1 を読み取れません</translation>
     </message>
     <message>
-        <location filename="../regexpimport.cpp" line="161"/>
-        <source>Enter the regular expression pattern.</source>
-        <translation>正規表現パターンを入力してください。</translation>
+      <location filename="../regexpimport.cpp" line="161"/>
+      <source>Enter the regular expression pattern.</source>
+      <translation>正規表現パターンを入力してください。</translation>
     </message>
     <message>
-        <location filename="../regexpimport.cpp" line="169"/>
-        <source>Invalid pattern: %1</source>
-        <translation>無効なパターン: %1</translation>
+      <location filename="../regexpimport.cpp" line="169"/>
+      <source>Invalid pattern: %1</source>
+      <translation>無効なパターン: %1</translation>
     </message>
     <message>
-        <location filename="../regexpimport.cpp" line="189"/>
-        <source>Requested capture index %1 is out of range.</source>
-        <translation>要求されたキャプチャインデックス %1 は範囲外です。</translation>
+      <location filename="../regexpimport.cpp" line="189"/>
+      <source>Requested capture index %1 is out of range.</source>
+      <translation>要求されたキャプチャインデックス %1 は範囲外です。</translation>
     </message>
     <message>
-        <location filename="../regexpimport.cpp" line="196"/>
-        <source>&lt;p&gt;Requested capture group name &apos;%1&apos;, but it&apos;s not defined in the pattern: &lt;pre&gt;%2&lt;/pre&gt;&lt;/p&gt;</source>
-        <translation>&lt;p&gt;要求されたキャプチャグループ名 &apos;%1&apos; はパターン内で定義されていません：&lt;pre&gt;%2&lt;/pre&gt;&lt;/p&gt;</translation>
+      <location filename="../regexpimport.cpp" line="196"/>
+      <source>&lt;p&gt;Requested capture group name &apos;%1&apos;, but it&apos;s not defined in the pattern: &lt;pre&gt;%2&lt;/pre&gt;&lt;/p&gt;</source>
+      <translation>&lt;p&gt;要求されたキャプチャグループ名 &apos;%1&apos; はパターン内で定義されていません：&lt;pre&gt;%2&lt;/pre&gt;&lt;/p&gt;</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>RegExpImportConfig</name>
     <message>
-        <location filename="../regexpimport.ui" line="20"/>
-        <source>Capture groups</source>
-        <translation>キャプチャグループ</translation>
+      <location filename="../regexpimport.ui" line="20"/>
+      <source>Capture groups</source>
+      <translation>キャプチャグループ</translation>
     </message>
     <message>
-        <location filename="../regexpimport.ui" line="26"/>
-        <source>Treat all RegExp capture groups as columns</source>
-        <translation>すべての正規表現キャプチャグループを列として扱う</translation>
+      <location filename="../regexpimport.ui" line="26"/>
+      <source>Treat all RegExp capture groups as columns</source>
+      <translation>すべての正規表現キャプチャグループを列として扱う</translation>
     </message>
     <message>
-        <location filename="../regexpimport.ui" line="39"/>
-        <source>Import only following groups:</source>
-        <translation>次のグループのみをインポート：</translation>
+      <location filename="../regexpimport.ui" line="39"/>
+      <source>Import only following groups:</source>
+      <translation>次のグループのみをインポート：</translation>
     </message>
     <message>
-        <location filename="../regexpimport.ui" line="52"/>
-        <source>&lt;p&gt;Enter comma separated list of capture group indexes. The 0 index refers to the entire matched string.&lt;/p&gt;
+      <location filename="../regexpimport.ui" line="52"/>
+      <source>&lt;p&gt;Enter comma separated list of capture group indexes. The 0 index refers to the entire matched string.&lt;/p&gt;
 &lt;p&gt;If you used named groups in the pattern, you can use names instead of indexes. You can mix indexes and names in this list.&lt;/p&gt;</source>
-        <translation>&lt;p&gt;キャプチャグループインデックスのカンマ区切りリストを入力してください。インデックス 0 は一致した文字列全体を指します。&lt;/p&gt;
+      <translation>&lt;p&gt;キャプチャグループインデックスのカンマ区切りリストを入力してください。インデックス 0 は一致した文字列全体を指します。&lt;/p&gt;
 &lt;p&gt;パターンで名前付きグループを使用した場合は、インデックスの代わりに名前を使用できます。このリストではインデックスと名前を混在させることができます。&lt;/p&gt;</translation>
     </message>
     <message>
-        <location filename="../regexpimport.ui" line="56"/>
-        <source>Example: 1, 3, 4</source>
-        <translation>例: 1, 3, 4</translation>
+      <location filename="../regexpimport.ui" line="56"/>
+      <source>Example: 1, 3, 4</source>
+      <translation>例: 1, 3, 4</translation>
     </message>
     <message>
-        <location filename="../regexpimport.ui" line="69"/>
-        <source>Pattern:</source>
-        <translation>パターン：</translation>
+      <location filename="../regexpimport.ui" line="69"/>
+      <source>Pattern:</source>
+      <translation>パターン：</translation>
     </message>
     <message>
-        <location filename="../regexpimport.ui" line="76"/>
-        <source>&lt;p&gt;Use Regular Expression groups to enclose parts of the expression that you want to import. If you want to use a group, that you don&apos;t want to import, then use &quot;import only following groups&quot; option below.
+      <location filename="../regexpimport.ui" line="76"/>
+      <source>&lt;p&gt;Use Regular Expression groups to enclose parts of the expression that you want to import. If you want to use a group, that you don&apos;t want to import, then use &quot;import only following groups&quot; option below.
 
 You can use named groups and refer to them in group list below. To name a group use: &lt;pre&gt;(?&amp;lt;myGroupName&amp;gt;\s+\d+\s+)&lt;/pre&gt;&lt;/p&gt;</source>
-        <translation>&lt;p&gt;インポートしたい式の部分を囲むには、正規表現グループを使用します。インポートしたくないグループを使用したい場合は、下の &quot;次のグループのみをインポート&quot; オプションを使用してください。
+      <translation>&lt;p&gt;インポートしたい式の部分を囲むには、正規表現グループを使用します。インポートしたくないグループを使用したい場合は、下の &quot;次のグループのみをインポート&quot; オプションを使用してください。
 
 名前付きグループを使用し、下のグループリストでそれらを参照できます。グループに名前を付けるには、次を使用します：&lt;pre&gt;(?&amp;lt;myGroupName&amp;gt;\s+\d+\s+)&lt;/pre&gt;&lt;/p&gt;</translation>
     </message>
     <message>
-        <location filename="../regexpimport.ui" line="81"/>
-        <source>Example: (\d+)\s+((\d+)\w+)\s+(\w+)</source>
-        <translation>例: (\d+)\s+((\d+)\w+)\s+(\w+)</translation>
+      <location filename="../regexpimport.ui" line="81"/>
+      <source>Example: (\d+)\s+((\d+)\w+)\s+(\w+)</source>
+      <translation>例: (\d+)\s+((\d+)\w+)\s+(\w+)</translation>
     </message>
-</context>
+  </context>
 </TS>
