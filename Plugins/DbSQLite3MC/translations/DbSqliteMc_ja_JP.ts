1aca22665224084c4433479541aea579ca7d381c
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 20:54:00 +0200

    New translations dbsqlitemc.ts (Japanese)
    [ci skip]

--- Plugins/DbSQLite3MC/translations/DbSqliteMc_ja_JP.ts
+++ Plugins/DbSQLite3MC/translations/DbSqliteMc_ja_JP.ts
@@ -1,46 +1,46 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="ja" sourcelanguage="en">
-<context>
+  <context>
     <name>DbSqliteMc</name>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="42"/>
-        <source>Password (key)</source>
-        <translation>パスワード（キー）</translation>
+      <location filename="../dbsqlitemc.cpp" line="42"/>
+      <source>Password (key)</source>
+      <translation>パスワード（キー）</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="43"/>
-        <source>Leave empty to create or connect to decrypted database.</source>
-        <translation>復号化されたデータベースを作成または接続するには、空のままにしてください。</translation>
+      <location filename="../dbsqlitemc.cpp" line="43"/>
+      <source>Leave empty to create or connect to decrypted database.</source>
+      <translation>復号化されたデータベースを作成または接続するには、空のままにしてください。</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="44"/>
-        <source>Encryption password</source>
-        <translation>暗号化パスワード</translation>
+      <location filename="../dbsqlitemc.cpp" line="44"/>
+      <source>Encryption password</source>
+      <translation>暗号化パスワード</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="50"/>
-        <source>Cipher</source>
-        <translation>暗号方式</translation>
+      <location filename="../dbsqlitemc.cpp" line="50"/>
+      <source>Cipher</source>
+      <translation>暗号方式</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="51"/>
-        <source>Cipher determines encryption algorithm used to encrypt the database.</source>
-        <translation>暗号方式は、データベースの暗号化に使用される暗号化アルゴリズムを決定します。</translation>
+      <location filename="../dbsqlitemc.cpp" line="51"/>
+      <source>Cipher determines encryption algorithm used to encrypt the database.</source>
+      <translation>暗号方式は、データベースの暗号化に使用される暗号化アルゴリズムを決定します。</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="61"/>
-        <source>Cipher configuration (optional)</source>
-        <translation>暗号方式の設定（任意）</translation>
+      <location filename="../dbsqlitemc.cpp" line="61"/>
+      <source>Cipher configuration (optional)</source>
+      <translation>暗号方式の設定（任意）</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="62"/>
-        <source>PRAGMA statements to customize SQLite3 Multiple Ciphers configuration, such as KDF iterations, legacy mode, etc.
+      <location filename="../dbsqlitemc.cpp" line="62"/>
+      <source>PRAGMA statements to customize SQLite3 Multiple Ciphers configuration, such as KDF iterations, legacy mode, etc.
 They will be executed upon each opening of the database.
 See documentation for SQLite3 Multiple Ciphers for details.</source>
-        <translation>KDF の反復回数、レガシーモードなど、SQLite3 Multiple Ciphers の設定をカスタマイズするための PRAGMA 文です。
+      <translation>KDF の反復回数、レガシーモードなど、SQLite3 Multiple Ciphers の設定をカスタマイズするための PRAGMA 文です。
 これらはデータベースを開くたびに実行されます。
 詳細については、SQLite3 Multiple Ciphers のドキュメントを参照してください。</translation>
     </message>
-</context>
+  </context>
 </TS>
