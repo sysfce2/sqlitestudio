25a3f2f6430227777eebfc219e5403e439fafc92
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 13:10:52 +0200

    New translations scriptingpython.ts (Persian)
    [ci skip]

--- Plugins/ScriptingPython/translations/ScriptingPython_fa_IR.ts
+++ Plugins/ScriptingPython/translations/ScriptingPython_fa_IR.ts
@@ -1,50 +1,50 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="fa" sourcelanguage="en">
-<context>
+  <context>
     <name>QObject</name>
     <message>
-        <location filename="../scriptingpython.cpp" line="730"/>
-        <source>Invalid use of %1 function. Expected %2 arguments, but got %3.</source>
-        <translation>استفادهٔ نامعتبر از تابع %1. انتظار می‌رفت %2 آرگومان دریافت شود، اما %3 دریافت شد.</translation>
+      <location filename="../scriptingpython.cpp" line="730"/>
+      <source>Invalid use of %1 function. Expected %2 arguments, but got %3.</source>
+      <translation>استفادهٔ نامعتبر از تابع %1. انتظار می‌رفت %2 آرگومان دریافت شود، اما %3 دریافت شد.</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="739"/>
-        <source>Unknown error from function %1.</source>
-        <translation>خطای ناشناخته از تابع %1.</translation>
+      <location filename="../scriptingpython.cpp" line="739"/>
+      <source>Unknown error from function %1.</source>
+      <translation>خطای ناشناخته از تابع %1.</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="780"/>
-        <source>Could not calculate string representation of the Python object passed as argument to the function %1.</source>
-        <translation>امکان محاسبهٔ نمایش رشته‌ای شیء پایتون که به‌عنوان آرگومان به تابع %1 ارسال شده است وجود نداشت.</translation>
+      <location filename="../scriptingpython.cpp" line="780"/>
+      <source>Could not calculate string representation of the Python object passed as argument to the function %1.</source>
+      <translation>امکان محاسبهٔ نمایش رشته‌ای شیء پایتون که به‌عنوان آرگومان به تابع %1 ارسال شده است وجود نداشت.</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="802"/>
-        <source>Could not find execution context for function %1. This is a bug of Python plugin. Please report it.</source>
-        <translation>امکان یافتن زمینهٔ اجرا برای تابع %1 وجود نداشت. این یک اشکال در افزونهٔ پایتون است. لطفاً آن را گزارش کنید.</translation>
+      <location filename="../scriptingpython.cpp" line="802"/>
+      <source>Could not find execution context for function %1. This is a bug of Python plugin. Please report it.</source>
+      <translation>امکان یافتن زمینهٔ اجرا برای تابع %1 وجود نداشت. این یک اشکال در افزونهٔ پایتون است. لطفاً آن را گزارش کنید.</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="827"/>
-        <source>Error from Python function %1: %2</source>
-        <translation>خطا از تابع پایتون %1: %2</translation>
+      <location filename="../scriptingpython.cpp" line="827"/>
+      <source>Error from Python function %1: %2</source>
+      <translation>خطا از تابع پایتون %1: %2</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>ScriptingPython</name>
     <message>
-        <location filename="../scriptingpython.ui" line="20"/>
-        <source>Python library path</source>
-        <translation>مسیر کتابخانهٔ پایتون</translation>
+      <location filename="../scriptingpython.ui" line="20"/>
+      <source>Python library path</source>
+      <translation>مسیر کتابخانهٔ پایتون</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.ui" line="29"/>
-        <source>Choose a Python library file</source>
-        <translation>انتخاب یک فایل کتابخانهٔ پایتون</translation>
+      <location filename="../scriptingpython.ui" line="29"/>
+      <source>Choose a Python library file</source>
+      <translation>انتخاب یک فایل کتابخانهٔ پایتون</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="370"/>
-        <source>The plugin is not configured properly.</source>
-        <translation>افزونه به‌درستی پیکربندی نشده است.</translation>
+      <location filename="../scriptingpython.cpp" line="370"/>
+      <source>The plugin is not configured properly.</source>
+      <translation>افزونه به‌درستی پیکربندی نشده است.</translation>
     </message>
-</context>
+  </context>
 </TS>
