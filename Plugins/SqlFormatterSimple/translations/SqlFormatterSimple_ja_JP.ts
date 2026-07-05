e8a572df34d5bd055ea76c9eefba6de4066b265b
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 20:51:57 +0200

    New translations sqlformattersimple.ts (Japanese)
    [ci skip]

--- Plugins/SqlFormatterSimple/translations/SqlFormatterSimple_ja_JP.ts
+++ Plugins/SqlFormatterSimple/translations/SqlFormatterSimple_ja_JP.ts
@@ -1,17 +1,17 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="ja" sourcelanguage="en">
-<context>
+  <context>
     <name>SqlFormatterSimplePlugin</name>
     <message>
-        <location filename="../SqlFormatterSimple.ui" line="20"/>
-        <source>Upper case keywords</source>
-        <translation>キーワードを大文字にする</translation>
+      <location filename="../SqlFormatterSimple.ui" line="20"/>
+      <source>Upper case keywords</source>
+      <translation>キーワードを大文字にする</translation>
     </message>
     <message>
-        <location filename="../SqlFormatterSimple.ui" line="30"/>
-        <source>Reduce multiple whitespaces to single whitespace</source>
-        <translation>複数の空白文字を単一の空白文字に減らす</translation>
+      <location filename="../SqlFormatterSimple.ui" line="30"/>
+      <source>Reduce multiple whitespaces to single whitespace</source>
+      <translation>複数の空白文字を単一の空白文字に減らす</translation>
     </message>
-</context>
+  </context>
 </TS>
