7a354d015e6b1ab7dc1c2d3143240bfc554506b5
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 22:58:04 +0200

    New translations printing.ts (Dutch)
    [ci skip]

--- Plugins/Printing/translations/Printing_nl_NL.ts
+++ Plugins/Printing/translations/Printing_nl_NL.ts
@@ -1,40 +1,40 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="nl" sourcelanguage="en">
-<context>
+  <context>
     <name>Printing</name>
     <message>
-        <location filename="../printing.cpp" line="36"/>
-        <source>Print data</source>
-        <translation>Gegevens afdrukken</translation>
+      <location filename="../printing.cpp" line="36"/>
+      <source>Print data</source>
+      <translation>Gegevens afdrukken</translation>
     </message>
     <message>
-        <location filename="../printing.cpp" line="38"/>
-        <source>Print query</source>
-        <translation>Query afdrukken</translation>
+      <location filename="../printing.cpp" line="38"/>
+      <source>Print query</source>
+      <translation>Query afdrukken</translation>
     </message>
     <message>
-        <location filename="../printing.cpp" line="75"/>
-        <source>No data to print.</source>
-        <translation>Geen gegevens om af te drukken.</translation>
+      <location filename="../printing.cpp" line="75"/>
+      <source>No data to print.</source>
+      <translation>Geen gegevens om af te drukken.</translation>
     </message>
     <message>
-        <location filename="../printing.cpp" line="83"/>
-        <source>Printing data.</source>
-        <translation>Gegevens worden afgedrukt.</translation>
+      <location filename="../printing.cpp" line="83"/>
+      <source>Printing data.</source>
+      <translation>Gegevens worden afgedrukt.</translation>
     </message>
     <message>
-        <location filename="../printing.cpp" line="109"/>
-        <source>Printing query.</source>
-        <translation>Query wordt afgedrukt.</translation>
+      <location filename="../printing.cpp" line="109"/>
+      <source>Printing query.</source>
+      <translation>Query wordt afgedrukt.</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>PrintingExport</name>
     <message>
-        <location filename="../printingexport.cpp" line="27"/>
-        <source>Printing</source>
-        <translation>Afdrukken</translation>
+      <location filename="../printingexport.cpp" line="27"/>
+      <source>Printing</source>
+      <translation>Afdrukken</translation>
     </message>
-</context>
+  </context>
 </TS>
