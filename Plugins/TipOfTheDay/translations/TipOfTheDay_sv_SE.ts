a1d13b657c94580390b2f6ccd3122d756b292039
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 23:00:59 +0200

    New translations tipoftheday.ts (Swedish)
    [ci skip]

--- Plugins/TipOfTheDay/translations/TipOfTheDay_sv_SE.ts
+++ Plugins/TipOfTheDay/translations/TipOfTheDay_sv_SE.ts
@@ -1,81 +1,81 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="sv-SE" sourcelanguage="en">
-<context>
+  <context>
     <name>TipOfTheDay</name>
     <message>
-        <location filename="../tipofthedayconfig.ui" line="20"/>
-        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Tips &amp;amp; Tricks are kept up to date on the Letos&apos;s wiki page and this option makes Letos get them from there.&lt;/p&gt;&lt;p&gt;You can disable it, so Letos will not attempt to make network connection. In that case it will use internal list, that was updated manually during this version release and may become outdated.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Tips &amp; Tricks are kept up to date on the Letos&apos;s wiki page and this option makes Letos get them from there.&lt;/p&gt;&lt;p&gt;You can disable it, so Letos will not attempt to make network connection. In that case it will use internal list, that was updated manually during this version release and may become outdated.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+      <location filename="../tipofthedayconfig.ui" line="20"/>
+      <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Tips &amp;amp; Tricks are kept up to date on the Letos&apos;s wiki page and this option makes Letos get them from there.&lt;/p&gt;&lt;p&gt;You can disable it, so Letos will not attempt to make network connection. In that case it will use internal list, that was updated manually during this version release and may become outdated.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+      <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Tips &amp; Tricks are kept up to date on the Letos&apos;s wiki page and this option makes Letos get them from there.&lt;/p&gt;&lt;p&gt;You can disable it, so Letos will not attempt to make network connection. In that case it will use internal list, that was updated manually during this version release and may become outdated.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
-        <location filename="../tipofthedayconfig.ui" line="23"/>
-        <source>Get up-to-date tips online</source>
-        <translation>Hämta aktuella tips online</translation>
+      <location filename="../tipofthedayconfig.ui" line="23"/>
+      <source>Get up-to-date tips online</source>
+      <translation>Hämta aktuella tips online</translation>
     </message>
     <message>
-        <location filename="../tipofthedayconfig.ui" line="36"/>
-        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;a href=&quot;https://github.com/pawelsalawa/letos/wiki/Tips_&amp;amp;_Tricks&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#1d99f3;&quot;&gt;Open the Tips Wiki page in your browser&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;a href=&quot;https://github.com/pawelsalawa/letos/wiki/Tips_&amp;_Tricks&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#1d99f3;&quot;&gt;Open the Tips Wiki page in your browser&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+      <location filename="../tipofthedayconfig.ui" line="36"/>
+      <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;a href=&quot;https://github.com/pawelsalawa/letos/wiki/Tips_&amp;amp;_Tricks&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#1d99f3;&quot;&gt;Open the Tips Wiki page in your browser&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+      <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;a href=&quot;https://github.com/pawelsalawa/letos/wiki/Tips_&amp;_Tricks&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#1d99f3;&quot;&gt;Open the Tips Wiki page in your browser&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
-        <location filename="../tipofthedayconfig.ui" line="49"/>
-        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;When enabled, tips will appear in the status field in short format with link to read its full content.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;När detta är aktiverat visas tips i statusfältet i kort format med en länk för att läsa hela innehållet.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+      <location filename="../tipofthedayconfig.ui" line="49"/>
+      <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;When enabled, tips will appear in the status field in short format with link to read its full content.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+      <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;När detta är aktiverat visas tips i statusfältet i kort format med en länk för att läsa hela innehållet.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
-        <location filename="../tipofthedayconfig.ui" line="52"/>
-        <source>Show Tip of the Day on startup</source>
-        <translation>Visa Dagens tips vid start</translation>
+      <location filename="../tipofthedayconfig.ui" line="52"/>
+      <source>Show Tip of the Day on startup</source>
+      <translation>Visa Dagens tips vid start</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>TipOfTheDayDialog</name>
     <message>
-        <location filename="../tipofthedaydialog.ui" line="14"/>
-        <source>Tip of the Day</source>
-        <translation>Dagens tips</translation>
+      <location filename="../tipofthedaydialog.ui" line="14"/>
+      <source>Tip of the Day</source>
+      <translation>Dagens tips</translation>
     </message>
     <message>
-        <location filename="../tipofthedaydialog.ui" line="20"/>
-        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;a href=&quot;https://github.com/pawelsalawa/letos/wiki/Tips_&amp;amp;_Tricks&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#1d99f3;&quot;&gt;Full list online&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;a href=&quot;https://github.com/pawelsalawa/letos/wiki/Tips_&amp;_Tricks&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#1d99f3;&quot;&gt;Full list online&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+      <location filename="../tipofthedaydialog.ui" line="20"/>
+      <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;a href=&quot;https://github.com/pawelsalawa/letos/wiki/Tips_&amp;amp;_Tricks&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#1d99f3;&quot;&gt;Full list online&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+      <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;a href=&quot;https://github.com/pawelsalawa/letos/wiki/Tips_&amp;_Tricks&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#1d99f3;&quot;&gt;Full list online&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
-        <location filename="../tipofthedaydialog.cpp" line="24"/>
-        <source>Configure</source>
-        <comment>tip of the day dialog</comment>
-        <translation>Konfigurera</translation>
+      <location filename="../tipofthedaydialog.cpp" line="24"/>
+      <source>Configure</source>
+      <comment>tip of the day dialog</comment>
+      <translation>Konfigurera</translation>
     </message>
     <message>
-        <location filename="../tipofthedaydialog.cpp" line="25"/>
-        <source>« Previous</source>
-        <comment>tip of the day dialog</comment>
-        <translation>« Föregående</translation>
+      <location filename="../tipofthedaydialog.cpp" line="25"/>
+      <source>« Previous</source>
+      <comment>tip of the day dialog</comment>
+      <translation>« Föregående</translation>
     </message>
     <message>
-        <location filename="../tipofthedaydialog.cpp" line="26"/>
-        <source>Next »</source>
-        <comment>tip of the day dialog</comment>
-        <translation>Nästa »</translation>
+      <location filename="../tipofthedaydialog.cpp" line="26"/>
+      <source>Next »</source>
+      <comment>tip of the day dialog</comment>
+      <translation>Nästa »</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>TipOfTheDayPlugin</name>
     <message>
-        <location filename="../tipofthedayplugin.cpp" line="20"/>
-        <source>Tip of the Day...</source>
-        <translation>Dagens tips...</translation>
+      <location filename="../tipofthedayplugin.cpp" line="20"/>
+      <source>Tip of the Day...</source>
+      <translation>Dagens tips...</translation>
     </message>
     <message>
-        <location filename="../tipofthedayplugin.cpp" line="89"/>
-        <source>Could not download tips from the internet. Letos will use locally stored tips instead.</source>
-        <translation>Det gick inte att hämta tips från internet. Letos kommer att använda lokalt lagrade tips i stället.</translation>
+      <location filename="../tipofthedayplugin.cpp" line="89"/>
+      <source>Could not download tips from the internet. Letos will use locally stored tips instead.</source>
+      <translation>Det gick inte att hämta tips från internet. Letos kommer att använda lokalt lagrade tips i stället.</translation>
     </message>
     <message>
-        <location filename="../tipofthedayplugin.cpp" line="188"/>
-        <source>&lt;b&gt;Quick tip:&lt;/b&gt; %1 - &lt;a href=&quot;%2&quot;&gt;Learn more&lt;/a&gt;</source>
-        <translation>&lt;b&gt;Snabbtips:&lt;/b&gt; %1 - &lt;a href=&quot;%2&quot;&gt;Läs mer&lt;/a&gt;</translation>
+      <location filename="../tipofthedayplugin.cpp" line="188"/>
+      <source>&lt;b&gt;Quick tip:&lt;/b&gt; %1 - &lt;a href=&quot;%2&quot;&gt;Learn more&lt;/a&gt;</source>
+      <translation>&lt;b&gt;Snabbtips:&lt;/b&gt; %1 - &lt;a href=&quot;%2&quot;&gt;Läs mer&lt;/a&gt;</translation>
     </message>
-</context>
+  </context>
 </TS>
