3b899bec9d4b0d7cb0c93e4a8bcbc60da90e18fb
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 13:20:39 +0200

    New translations scriptingpython.ts (Swedish)
    [ci skip]

--- Plugins/ScriptingPython/translations/ScriptingPython_sv_SE.ts
+++ Plugins/ScriptingPython/translations/ScriptingPython_sv_SE.ts
@@ -1,50 +1,50 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="sv-SE" sourcelanguage="en">
-<context>
+  <context>
     <name>QObject</name>
     <message>
-        <location filename="../scriptingpython.cpp" line="730"/>
-        <source>Invalid use of %1 function. Expected %2 arguments, but got %3.</source>
-        <translation>Ogiltig användning av funktionen %1. Förväntade %2 argument, men fick %3.</translation>
+      <location filename="../scriptingpython.cpp" line="730"/>
+      <source>Invalid use of %1 function. Expected %2 arguments, but got %3.</source>
+      <translation>Ogiltig användning av funktionen %1. Förväntade %2 argument, men fick %3.</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="739"/>
-        <source>Unknown error from function %1.</source>
-        <translation>Okänt fel från funktionen %1.</translation>
+      <location filename="../scriptingpython.cpp" line="739"/>
+      <source>Unknown error from function %1.</source>
+      <translation>Okänt fel från funktionen %1.</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="780"/>
-        <source>Could not calculate string representation of the Python object passed as argument to the function %1.</source>
-        <translation>Kunde inte beräkna strängrepresentationen av Python-objektet som skickades som argument till funktionen %1.</translation>
+      <location filename="../scriptingpython.cpp" line="780"/>
+      <source>Could not calculate string representation of the Python object passed as argument to the function %1.</source>
+      <translation>Kunde inte beräkna strängrepresentationen av Python-objektet som skickades som argument till funktionen %1.</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="802"/>
-        <source>Could not find execution context for function %1. This is a bug of Python plugin. Please report it.</source>
-        <translation>Kunde inte hitta exekveringskontexten för funktionen %1. Detta är en bugg i Python-insticksprogrammet. Rapportera det.</translation>
+      <location filename="../scriptingpython.cpp" line="802"/>
+      <source>Could not find execution context for function %1. This is a bug of Python plugin. Please report it.</source>
+      <translation>Kunde inte hitta exekveringskontexten för funktionen %1. Detta är en bugg i Python-insticksprogrammet. Rapportera det.</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="827"/>
-        <source>Error from Python function %1: %2</source>
-        <translation>Fel från Python-funktionen %1: %2</translation>
+      <location filename="../scriptingpython.cpp" line="827"/>
+      <source>Error from Python function %1: %2</source>
+      <translation>Fel från Python-funktionen %1: %2</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>ScriptingPython</name>
     <message>
-        <location filename="../scriptingpython.ui" line="20"/>
-        <source>Python library path</source>
-        <translation>Sökväg till Python-bibliotek</translation>
+      <location filename="../scriptingpython.ui" line="20"/>
+      <source>Python library path</source>
+      <translation>Sökväg till Python-bibliotek</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.ui" line="29"/>
-        <source>Choose a Python library file</source>
-        <translation>Välj en Python-biblioteksfil</translation>
+      <location filename="../scriptingpython.ui" line="29"/>
+      <source>Choose a Python library file</source>
+      <translation>Välj en Python-biblioteksfil</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="370"/>
-        <source>The plugin is not configured properly.</source>
-        <translation>Insticksprogrammet är inte korrekt konfigurerat.</translation>
+      <location filename="../scriptingpython.cpp" line="370"/>
+      <source>The plugin is not configured properly.</source>
+      <translation>Insticksprogrammet är inte korrekt konfigurerat.</translation>
     </message>
-</context>
+  </context>
 </TS>
