35d766b8eaf47f8aa66c9743af5a0aceb1860033
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 20:51:47 +0200

    New translations xmlexport.ts (Belarusian)
    [ci skip]

--- Plugins/XmlExport/translations/XmlExport_be_BY.ts
+++ Plugins/XmlExport/translations/XmlExport_be_BY.ts
@@ -1,70 +1,70 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="be" sourcelanguage="en">
-<context>
+  <context>
     <name>XmlExport</name>
     <message>
-        <location filename="../xmlexport.cpp" line="37"/>
-        <source>Enter the namespace to use (for example: http://my.namespace.org)</source>
-        <translation>Увядзіце прастору назваў для выкарыстання (напрыклад: http://my.namespace.org)</translation>
+      <location filename="../xmlexport.cpp" line="37"/>
+      <source>Enter the namespace to use (for example: http://my.namespace.org)</source>
+      <translation>Увядзіце прастору назваў для выкарыстання (напрыклад: http://my.namespace.org)</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>XmlExportConfig</name>
     <message>
-        <location filename="../XmlExport.ui" line="20"/>
-        <source>Output format</source>
-        <translation>Фармат вываду</translation>
+      <location filename="../XmlExport.ui" line="20"/>
+      <source>Output format</source>
+      <translation>Фармат вываду</translation>
     </message>
     <message>
-        <location filename="../XmlExport.ui" line="26"/>
-        <source>Format document (new lines, indentation)</source>
-        <translation>Фарматаваць дакумент (новыя радкі, водступы)</translation>
+      <location filename="../XmlExport.ui" line="26"/>
+      <source>Format document (new lines, indentation)</source>
+      <translation>Фарматаваць дакумент (новыя радкі, водступы)</translation>
     </message>
     <message>
-        <location filename="../XmlExport.ui" line="42"/>
-        <source>Compress (everything in one line)</source>
-        <translation>Сціснуць (усё ў адзін радок)</translation>
+      <location filename="../XmlExport.ui" line="42"/>
+      <source>Compress (everything in one line)</source>
+      <translation>Сціснуць (усё ў адзін радок)</translation>
     </message>
     <message>
-        <location filename="../XmlExport.ui" line="58"/>
-        <source>Special characters escaping</source>
-        <translation>Экранiраванне спецыяльных сімвалаў</translation>
+      <location filename="../XmlExport.ui" line="58"/>
+      <source>Special characters escaping</source>
+      <translation>Экранiраванне спецыяльных сімвалаў</translation>
     </message>
     <message>
-        <location filename="../XmlExport.ui" line="64"/>
-        <source>&lt;p&gt;Ampersands will be used for shorter values and CDATA will be used for larger values. This applies only to values that require character escaping. Other values will be exported as they are.&lt;/p&gt;</source>
-        <translation>&lt;p&gt;Для карацейшых значэнняў будуць выкарыстоўвацца амперсанды, а для большых значэнняў — CDATA. Гэта датычыцца толькі значэнняў, якія патрабуюць экранавання сімвалаў. Іншыя значэнні будуць экспартаваны як ёсць.&lt;/p&gt;</translation>
+      <location filename="../XmlExport.ui" line="64"/>
+      <source>&lt;p&gt;Ampersands will be used for shorter values and CDATA will be used for larger values. This applies only to values that require character escaping. Other values will be exported as they are.&lt;/p&gt;</source>
+      <translation>&lt;p&gt;Для карацейшых значэнняў будуць выкарыстоўвацца амперсанды, а для большых значэнняў — CDATA. Гэта датычыцца толькі значэнняў, якія патрабуюць экранавання сімвалаў. Іншыя значэнні будуць экспартаваны як ёсць.&lt;/p&gt;</translation>
     </message>
     <message>
-        <location filename="../XmlExport.ui" line="67"/>
-        <source>Use CDATA and ampersands</source>
-        <translation>Выкарыстоўваць CDATA і амперсанды</translation>
+      <location filename="../XmlExport.ui" line="67"/>
+      <source>Use CDATA and ampersands</source>
+      <translation>Выкарыстоўваць CDATA і амперсанды</translation>
     </message>
     <message>
-        <location filename="../XmlExport.ui" line="83"/>
-        <source>&lt;p&gt;Every value requiring character escepe will be enclosed in CDATA block.&lt;/p&gt;</source>
-        <translation>&lt;p&gt;Кожнае значэнне, якое патрабуе экранавання сімвалаў, будзе заключана ў блок CDATA.&lt;/p&gt;</translation>
+      <location filename="../XmlExport.ui" line="83"/>
+      <source>&lt;p&gt;Every value requiring character escepe will be enclosed in CDATA block.&lt;/p&gt;</source>
+      <translation>&lt;p&gt;Кожнае значэнне, якое патрабуе экранавання сімвалаў, будзе заключана ў блок CDATA.&lt;/p&gt;</translation>
     </message>
     <message>
-        <location filename="../XmlExport.ui" line="86"/>
-        <source>Always use CDATA</source>
-        <translation>Заўсёды выкарыстоўваць CDATA</translation>
+      <location filename="../XmlExport.ui" line="86"/>
+      <source>Always use CDATA</source>
+      <translation>Заўсёды выкарыстоўваць CDATA</translation>
     </message>
     <message>
-        <location filename="../XmlExport.ui" line="99"/>
-        <source>&lt;p&gt;Every character that require esceping will be replaced with its ampersand escape sequence. No CDATA blocks will be used.&lt;/p&gt;</source>
-        <translation>&lt;p&gt;Кожны сімвал, які патрабуе экранавання, будзе заменены яго escape-паслядоўнасцю з амперсандам. Блокі CDATA выкарыстоўвацца не будуць.&lt;/p&gt;</translation>
+      <location filename="../XmlExport.ui" line="99"/>
+      <source>&lt;p&gt;Every character that require esceping will be replaced with its ampersand escape sequence. No CDATA blocks will be used.&lt;/p&gt;</source>
+      <translation>&lt;p&gt;Кожны сімвал, які патрабуе экранавання, будзе заменены яго escape-паслядоўнасцю з амперсандам. Блокі CDATA выкарыстоўвацца не будуць.&lt;/p&gt;</translation>
     </message>
     <message>
-        <location filename="../XmlExport.ui" line="102"/>
-        <source>Always use ampersand</source>
-        <translation>Заўсёды выкарыстоўваць амперсанд</translation>
+      <location filename="../XmlExport.ui" line="102"/>
+      <source>Always use ampersand</source>
+      <translation>Заўсёды выкарыстоўваць амперсанд</translation>
     </message>
     <message>
-        <location filename="../XmlExport.ui" line="118"/>
-        <source>Define XML namespace</source>
-        <translation>Вызначыць прастору назваў XML</translation>
+      <location filename="../XmlExport.ui" line="118"/>
+      <source>Define XML namespace</source>
+      <translation>Вызначыць прастору назваў XML</translation>
     </message>
-</context>
+  </context>
 </TS>
