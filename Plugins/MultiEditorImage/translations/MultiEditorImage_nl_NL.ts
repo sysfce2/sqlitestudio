c339311c2846b5cd8728bc5efba9057b5505a34c
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 13:14:52 +0200

    New translations multieditorimage.ts (Dutch)
    [ci skip]

--- Plugins/MultiEditorImage/translations/MultiEditorImage_nl_NL.ts
+++ Plugins/MultiEditorImage/translations/MultiEditorImage_nl_NL.ts
@@ -1,30 +1,30 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="nl" sourcelanguage="en">
-<context>
+  <context>
     <name>MultiEditorImage</name>
     <message>
-        <location filename="../multieditorimage.cpp" line="26"/>
-        <source>Zoom in by 25%</source>
-        <translation>Zoom in met 25%</translation>
+      <location filename="../multieditorimage.cpp" line="26"/>
+      <source>Zoom in by 25%</source>
+      <translation>Zoom in met 25%</translation>
     </message>
     <message>
-        <location filename="../multieditorimage.cpp" line="27"/>
-        <source>Zoom out by 25%</source>
-        <translation>Zoom uit met 25%</translation>
+      <location filename="../multieditorimage.cpp" line="27"/>
+      <source>Zoom out by 25%</source>
+      <translation>Zoom uit met 25%</translation>
     </message>
     <message>
-        <location filename="../multieditorimage.cpp" line="28"/>
-        <source>Reset zoom</source>
-        <translation>Zoom resetten</translation>
+      <location filename="../multieditorimage.cpp" line="28"/>
+      <source>Reset zoom</source>
+      <translation>Zoom resetten</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>MultiEditorImagePlugin</name>
     <message>
-        <location filename="../multieditorimage.cpp" line="200"/>
-        <source>Image</source>
-        <translation>Afbeelding</translation>
+      <location filename="../multieditorimage.cpp" line="200"/>
+      <source>Image</source>
+      <translation>Afbeelding</translation>
     </message>
-</context>
+  </context>
 </TS>
