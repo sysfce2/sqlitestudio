91e6d09cc0fa8649943ccdb4b3120783756544ca
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 13:10:46 +0200

    New translations scriptingtcl.ts (Persian)
    [ci skip]

--- Plugins/ScriptingTcl/translations/ScriptingTcl_fa_IR.ts
+++ Plugins/ScriptingTcl/translations/ScriptingTcl_fa_IR.ts
@@ -1,27 +1,27 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="fa" sourcelanguage="en">
-<context>
+  <context>
     <name>ScriptingTcl</name>
     <message>
-        <location filename="../scriptingtcl.cpp" line="428"/>
-        <source>No database available in current context, while called Tcl&apos;s &apos;%1&apos; command.</source>
-        <translation>هیچ پایگاه داده‌ای در زمینهٔ فعلی در دسترس نیست، هنگام فراخوانی فرمان &apos;%1&apos; مربوط به Tcl.</translation>
+      <location filename="../scriptingtcl.cpp" line="428"/>
+      <source>No database available in current context, while called Tcl&apos;s &apos;%1&apos; command.</source>
+      <translation>هیچ پایگاه داده‌ای در زمینهٔ فعلی در دسترس نیست، هنگام فراخوانی فرمان &apos;%1&apos; مربوط به Tcl.</translation>
     </message>
     <message>
-        <location filename="../scriptingtcl.cpp" line="450"/>
-        <source>Invalid &apos;%1&apos; command syntax. Should be: %2</source>
-        <translation>نحو فرمان &apos;%1&apos; نامعتبر است. باید این باشد: %2</translation>
+      <location filename="../scriptingtcl.cpp" line="450"/>
+      <source>Invalid &apos;%1&apos; command syntax. Should be: %2</source>
+      <translation>نحو فرمان &apos;%1&apos; نامعتبر است. باید این باشد: %2</translation>
     </message>
     <message>
-        <location filename="../scriptingtcl.cpp" line="462"/>
-        <source>Error from Tcl&apos;s &apos;%1&apos; command: %2</source>
-        <translation>خطا از فرمان &apos;%1&apos; مربوط به Tcl&apos;s: %2</translation>
+      <location filename="../scriptingtcl.cpp" line="462"/>
+      <source>Error from Tcl&apos;s &apos;%1&apos; command: %2</source>
+      <translation>خطا از فرمان &apos;%1&apos; مربوط به Tcl&apos;s: %2</translation>
     </message>
     <message>
-        <location filename="../scriptingtcl.cpp" line="593"/>
-        <source>Error from Tcl&apos;s&apos; &apos;%1&apos; command: %2</source>
-        <translation>خطا از فرمان &apos;%1&apos; مربوط به Tcl&apos;s&apos;: %2</translation>
+      <location filename="../scriptingtcl.cpp" line="593"/>
+      <source>Error from Tcl&apos;s&apos; &apos;%1&apos; command: %2</source>
+      <translation>خطا از فرمان &apos;%1&apos; مربوط به Tcl&apos;s&apos;: %2</translation>
     </message>
-</context>
+  </context>
 </TS>
