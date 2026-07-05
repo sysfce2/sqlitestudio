943aa2717a57aadcaee8d497cd03c9b3ae0fe850
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 23:03:24 +0200

    New translations sqlformattersimple.ts (Vietnamese)
    [ci skip]

--- Plugins/SqlFormatterSimple/translations/SqlFormatterSimple_vi_VN.ts
+++ Plugins/SqlFormatterSimple/translations/SqlFormatterSimple_vi_VN.ts
@@ -1,17 +1,17 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="vi" sourcelanguage="en">
-<context>
+  <context>
     <name>SqlFormatterSimplePlugin</name>
     <message>
-        <location filename="../SqlFormatterSimple.ui" line="20"/>
-        <source>Upper case keywords</source>
-        <translation>Viết hoa từ khóa</translation>
+      <location filename="../SqlFormatterSimple.ui" line="20"/>
+      <source>Upper case keywords</source>
+      <translation>Viết hoa từ khóa</translation>
     </message>
     <message>
-        <location filename="../SqlFormatterSimple.ui" line="30"/>
-        <source>Reduce multiple whitespaces to single whitespace</source>
-        <translation>Giảm nhiều khoảng trắng thành một khoảng trắng</translation>
+      <location filename="../SqlFormatterSimple.ui" line="30"/>
+      <source>Reduce multiple whitespaces to single whitespace</source>
+      <translation>Giảm nhiều khoảng trắng thành một khoảng trắng</translation>
     </message>
-</context>
+  </context>
 </TS>
