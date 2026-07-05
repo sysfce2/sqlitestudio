9fa594e1bba665e58b10558ea5a1c4633bff301f
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 22:54:19 +0200

    New translations sqlexport.ts (Czech)
    [ci skip]

--- Plugins/SqlExport/translations/SqlExport_cs_CZ.ts
+++ Plugins/SqlExport/translations/SqlExport_cs_CZ.ts
@@ -1,108 +1,108 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="cs" sourcelanguage="en">
-<context>
+  <context>
     <name>SqlExport</name>
     <message>
-        <location filename="../sqlexport.cpp" line="87"/>
-        <source>-- Results of query:</source>
-        <translation>-- Výsledky dotazu:</translation>
+      <location filename="../sqlexport.cpp" line="87"/>
+      <source>-- Results of query:</source>
+      <translation>-- Výsledky dotazu:</translation>
     </message>
     <message>
-        <location filename="../sqlexport.cpp" line="159"/>
-        <source>-- Table: %1</source>
-        <translation>-- Tabulka: %1</translation>
+      <location filename="../sqlexport.cpp" line="159"/>
+      <source>-- Table: %1</source>
+      <translation>-- Tabulka: %1</translation>
     </message>
     <message>
-        <location filename="../sqlexport.cpp" line="206"/>
-        <source>-- Index: %1</source>
-        <translation>-- Index: %1</translation>
+      <location filename="../sqlexport.cpp" line="206"/>
+      <source>-- Index: %1</source>
+      <translation>-- Index: %1</translation>
     </message>
     <message>
-        <location filename="../sqlexport.cpp" line="231"/>
-        <source>-- Trigger: %1</source>
-        <translation>-- Trigger: %1</translation>
+      <location filename="../sqlexport.cpp" line="231"/>
+      <source>-- Trigger: %1</source>
+      <translation>-- Trigger: %1</translation>
     </message>
     <message>
-        <location filename="../sqlexport.cpp" line="259"/>
-        <source>-- View: %1</source>
-        <translation>-- Pohled: %1</translation>
+      <location filename="../sqlexport.cpp" line="259"/>
+      <source>-- View: %1</source>
+      <translation>-- Pohled: %1</translation>
     </message>
     <message>
-        <location filename="../sqlexport.cpp" line="284"/>
-        <source>-- File generated with Letos v%1 on %2</source>
-        <translation>-- Soubor vygenerován pomocí Letos v%1 dne %2</translation>
+      <location filename="../sqlexport.cpp" line="284"/>
+      <source>-- File generated with Letos v%1 on %2</source>
+      <translation>-- Soubor vygenerován pomocí Letos v%1 dne %2</translation>
     </message>
     <message>
-        <location filename="../sqlexport.cpp" line="288"/>
-        <source>-- Text encoding used: %1</source>
-        <translation>-- Použité kódování textu: %1</translation>
+      <location filename="../sqlexport.cpp" line="288"/>
+      <source>-- Text encoding used: %1</source>
+      <translation>-- Použité kódování textu: %1</translation>
     </message>
     <message>
-        <location filename="../sqlexport.cpp" line="370"/>
-        <source>Table name for INSERT statements is mandatory.</source>
-        <translation>Název tabulky pro příkazy INSERT je povinný.</translation>
+      <location filename="../sqlexport.cpp" line="370"/>
+      <source>Table name for INSERT statements is mandatory.</source>
+      <translation>Název tabulky pro příkazy INSERT je povinný.</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>sqlExportCommonConfig</name>
     <message>
-        <location filename="../SqlExportCommon.ui" line="30"/>
-        <source>Generate &quot;DROP IF EXISTS&quot; statement before &quot;CREATE&quot; statement</source>
-        <translation>Generovat příkaz &quot;DROP IF EXISTS&quot; před příkazem &quot;CREATE&quot;</translation>
+      <location filename="../SqlExportCommon.ui" line="30"/>
+      <source>Generate &quot;DROP IF EXISTS&quot; statement before &quot;CREATE&quot; statement</source>
+      <translation>Generovat příkaz &quot;DROP IF EXISTS&quot; před příkazem &quot;CREATE&quot;</translation>
     </message>
     <message>
-        <location filename="../SqlExportCommon.ui" line="40"/>
-        <source>Format DDL statements only (excludes &quot;INSERT&quot; statements)</source>
-        <translation>Formátovat pouze příkazy DDL (nezahrnuje příkazy &quot;INSERT&quot;)</translation>
+      <location filename="../SqlExportCommon.ui" line="40"/>
+      <source>Format DDL statements only (excludes &quot;INSERT&quot; statements)</source>
+      <translation>Formátovat pouze příkazy DDL (nezahrnuje příkazy &quot;INSERT&quot;)</translation>
     </message>
     <message>
-        <location filename="../SqlExportCommon.ui" line="50"/>
-        <source>Add &quot;IF NOT EXISTS&quot; clause to &quot;CREATE&quot; statement</source>
-        <translation>Přidat klauzuli &quot;IF NOT EXISTS&quot; do příkazu &quot;CREATE&quot;</translation>
+      <location filename="../SqlExportCommon.ui" line="50"/>
+      <source>Add &quot;IF NOT EXISTS&quot; clause to &quot;CREATE&quot; statement</source>
+      <translation>Přidat klauzuli &quot;IF NOT EXISTS&quot; do příkazu &quot;CREATE&quot;</translation>
     </message>
     <message>
-        <location filename="../SqlExportCommon.ui" line="20"/>
-        <source>Use SQL formatter to format exported SQL statements</source>
-        <translation>Použít formátovač SQL k formátování exportovaných příkazů SQL</translation>
+      <location filename="../SqlExportCommon.ui" line="20"/>
+      <source>Use SQL formatter to format exported SQL statements</source>
+      <translation>Použít formátovač SQL k formátování exportovaných příkazů SQL</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>sqlExportQueryConfig</name>
     <message>
-        <location filename="../SqlExportQuery.ui" line="47"/>
-        <source>Use SQL formatter to format exported SQL statements</source>
-        <translation>Použít formátovač SQL k formátování exportovaných příkazů SQL</translation>
+      <location filename="../SqlExportQuery.ui" line="47"/>
+      <source>Use SQL formatter to format exported SQL statements</source>
+      <translation>Použít formátovač SQL k formátování exportovaných příkazů SQL</translation>
     </message>
     <message>
-        <location filename="../SqlExportQuery.ui" line="40"/>
-        <source>Table name to use for INSERT statements:</source>
-        <translation>Název tabulky pro příkazy INSERT:</translation>
+      <location filename="../SqlExportQuery.ui" line="40"/>
+      <source>Table name to use for INSERT statements:</source>
+      <translation>Název tabulky pro příkazy INSERT:</translation>
     </message>
     <message>
-        <location filename="../SqlExportQuery.ui" line="64"/>
-        <source>Generate &quot;CREATE TABLE&quot; statement at the begining</source>
-        <translation>Generovat příkaz &quot;CREATE TABLE&quot; na začátku</translation>
+      <location filename="../SqlExportQuery.ui" line="64"/>
+      <source>Generate &quot;CREATE TABLE&quot; statement at the begining</source>
+      <translation>Generovat příkaz &quot;CREATE TABLE&quot; na začátku</translation>
     </message>
     <message>
-        <location filename="../SqlExportQuery.ui" line="20"/>
-        <source>Include the query in comments</source>
-        <translation>Zahrnout dotaz do komentářů</translation>
+      <location filename="../SqlExportQuery.ui" line="20"/>
+      <source>Include the query in comments</source>
+      <translation>Zahrnout dotaz do komentářů</translation>
     </message>
     <message>
-        <location filename="../SqlExportQuery.ui" line="30"/>
-        <source>Generate &quot;DROP IF EXISTS&quot; statement before &quot;CREATE&quot; statement</source>
-        <translation>Generovat příkaz &quot;DROP IF EXISTS&quot; před příkazem &quot;CREATE&quot;</translation>
+      <location filename="../SqlExportQuery.ui" line="30"/>
+      <source>Generate &quot;DROP IF EXISTS&quot; statement before &quot;CREATE&quot; statement</source>
+      <translation>Generovat příkaz &quot;DROP IF EXISTS&quot; před příkazem &quot;CREATE&quot;</translation>
     </message>
     <message>
-        <location filename="../SqlExportQuery.ui" line="74"/>
-        <source>Format DDL statements only (excludes &quot;INSERT&quot; statements)</source>
-        <translation>Formátovat pouze příkazy DDL (nezahrnuje příkazy &quot;INSERT&quot;)</translation>
+      <location filename="../SqlExportQuery.ui" line="74"/>
+      <source>Format DDL statements only (excludes &quot;INSERT&quot; statements)</source>
+      <translation>Formátovat pouze příkazy DDL (nezahrnuje příkazy &quot;INSERT&quot;)</translation>
     </message>
     <message>
-        <location filename="../SqlExportQuery.ui" line="84"/>
-        <source>Add &quot;IF NOT EXISTS&quot; clause to &quot;CREATE&quot; statement</source>
-        <translation>Přidat klauzuli &quot;IF NOT EXISTS&quot; do příkazu &quot;CREATE&quot;</translation>
+      <location filename="../SqlExportQuery.ui" line="84"/>
+      <source>Add &quot;IF NOT EXISTS&quot; clause to &quot;CREATE&quot; statement</source>
+      <translation>Přidat klauzuli &quot;IF NOT EXISTS&quot; do příkazu &quot;CREATE&quot;</translation>
     </message>
-</context>
+  </context>
 </TS>
