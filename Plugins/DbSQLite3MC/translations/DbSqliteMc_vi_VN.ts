49b23ccdd01a2b90a5894e88ad2df4c53cad6013
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 13:22:01 +0200

    New translations dbsqlitemc.ts (Vietnamese)
    [ci skip]

--- Plugins/DbSQLite3MC/translations/DbSqliteMc_vi_VN.ts
+++ Plugins/DbSQLite3MC/translations/DbSqliteMc_vi_VN.ts
@@ -1,46 +1,46 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="vi" sourcelanguage="en">
-<context>
+  <context>
     <name>DbSqliteMc</name>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="42"/>
-        <source>Password (key)</source>
-        <translation>Mật khẩu (khóa)</translation>
+      <location filename="../dbsqlitemc.cpp" line="42"/>
+      <source>Password (key)</source>
+      <translation>Mật khẩu (khóa)</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="43"/>
-        <source>Leave empty to create or connect to decrypted database.</source>
-        <translation>Để trống để tạo hoặc kết nối với cơ sở dữ liệu đã giải mã.</translation>
+      <location filename="../dbsqlitemc.cpp" line="43"/>
+      <source>Leave empty to create or connect to decrypted database.</source>
+      <translation>Để trống để tạo hoặc kết nối với cơ sở dữ liệu đã giải mã.</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="44"/>
-        <source>Encryption password</source>
-        <translation>Mật khẩu mã hóa</translation>
+      <location filename="../dbsqlitemc.cpp" line="44"/>
+      <source>Encryption password</source>
+      <translation>Mật khẩu mã hóa</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="50"/>
-        <source>Cipher</source>
-        <translation>Mật mã</translation>
+      <location filename="../dbsqlitemc.cpp" line="50"/>
+      <source>Cipher</source>
+      <translation>Mật mã</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="51"/>
-        <source>Cipher determines encryption algorithm used to encrypt the database.</source>
-        <translation>Mật mã xác định thuật toán mã hóa được dùng để mã hóa cơ sở dữ liệu.</translation>
+      <location filename="../dbsqlitemc.cpp" line="51"/>
+      <source>Cipher determines encryption algorithm used to encrypt the database.</source>
+      <translation>Mật mã xác định thuật toán mã hóa được dùng để mã hóa cơ sở dữ liệu.</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="61"/>
-        <source>Cipher configuration (optional)</source>
-        <translation>Cấu hình mật mã (tùy chọn)</translation>
+      <location filename="../dbsqlitemc.cpp" line="61"/>
+      <source>Cipher configuration (optional)</source>
+      <translation>Cấu hình mật mã (tùy chọn)</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="62"/>
-        <source>PRAGMA statements to customize SQLite3 Multiple Ciphers configuration, such as KDF iterations, legacy mode, etc.
+      <location filename="../dbsqlitemc.cpp" line="62"/>
+      <source>PRAGMA statements to customize SQLite3 Multiple Ciphers configuration, such as KDF iterations, legacy mode, etc.
 They will be executed upon each opening of the database.
 See documentation for SQLite3 Multiple Ciphers for details.</source>
-        <translation>Các câu lệnh PRAGMA để tùy chỉnh cấu hình SQLite3 Multiple Ciphers, chẳng hạn như số lần lặp KDF, chế độ kế thừa, v.v.
+      <translation>Các câu lệnh PRAGMA để tùy chỉnh cấu hình SQLite3 Multiple Ciphers, chẳng hạn như số lần lặp KDF, chế độ kế thừa, v.v.
 Chúng sẽ được thực thi mỗi khi mở cơ sở dữ liệu.
 Xem tài liệu về SQLite3 Multiple Ciphers để biết chi tiết.</translation>
     </message>
-</context>
+  </context>
 </TS>
