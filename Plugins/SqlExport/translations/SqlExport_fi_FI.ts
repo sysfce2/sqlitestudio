7c048dcdefd1623dae7a9978ce01a89662d75f64
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 22:55:59 +0200

    New translations sqlexport.ts (Finnish)
    [ci skip]

--- Plugins/SqlExport/translations/SqlExport_fi_FI.ts
+++ Plugins/SqlExport/translations/SqlExport_fi_FI.ts
@@ -1,108 +1,108 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="fi" sourcelanguage="en">
-<context>
+  <context>
     <name>SqlExport</name>
     <message>
-        <location filename="../sqlexport.cpp" line="87"/>
-        <source>-- Results of query:</source>
-        <translation>-- Kyselyn tulokset:</translation>
+      <location filename="../sqlexport.cpp" line="87"/>
+      <source>-- Results of query:</source>
+      <translation>-- Kyselyn tulokset:</translation>
     </message>
     <message>
-        <location filename="../sqlexport.cpp" line="159"/>
-        <source>-- Table: %1</source>
-        <translation>-- Taulu: %1</translation>
+      <location filename="../sqlexport.cpp" line="159"/>
+      <source>-- Table: %1</source>
+      <translation>-- Taulu: %1</translation>
     </message>
     <message>
-        <location filename="../sqlexport.cpp" line="206"/>
-        <source>-- Index: %1</source>
-        <translation>-- Indeksi: %1</translation>
+      <location filename="../sqlexport.cpp" line="206"/>
+      <source>-- Index: %1</source>
+      <translation>-- Indeksi: %1</translation>
     </message>
     <message>
-        <location filename="../sqlexport.cpp" line="231"/>
-        <source>-- Trigger: %1</source>
-        <translation>-- Liipaisin: %1</translation>
+      <location filename="../sqlexport.cpp" line="231"/>
+      <source>-- Trigger: %1</source>
+      <translation>-- Liipaisin: %1</translation>
     </message>
     <message>
-        <location filename="../sqlexport.cpp" line="259"/>
-        <source>-- View: %1</source>
-        <translation>-- Näkymä: %1</translation>
+      <location filename="../sqlexport.cpp" line="259"/>
+      <source>-- View: %1</source>
+      <translation>-- Näkymä: %1</translation>
     </message>
     <message>
-        <location filename="../sqlexport.cpp" line="284"/>
-        <source>-- File generated with Letos v%1 on %2</source>
-        <translation>-- Tiedosto luotu Letos v%1:lla %2</translation>
+      <location filename="../sqlexport.cpp" line="284"/>
+      <source>-- File generated with Letos v%1 on %2</source>
+      <translation>-- Tiedosto luotu Letos v%1:lla %2</translation>
     </message>
     <message>
-        <location filename="../sqlexport.cpp" line="288"/>
-        <source>-- Text encoding used: %1</source>
-        <translation>-- Käytetty tekstikoodaus: %1</translation>
+      <location filename="../sqlexport.cpp" line="288"/>
+      <source>-- Text encoding used: %1</source>
+      <translation>-- Käytetty tekstikoodaus: %1</translation>
     </message>
     <message>
-        <location filename="../sqlexport.cpp" line="370"/>
-        <source>Table name for INSERT statements is mandatory.</source>
-        <translation>Taulun nimi INSERT-lauseille on pakollinen.</translation>
+      <location filename="../sqlexport.cpp" line="370"/>
+      <source>Table name for INSERT statements is mandatory.</source>
+      <translation>Taulun nimi INSERT-lauseille on pakollinen.</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>sqlExportCommonConfig</name>
     <message>
-        <location filename="../SqlExportCommon.ui" line="30"/>
-        <source>Generate &quot;DROP IF EXISTS&quot; statement before &quot;CREATE&quot; statement</source>
-        <translation>Luo &quot;DROP IF EXISTS&quot; -lause ennen &quot;CREATE&quot;-lausetta</translation>
+      <location filename="../SqlExportCommon.ui" line="30"/>
+      <source>Generate &quot;DROP IF EXISTS&quot; statement before &quot;CREATE&quot; statement</source>
+      <translation>Luo &quot;DROP IF EXISTS&quot; -lause ennen &quot;CREATE&quot;-lausetta</translation>
     </message>
     <message>
-        <location filename="../SqlExportCommon.ui" line="40"/>
-        <source>Format DDL statements only (excludes &quot;INSERT&quot; statements)</source>
-        <translation>Muotoile vain DDL-lauseet (ei &quot;INSERT&quot;-lauseita)</translation>
+      <location filename="../SqlExportCommon.ui" line="40"/>
+      <source>Format DDL statements only (excludes &quot;INSERT&quot; statements)</source>
+      <translation>Muotoile vain DDL-lauseet (ei &quot;INSERT&quot;-lauseita)</translation>
     </message>
     <message>
-        <location filename="../SqlExportCommon.ui" line="50"/>
-        <source>Add &quot;IF NOT EXISTS&quot; clause to &quot;CREATE&quot; statement</source>
-        <translation>Lisää &quot;IF NOT EXISTS&quot; -ehto &quot;CREATE&quot;-lauseeseen</translation>
+      <location filename="../SqlExportCommon.ui" line="50"/>
+      <source>Add &quot;IF NOT EXISTS&quot; clause to &quot;CREATE&quot; statement</source>
+      <translation>Lisää &quot;IF NOT EXISTS&quot; -ehto &quot;CREATE&quot;-lauseeseen</translation>
     </message>
     <message>
-        <location filename="../SqlExportCommon.ui" line="20"/>
-        <source>Use SQL formatter to format exported SQL statements</source>
-        <translation>Käytä SQL-muotoilijaa vietyjen SQL-lauseiden muotoiluun</translation>
+      <location filename="../SqlExportCommon.ui" line="20"/>
+      <source>Use SQL formatter to format exported SQL statements</source>
+      <translation>Käytä SQL-muotoilijaa vietyjen SQL-lauseiden muotoiluun</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>sqlExportQueryConfig</name>
     <message>
-        <location filename="../SqlExportQuery.ui" line="47"/>
-        <source>Use SQL formatter to format exported SQL statements</source>
-        <translation>Käytä SQL-muotoilijaa vietyjen SQL-lauseiden muotoiluun</translation>
+      <location filename="../SqlExportQuery.ui" line="47"/>
+      <source>Use SQL formatter to format exported SQL statements</source>
+      <translation>Käytä SQL-muotoilijaa vietyjen SQL-lauseiden muotoiluun</translation>
     </message>
     <message>
-        <location filename="../SqlExportQuery.ui" line="40"/>
-        <source>Table name to use for INSERT statements:</source>
-        <translation>INSERT-lauseissa käytettävän taulun nimi:</translation>
+      <location filename="../SqlExportQuery.ui" line="40"/>
+      <source>Table name to use for INSERT statements:</source>
+      <translation>INSERT-lauseissa käytettävän taulun nimi:</translation>
     </message>
     <message>
-        <location filename="../SqlExportQuery.ui" line="64"/>
-        <source>Generate &quot;CREATE TABLE&quot; statement at the begining</source>
-        <translation>Luo &quot;CREATE TABLE&quot; -lause alkuun</translation>
+      <location filename="../SqlExportQuery.ui" line="64"/>
+      <source>Generate &quot;CREATE TABLE&quot; statement at the begining</source>
+      <translation>Luo &quot;CREATE TABLE&quot; -lause alkuun</translation>
     </message>
     <message>
-        <location filename="../SqlExportQuery.ui" line="20"/>
-        <source>Include the query in comments</source>
-        <translation>Sisällytä kysely kommentteihin</translation>
+      <location filename="../SqlExportQuery.ui" line="20"/>
+      <source>Include the query in comments</source>
+      <translation>Sisällytä kysely kommentteihin</translation>
     </message>
     <message>
-        <location filename="../SqlExportQuery.ui" line="30"/>
-        <source>Generate &quot;DROP IF EXISTS&quot; statement before &quot;CREATE&quot; statement</source>
-        <translation>Luo &quot;DROP IF EXISTS&quot; -lause ennen &quot;CREATE&quot;-lausetta</translation>
+      <location filename="../SqlExportQuery.ui" line="30"/>
+      <source>Generate &quot;DROP IF EXISTS&quot; statement before &quot;CREATE&quot; statement</source>
+      <translation>Luo &quot;DROP IF EXISTS&quot; -lause ennen &quot;CREATE&quot;-lausetta</translation>
     </message>
     <message>
-        <location filename="../SqlExportQuery.ui" line="74"/>
-        <source>Format DDL statements only (excludes &quot;INSERT&quot; statements)</source>
-        <translation>Muotoile vain DDL-lauseet (ei &quot;INSERT&quot;-lauseita)</translation>
+      <location filename="../SqlExportQuery.ui" line="74"/>
+      <source>Format DDL statements only (excludes &quot;INSERT&quot; statements)</source>
+      <translation>Muotoile vain DDL-lauseet (ei &quot;INSERT&quot;-lauseita)</translation>
     </message>
     <message>
-        <location filename="../SqlExportQuery.ui" line="84"/>
-        <source>Add &quot;IF NOT EXISTS&quot; clause to &quot;CREATE&quot; statement</source>
-        <translation>Lisää &quot;IF NOT EXISTS&quot; -ehto &quot;CREATE&quot;-lauseeseen</translation>
+      <location filename="../SqlExportQuery.ui" line="84"/>
+      <source>Add &quot;IF NOT EXISTS&quot; clause to &quot;CREATE&quot; statement</source>
+      <translation>Lisää &quot;IF NOT EXISTS&quot; -ehto &quot;CREATE&quot;-lauseeseen</translation>
     </message>
-</context>
+  </context>
 </TS>
