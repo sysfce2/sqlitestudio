e3993e1bb6170ba1516d04cd54f07f4dc9efce05
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 20:51:41 +0200

    New translations xmlexport.ts (Romanian)
    [ci skip]

--- Plugins/XmlExport/translations/XmlExport_ro_RO.ts
+++ Plugins/XmlExport/translations/XmlExport_ro_RO.ts
@@ -1,70 +1,70 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="ro" sourcelanguage="en">
-<context>
+  <context>
     <name>XmlExport</name>
     <message>
-        <location filename="../xmlexport.cpp" line="37"/>
-        <source>Enter the namespace to use (for example: http://my.namespace.org)</source>
-        <translation>Introduceți spațiul de nume de utilizat (de exemplu: http://my.namespace.org)</translation>
+      <location filename="../xmlexport.cpp" line="37"/>
+      <source>Enter the namespace to use (for example: http://my.namespace.org)</source>
+      <translation>Introduceți spațiul de nume de utilizat (de exemplu: http://my.namespace.org)</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>XmlExportConfig</name>
     <message>
-        <location filename="../XmlExport.ui" line="20"/>
-        <source>Output format</source>
-        <translation>Format de ieșire</translation>
+      <location filename="../XmlExport.ui" line="20"/>
+      <source>Output format</source>
+      <translation>Format de ieșire</translation>
     </message>
     <message>
-        <location filename="../XmlExport.ui" line="26"/>
-        <source>Format document (new lines, indentation)</source>
-        <translation>Formatează documentul (linii noi, indentare)</translation>
+      <location filename="../XmlExport.ui" line="26"/>
+      <source>Format document (new lines, indentation)</source>
+      <translation>Formatează documentul (linii noi, indentare)</translation>
     </message>
     <message>
-        <location filename="../XmlExport.ui" line="42"/>
-        <source>Compress (everything in one line)</source>
-        <translation>Comprimă (totul pe o singură linie)</translation>
+      <location filename="../XmlExport.ui" line="42"/>
+      <source>Compress (everything in one line)</source>
+      <translation>Comprimă (totul pe o singură linie)</translation>
     </message>
     <message>
-        <location filename="../XmlExport.ui" line="58"/>
-        <source>Special characters escaping</source>
-        <translation>Escaparea caracterelor speciale</translation>
+      <location filename="../XmlExport.ui" line="58"/>
+      <source>Special characters escaping</source>
+      <translation>Escaparea caracterelor speciale</translation>
     </message>
     <message>
-        <location filename="../XmlExport.ui" line="64"/>
-        <source>&lt;p&gt;Ampersands will be used for shorter values and CDATA will be used for larger values. This applies only to values that require character escaping. Other values will be exported as they are.&lt;/p&gt;</source>
-        <translation>&lt;p&gt;Pentru valorile mai scurte se vor utiliza ampersanduri, iar pentru valorile mai mari se va utiliza CDATA. Acest lucru se aplică numai valorilor care necesită escaparea caracterelor. Celelalte valori vor fi exportate așa cum sunt.&lt;/p&gt;</translation>
+      <location filename="../XmlExport.ui" line="64"/>
+      <source>&lt;p&gt;Ampersands will be used for shorter values and CDATA will be used for larger values. This applies only to values that require character escaping. Other values will be exported as they are.&lt;/p&gt;</source>
+      <translation>&lt;p&gt;Pentru valorile mai scurte se vor utiliza ampersanduri, iar pentru valorile mai mari se va utiliza CDATA. Acest lucru se aplică numai valorilor care necesită escaparea caracterelor. Celelalte valori vor fi exportate așa cum sunt.&lt;/p&gt;</translation>
     </message>
     <message>
-        <location filename="../XmlExport.ui" line="67"/>
-        <source>Use CDATA and ampersands</source>
-        <translation>Utilizează CDATA și ampersanduri</translation>
+      <location filename="../XmlExport.ui" line="67"/>
+      <source>Use CDATA and ampersands</source>
+      <translation>Utilizează CDATA și ampersanduri</translation>
     </message>
     <message>
-        <location filename="../XmlExport.ui" line="83"/>
-        <source>&lt;p&gt;Every value requiring character escepe will be enclosed in CDATA block.&lt;/p&gt;</source>
-        <translation>&lt;p&gt;Fiecare valoare care necesită escaparea caracterelor va fi inclusă într-un bloc CDATA.&lt;/p&gt;</translation>
+      <location filename="../XmlExport.ui" line="83"/>
+      <source>&lt;p&gt;Every value requiring character escepe will be enclosed in CDATA block.&lt;/p&gt;</source>
+      <translation>&lt;p&gt;Fiecare valoare care necesită escaparea caracterelor va fi inclusă într-un bloc CDATA.&lt;/p&gt;</translation>
     </message>
     <message>
-        <location filename="../XmlExport.ui" line="86"/>
-        <source>Always use CDATA</source>
-        <translation>Utilizează întotdeauna CDATA</translation>
+      <location filename="../XmlExport.ui" line="86"/>
+      <source>Always use CDATA</source>
+      <translation>Utilizează întotdeauna CDATA</translation>
     </message>
     <message>
-        <location filename="../XmlExport.ui" line="99"/>
-        <source>&lt;p&gt;Every character that require esceping will be replaced with its ampersand escape sequence. No CDATA blocks will be used.&lt;/p&gt;</source>
-        <translation>&lt;p&gt;Fiecare caracter care necesită escapare va fi înlocuit cu secvența sa de escapare cu ampersand. Nu se vor utiliza blocuri CDATA.&lt;/p&gt;</translation>
+      <location filename="../XmlExport.ui" line="99"/>
+      <source>&lt;p&gt;Every character that require esceping will be replaced with its ampersand escape sequence. No CDATA blocks will be used.&lt;/p&gt;</source>
+      <translation>&lt;p&gt;Fiecare caracter care necesită escapare va fi înlocuit cu secvența sa de escapare cu ampersand. Nu se vor utiliza blocuri CDATA.&lt;/p&gt;</translation>
     </message>
     <message>
-        <location filename="../XmlExport.ui" line="102"/>
-        <source>Always use ampersand</source>
-        <translation>Utilizează întotdeauna ampersandul</translation>
+      <location filename="../XmlExport.ui" line="102"/>
+      <source>Always use ampersand</source>
+      <translation>Utilizează întotdeauna ampersandul</translation>
     </message>
     <message>
-        <location filename="../XmlExport.ui" line="118"/>
-        <source>Define XML namespace</source>
-        <translation>Definește spațiul de nume XML</translation>
+      <location filename="../XmlExport.ui" line="118"/>
+      <source>Define XML namespace</source>
+      <translation>Definește spațiul de nume XML</translation>
     </message>
-</context>
+  </context>
 </TS>
