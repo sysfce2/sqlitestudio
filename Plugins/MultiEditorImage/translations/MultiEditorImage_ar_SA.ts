a170398957e80e30ea175a385c3b259d6caa9e71
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 20:47:12 +0200

    New translations multieditorimage.ts (Arabic)
    [ci skip]

--- Plugins/MultiEditorImage/translations/MultiEditorImage_ar_SA.ts
+++ Plugins/MultiEditorImage/translations/MultiEditorImage_ar_SA.ts
@@ -1,30 +1,30 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="ar" sourcelanguage="en">
-<context>
+  <context>
     <name>MultiEditorImage</name>
     <message>
-        <location filename="../multieditorimage.cpp" line="26"/>
-        <source>Zoom in by 25%</source>
-        <translation>تكبير بنسبة 25%</translation>
+      <location filename="../multieditorimage.cpp" line="26"/>
+      <source>Zoom in by 25%</source>
+      <translation>تكبير بنسبة 25%</translation>
     </message>
     <message>
-        <location filename="../multieditorimage.cpp" line="27"/>
-        <source>Zoom out by 25%</source>
-        <translation>تصغير بنسبة 25%</translation>
+      <location filename="../multieditorimage.cpp" line="27"/>
+      <source>Zoom out by 25%</source>
+      <translation>تصغير بنسبة 25%</translation>
     </message>
     <message>
-        <location filename="../multieditorimage.cpp" line="28"/>
-        <source>Reset zoom</source>
-        <translation>إعادة ضبط التكبير/التصغير</translation>
+      <location filename="../multieditorimage.cpp" line="28"/>
+      <source>Reset zoom</source>
+      <translation>إعادة ضبط التكبير/التصغير</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>MultiEditorImagePlugin</name>
     <message>
-        <location filename="../multieditorimage.cpp" line="200"/>
-        <source>Image</source>
-        <translation>صورة</translation>
+      <location filename="../multieditorimage.cpp" line="200"/>
+      <source>Image</source>
+      <translation>صورة</translation>
     </message>
-</context>
+  </context>
 </TS>
