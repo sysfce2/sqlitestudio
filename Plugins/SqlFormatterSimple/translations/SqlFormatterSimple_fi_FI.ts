7a91c858b66cbc83ae7abeae7f2ec7b494bb96d8
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 13:19:11 +0200

    New translations sqlformattersimple.ts (Finnish)
    [ci skip]

--- Plugins/SqlFormatterSimple/translations/SqlFormatterSimple_fi_FI.ts
+++ Plugins/SqlFormatterSimple/translations/SqlFormatterSimple_fi_FI.ts
@@ -1,17 +1,17 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="fi" sourcelanguage="en">
-<context>
+  <context>
     <name>SqlFormatterSimplePlugin</name>
     <message>
-        <location filename="../SqlFormatterSimple.ui" line="20"/>
-        <source>Upper case keywords</source>
-        <translation>Avainsanat isoilla kirjaimilla</translation>
+      <location filename="../SqlFormatterSimple.ui" line="20"/>
+      <source>Upper case keywords</source>
+      <translation>Avainsanat isoilla kirjaimilla</translation>
     </message>
     <message>
-        <location filename="../SqlFormatterSimple.ui" line="30"/>
-        <source>Reduce multiple whitespaces to single whitespace</source>
-        <translation>Vähennä useat tyhjämerkit yhdeksi tyhjämerkiksi</translation>
+      <location filename="../SqlFormatterSimple.ui" line="30"/>
+      <source>Reduce multiple whitespaces to single whitespace</source>
+      <translation>Vähennä useat tyhjämerkit yhdeksi tyhjämerkiksi</translation>
     </message>
-</context>
+  </context>
 </TS>
