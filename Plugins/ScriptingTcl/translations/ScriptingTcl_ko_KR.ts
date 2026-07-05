7beadd5664c720e16abc2bc7c1bb2bdf9b2340f2
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 20:50:16 +0200

    New translations scriptingtcl.ts (Korean)
    [ci skip]

--- Plugins/ScriptingTcl/translations/ScriptingTcl_ko_KR.ts
+++ Plugins/ScriptingTcl/translations/ScriptingTcl_ko_KR.ts
@@ -1,27 +1,27 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="ko" sourcelanguage="en">
-<context>
+  <context>
     <name>ScriptingTcl</name>
     <message>
-        <location filename="../scriptingtcl.cpp" line="428"/>
-        <source>No database available in current context, while called Tcl&apos;s &apos;%1&apos; command.</source>
-        <translation>현재 컨텍스트에서 사용할 수 있는 데이터베이스가 없습니다. Tcl&apos;s &apos;%1&apos; 명령을 호출하는 중입니다.</translation>
+      <location filename="../scriptingtcl.cpp" line="428"/>
+      <source>No database available in current context, while called Tcl&apos;s &apos;%1&apos; command.</source>
+      <translation>현재 컨텍스트에서 사용할 수 있는 데이터베이스가 없습니다. Tcl&apos;s &apos;%1&apos; 명령을 호출하는 중입니다.</translation>
     </message>
     <message>
-        <location filename="../scriptingtcl.cpp" line="450"/>
-        <source>Invalid &apos;%1&apos; command syntax. Should be: %2</source>
-        <translation>잘못된 &apos;%1&apos; 명령 구문입니다. 올바른 형식: %2</translation>
+      <location filename="../scriptingtcl.cpp" line="450"/>
+      <source>Invalid &apos;%1&apos; command syntax. Should be: %2</source>
+      <translation>잘못된 &apos;%1&apos; 명령 구문입니다. 올바른 형식: %2</translation>
     </message>
     <message>
-        <location filename="../scriptingtcl.cpp" line="462"/>
-        <source>Error from Tcl&apos;s &apos;%1&apos; command: %2</source>
-        <translation>Tcl&apos;s &apos;%1&apos; 명령에서 오류 발생: %2</translation>
+      <location filename="../scriptingtcl.cpp" line="462"/>
+      <source>Error from Tcl&apos;s &apos;%1&apos; command: %2</source>
+      <translation>Tcl&apos;s &apos;%1&apos; 명령에서 오류 발생: %2</translation>
     </message>
     <message>
-        <location filename="../scriptingtcl.cpp" line="593"/>
-        <source>Error from Tcl&apos;s&apos; &apos;%1&apos; command: %2</source>
-        <translation>Tcl&apos;s&apos; &apos;%1&apos; 명령에서 오류 발생: %2</translation>
+      <location filename="../scriptingtcl.cpp" line="593"/>
+      <source>Error from Tcl&apos;s&apos; &apos;%1&apos; command: %2</source>
+      <translation>Tcl&apos;s&apos; &apos;%1&apos; 명령에서 오류 발생: %2</translation>
     </message>
-</context>
+  </context>
 </TS>
