81cd10c1377f4f030dac4e40381c43c47843b343
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 20:51:59 +0200

    New translations sqlformattersimple.ts (Dutch)
    [ci skip]

--- Plugins/SqlFormatterSimple/translations/SqlFormatterSimple_nl_NL.ts
+++ Plugins/SqlFormatterSimple/translations/SqlFormatterSimple_nl_NL.ts
@@ -1,17 +1,17 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="nl" sourcelanguage="en">
-<context>
+  <context>
     <name>SqlFormatterSimplePlugin</name>
     <message>
-        <location filename="../SqlFormatterSimple.ui" line="20"/>
-        <source>Upper case keywords</source>
-        <translation>Trefwoorden in hoofdletters</translation>
+      <location filename="../SqlFormatterSimple.ui" line="20"/>
+      <source>Upper case keywords</source>
+      <translation>Trefwoorden in hoofdletters</translation>
     </message>
     <message>
-        <location filename="../SqlFormatterSimple.ui" line="30"/>
-        <source>Reduce multiple whitespaces to single whitespace</source>
-        <translation>Meerdere witruimtes reduceren tot één witruimte</translation>
+      <location filename="../SqlFormatterSimple.ui" line="30"/>
+      <source>Reduce multiple whitespaces to single whitespace</source>
+      <translation>Meerdere witruimtes reduceren tot één witruimte</translation>
     </message>
-</context>
+  </context>
 </TS>
