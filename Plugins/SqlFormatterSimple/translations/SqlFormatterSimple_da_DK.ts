0ac29d7a70590e41c67f8ccefa1888230f692e93
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 20:51:35 +0200

    New translations sqlformattersimple.ts (Danish)
    [ci skip]

--- Plugins/SqlFormatterSimple/translations/SqlFormatterSimple_da_DK.ts
+++ Plugins/SqlFormatterSimple/translations/SqlFormatterSimple_da_DK.ts
@@ -1,17 +1,17 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="da" sourcelanguage="en">
-<context>
+  <context>
     <name>SqlFormatterSimplePlugin</name>
     <message>
-        <location filename="../SqlFormatterSimple.ui" line="20"/>
-        <source>Upper case keywords</source>
-        <translation>Nøgleord med store bogstaver</translation>
+      <location filename="../SqlFormatterSimple.ui" line="20"/>
+      <source>Upper case keywords</source>
+      <translation>Nøgleord med store bogstaver</translation>
     </message>
     <message>
-        <location filename="../SqlFormatterSimple.ui" line="30"/>
-        <source>Reduce multiple whitespaces to single whitespace</source>
-        <translation>Reducer flere blanktegn til et enkelt blanktegn</translation>
+      <location filename="../SqlFormatterSimple.ui" line="30"/>
+      <source>Reduce multiple whitespaces to single whitespace</source>
+      <translation>Reducer flere blanktegn til et enkelt blanktegn</translation>
     </message>
-</context>
+  </context>
 </TS>
