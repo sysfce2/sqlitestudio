992dbe6fc387a1612af4b73dba42b13ec12bd04c
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 20:53:28 +0200

    New translations dbsqlitemc.ts (Afrikaans)
    [ci skip]

--- Plugins/DbSQLite3MC/translations/DbSqliteMc_af_ZA.ts
+++ Plugins/DbSQLite3MC/translations/DbSqliteMc_af_ZA.ts
@@ -1,46 +1,46 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="af" sourcelanguage="en">
-<context>
+  <context>
     <name>DbSqliteMc</name>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="42"/>
-        <source>Password (key)</source>
-        <translation>Wagwoord (sleutel)</translation>
+      <location filename="../dbsqlitemc.cpp" line="42"/>
+      <source>Password (key)</source>
+      <translation>Wagwoord (sleutel)</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="43"/>
-        <source>Leave empty to create or connect to decrypted database.</source>
-        <translation>Laat leeg om &apos;n ongeënkripteerde databasis te skep of daaraan te koppel.</translation>
+      <location filename="../dbsqlitemc.cpp" line="43"/>
+      <source>Leave empty to create or connect to decrypted database.</source>
+      <translation>Laat leeg om &apos;n ongeënkripteerde databasis te skep of daaraan te koppel.</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="44"/>
-        <source>Encryption password</source>
-        <translation>Enkripsiewagwoord</translation>
+      <location filename="../dbsqlitemc.cpp" line="44"/>
+      <source>Encryption password</source>
+      <translation>Enkripsiewagwoord</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="50"/>
-        <source>Cipher</source>
-        <translation>Sifer</translation>
+      <location filename="../dbsqlitemc.cpp" line="50"/>
+      <source>Cipher</source>
+      <translation>Sifer</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="51"/>
-        <source>Cipher determines encryption algorithm used to encrypt the database.</source>
-        <translation>Sifer bepaal die enkripsie-algoritme wat gebruik word om die databasis te enkripteer.</translation>
+      <location filename="../dbsqlitemc.cpp" line="51"/>
+      <source>Cipher determines encryption algorithm used to encrypt the database.</source>
+      <translation>Sifer bepaal die enkripsie-algoritme wat gebruik word om die databasis te enkripteer.</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="61"/>
-        <source>Cipher configuration (optional)</source>
-        <translation>Siferkonfigurasie (opsioneel)</translation>
+      <location filename="../dbsqlitemc.cpp" line="61"/>
+      <source>Cipher configuration (optional)</source>
+      <translation>Siferkonfigurasie (opsioneel)</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="62"/>
-        <source>PRAGMA statements to customize SQLite3 Multiple Ciphers configuration, such as KDF iterations, legacy mode, etc.
+      <location filename="../dbsqlitemc.cpp" line="62"/>
+      <source>PRAGMA statements to customize SQLite3 Multiple Ciphers configuration, such as KDF iterations, legacy mode, etc.
 They will be executed upon each opening of the database.
 See documentation for SQLite3 Multiple Ciphers for details.</source>
-        <translation>PRAGMA-stellings om SQLite3 Multiple Ciphers-konfigurasie aan te pas, soos KDF-iterasies, nalatenskapmodus, ens.
+      <translation>PRAGMA-stellings om SQLite3 Multiple Ciphers-konfigurasie aan te pas, soos KDF-iterasies, nalatenskapmodus, ens.
 Hulle sal uitgevoer word elke keer wanneer die databasis oopgemaak word.
 Sien die dokumentasie vir SQLite3 Multiple Ciphers vir besonderhede.</translation>
     </message>
-</context>
+  </context>
 </TS>
