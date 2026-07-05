168c50754db18436e466c163cd7f4c55f8ed5877
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 22:56:38 +0200

    New translations cellrendererimage.ts (Hungarian)
    [ci skip]

--- Plugins/CellRendererImage/translations/CellRendererImage_hu_HU.ts
+++ Plugins/CellRendererImage/translations/CellRendererImage_hu_HU.ts
@@ -1,36 +1,36 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="hu" sourcelanguage="en">
-<context>
+  <context>
     <name>CellRendererImage</name>
     <message>
-        <location filename="../cellrendererimage.ui" line="20"/>
-        <source>Preferred image cell size</source>
-        <translation>Előnyben részesített képcellaméret</translation>
+      <location filename="../cellrendererimage.ui" line="20"/>
+      <source>Preferred image cell size</source>
+      <translation>Előnyben részesített képcellaméret</translation>
     </message>
     <message>
-        <location filename="../cellrendererimage.ui" line="42"/>
-        <source>Height</source>
-        <translation>Magasság</translation>
+      <location filename="../cellrendererimage.ui" line="42"/>
+      <source>Height</source>
+      <translation>Magasság</translation>
     </message>
     <message>
-        <location filename="../cellrendererimage.ui" line="49"/>
-        <source>Width</source>
-        <translation>Szélesség</translation>
+      <location filename="../cellrendererimage.ui" line="49"/>
+      <source>Width</source>
+      <translation>Szélesség</translation>
     </message>
     <message>
-        <location filename="../cellrendererimage.ui" line="72"/>
-        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:700;&quot;&gt;Tip:&lt;/span&gt; Set these values to &lt;span style=&quot; font-weight:700;&quot;&gt;1&lt;/span&gt; if you want cells to remain as small as required for regular &lt;span style=&quot; font-weight:700;&quot;&gt;text&lt;/span&gt; rendering, without affecting the overall grid layout.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:700;&quot;&gt;Tipp:&lt;/span&gt; Állítsa ezeket az értékeket &lt;span style=&quot; font-weight:700;&quot;&gt;1&lt;/span&gt;-re, ha azt szeretné, hogy a cellák olyan kicsik maradjanak, amekkora a szokásos &lt;span style=&quot; font-weight:700;&quot;&gt;szöveg&lt;/span&gt; megjelenítéséhez szükséges, anélkül, hogy befolyásolnák a rács általános elrendezését.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+      <location filename="../cellrendererimage.ui" line="72"/>
+      <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:700;&quot;&gt;Tip:&lt;/span&gt; Set these values to &lt;span style=&quot; font-weight:700;&quot;&gt;1&lt;/span&gt; if you want cells to remain as small as required for regular &lt;span style=&quot; font-weight:700;&quot;&gt;text&lt;/span&gt; rendering, without affecting the overall grid layout.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+      <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:700;&quot;&gt;Tipp:&lt;/span&gt; Állítsa ezeket az értékeket &lt;span style=&quot; font-weight:700;&quot;&gt;1&lt;/span&gt;-re, ha azt szeretné, hogy a cellák olyan kicsik maradjanak, amekkora a szokásos &lt;span style=&quot; font-weight:700;&quot;&gt;szöveg&lt;/span&gt; megjelenítéséhez szükséges, anélkül, hogy befolyásolnák a rács általános elrendezését.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>CellRendererImagePlugin</name>
     <message>
-        <location filename="../cellrendererimageplugin.cpp" line="42"/>
-        <source>Image</source>
-        <comment>cell preview renderer name</comment>
-        <translation>Kép</translation>
+      <location filename="../cellrendererimageplugin.cpp" line="42"/>
+      <source>Image</source>
+      <comment>cell preview renderer name</comment>
+      <translation>Kép</translation>
     </message>
-</context>
+  </context>
 </TS>
