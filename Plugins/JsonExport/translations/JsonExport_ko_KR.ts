667b8293196b17ab8636ca48fd1462401e637c48
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 20:47:23 +0200

    New translations jsonexport.ts (Korean)
    [ci skip]

--- Plugins/JsonExport/translations/JsonExport_ko_KR.ts
+++ Plugins/JsonExport/translations/JsonExport_ko_KR.ts
@@ -1,22 +1,22 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="ko" sourcelanguage="en">
-<context>
+  <context>
     <name>JsonExportConfig</name>
     <message>
-        <location filename="../jsonexport.ui" line="20"/>
-        <source>Output format</source>
-        <translation>출력 형식</translation>
+      <location filename="../jsonexport.ui" line="20"/>
+      <source>Output format</source>
+      <translation>출력 형식</translation>
     </message>
     <message>
-        <location filename="../jsonexport.ui" line="26"/>
-        <source>Format document (new lines, indentation)</source>
-        <translation>문서 서식 지정(새 줄, 들여쓰기)</translation>
+      <location filename="../jsonexport.ui" line="26"/>
+      <source>Format document (new lines, indentation)</source>
+      <translation>문서 서식 지정(새 줄, 들여쓰기)</translation>
     </message>
     <message>
-        <location filename="../jsonexport.ui" line="42"/>
-        <source>Compress (everything in one line)</source>
-        <translation>압축(모든 내용을 한 줄로)</translation>
+      <location filename="../jsonexport.ui" line="42"/>
+      <source>Compress (everything in one line)</source>
+      <translation>압축(모든 내용을 한 줄로)</translation>
     </message>
-</context>
+  </context>
 </TS>
