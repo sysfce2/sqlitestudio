5a5d2e0412c9fba90827f7671c04356beaa2427a
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 20:56:17 +0200

    New translations letos.ts (Greek)
    [ci skip]

--- Letos/letos/translations/letos_el_GR.ts
+++ Letos/letos/translations/letos_el_GR.ts
@@ -1,97 +1,97 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="el" sourcelanguage="en">
-<context>
+  <context>
     <name>QObject</name>
     <message>
-        <location filename="../main.cpp" line="52"/>
-        <source>GUI interface to Letos, a SQLite manager.</source>
-        <translation>Γραφική διεπαφή για το Letos, έναν διαχειριστή SQLite.</translation>
+      <location filename="../main.cpp" line="52"/>
+      <source>GUI interface to Letos, a SQLite manager.</source>
+      <translation>Γραφική διεπαφή για το Letos, έναν διαχειριστή SQLite.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="57"/>
-        <source>Starts the application in safe mode without restoring the previous session. Use this to bypass issues caused by a corrupted session.</source>
-        <translation>Εκκινεί την εφαρμογή σε ασφαλή λειτουργία χωρίς επαναφορά της προηγούμενης συνεδρίας. Χρησιμοποιήστε το για να παρακάμψετε προβλήματα που προκαλούνται από κατεστραμμένη συνεδρία.</translation>
+      <location filename="../main.cpp" line="57"/>
+      <source>Starts the application in safe mode without restoring the previous session. Use this to bypass issues caused by a corrupted session.</source>
+      <translation>Εκκινεί την εφαρμογή σε ασφαλή λειτουργία χωρίς επαναφορά της προηγούμενης συνεδρίας. Χρησιμοποιήστε το για να παρακάμψετε προβλήματα που προκαλούνται από κατεστραμμένη συνεδρία.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="58"/>
-        <source>Do not load any plugins. Can be used alongside safe mode to further isolate potential issues.</source>
-        <translation>Να μην φορτωθούν plugins. Μπορεί να χρησιμοποιηθεί μαζί με την ασφαλή λειτουργία για περαιτέρω απομόνωση πιθανών προβλημάτων.</translation>
+      <location filename="../main.cpp" line="58"/>
+      <source>Do not load any plugins. Can be used alongside safe mode to further isolate potential issues.</source>
+      <translation>Να μην φορτωθούν plugins. Μπορεί να χρησιμοποιηθεί μαζί με την ασφαλή λειτουργία για περαιτέρω απομόνωση πιθανών προβλημάτων.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="59"/>
-        <source>Enables debug messages in console (accessible with F12).</source>
-        <translation>Ενεργοποιεί μηνύματα debug στην κονσόλα (προσβάσιμη με F12).</translation>
+      <location filename="../main.cpp" line="59"/>
+      <source>Enables debug messages in console (accessible with F12).</source>
+      <translation>Ενεργοποιεί μηνύματα debug στην κονσόλα (προσβάσιμη με F12).</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="60"/>
-        <source>Redirects debug messages into standard output (forces debug mode).</source>
-        <translation>Ανακατευθύνει τα μηνύματα debug στην τυπική έξοδο (επιβάλλει τη λειτουργία debug).</translation>
+      <location filename="../main.cpp" line="60"/>
+      <source>Redirects debug messages into standard output (forces debug mode).</source>
+      <translation>Ανακατευθύνει τα μηνύματα debug στην τυπική έξοδο (επιβάλλει τη λειτουργία debug).</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="61"/>
-        <source>Redirects debug messages into given file (forces debug mode).</source>
-        <translation>Ανακατευθύνει τα μηνύματα debug στο δοσμένο αρχείο (επιβάλλει τη λειτουργία debug).</translation>
+      <location filename="../main.cpp" line="61"/>
+      <source>Redirects debug messages into given file (forces debug mode).</source>
+      <translation>Ανακατευθύνει τα μηνύματα debug στο δοσμένο αρχείο (επιβάλλει τη λειτουργία debug).</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="61"/>
-        <source>log file</source>
-        <translation>αρχείο καταγραφής</translation>
+      <location filename="../main.cpp" line="61"/>
+      <source>log file</source>
+      <translation>αρχείο καταγραφής</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="62"/>
-        <source>Enables Lemon parser debug messages for SQL code assistant.</source>
-        <translation>Ενεργοποιεί μηνύματα debug του parser Lemon για τον βοηθό κώδικα SQL.</translation>
+      <location filename="../main.cpp" line="62"/>
+      <source>Enables Lemon parser debug messages for SQL code assistant.</source>
+      <translation>Ενεργοποιεί μηνύματα debug του parser Lemon για τον βοηθό κώδικα SQL.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="63"/>
-        <source>Enables debugging of every single SQL query being sent to any database.</source>
-        <translation>Ενεργοποιεί το debug κάθε μεμονωμένου ερωτήματος SQL που αποστέλλεται σε οποιαδήποτε βάση δεδομένων.</translation>
+      <location filename="../main.cpp" line="63"/>
+      <source>Enables debugging of every single SQL query being sent to any database.</source>
+      <translation>Ενεργοποιεί το debug κάθε μεμονωμένου ερωτήματος SQL που αποστέλλεται σε οποιαδήποτε βάση δεδομένων.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="64"/>
-        <source>Limits SQL query messages to only the given &lt;database&gt;.</source>
-        <translation>Περιορίζει τα μηνύματα ερωτημάτων SQL μόνο στη δοσμένη &lt;βάση δεδομένων&gt;.</translation>
+      <location filename="../main.cpp" line="64"/>
+      <source>Limits SQL query messages to only the given &lt;database&gt;.</source>
+      <translation>Περιορίζει τα μηνύματα ερωτημάτων SQL μόνο στη δοσμένη &lt;βάση δεδομένων&gt;.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="64"/>
-        <source>database</source>
-        <translation>βάση δεδομένων</translation>
+      <location filename="../main.cpp" line="64"/>
+      <source>database</source>
+      <translation>βάση δεδομένων</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="56"/>
-        <source>Points to the master configuration file. Read manual at wiki page for more details.</source>
-        <translation>Δείχνει στο κύριο αρχείο ρυθμίσεων. Διαβάστε το εγχειρίδιο στη σελίδα wiki για περισσότερες λεπτομέρειες.</translation>
+      <location filename="../main.cpp" line="56"/>
+      <source>Points to the master configuration file. Read manual at wiki page for more details.</source>
+      <translation>Δείχνει στο κύριο αρχείο ρυθμίσεων. Διαβάστε το εγχειρίδιο στη σελίδα wiki για περισσότερες λεπτομέρειες.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="56"/>
-        <source>settings file</source>
-        <translation>αρχείο ρυθμίσεων</translation>
+      <location filename="../main.cpp" line="56"/>
+      <source>settings file</source>
+      <translation>αρχείο ρυθμίσεων</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="65"/>
-        <source>Enables debugging of Letos&apos;s query executor.</source>
-        <translation>Ενεργοποιεί το debug του εκτελεστή ερωτημάτων του Letos&apos;s.</translation>
+      <location filename="../main.cpp" line="65"/>
+      <source>Enables debugging of Letos&apos;s query executor.</source>
+      <translation>Ενεργοποιεί το debug του εκτελεστή ερωτημάτων του Letos&apos;s.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="66"/>
-        <source>Lists plugins installed in the Letos and quits.</source>
-        <translation>Παραθέτει τα plugins που είναι εγκατεστημένα στο Letos και τερματίζει.</translation>
+      <location filename="../main.cpp" line="66"/>
+      <source>Lists plugins installed in the Letos and quits.</source>
+      <translation>Παραθέτει τα plugins που είναι εγκατεστημένα στο Letos και τερματίζει.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="79"/>
-        <source>file</source>
-        <translation>αρχείο</translation>
+      <location filename="../main.cpp" line="79"/>
+      <source>file</source>
+      <translation>αρχείο</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="79"/>
-        <source>Database file to open</source>
-        <translation>Αρχείο βάσης δεδομένων για άνοιγμα</translation>
+      <location filename="../main.cpp" line="79"/>
+      <source>Database file to open</source>
+      <translation>Αρχείο βάσης δεδομένων για άνοιγμα</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="163"/>
-        <source>Select configuration directory</source>
-        <translation>Επιλογή καταλόγου ρυθμίσεων</translation>
+      <location filename="../main.cpp" line="163"/>
+      <source>Select configuration directory</source>
+      <translation>Επιλογή καταλόγου ρυθμίσεων</translation>
     </message>
-</context>
+  </context>
 </TS>
