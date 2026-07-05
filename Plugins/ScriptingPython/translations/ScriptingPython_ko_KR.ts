bb11255e3ffcb485b75f535a5db54d524004aed2
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 13:20:30 +0200

    New translations scriptingpython.ts (Korean)
    [ci skip]

--- Plugins/ScriptingPython/translations/ScriptingPython_ko_KR.ts
+++ Plugins/ScriptingPython/translations/ScriptingPython_ko_KR.ts
@@ -1,50 +1,50 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="ko" sourcelanguage="en">
-<context>
+  <context>
     <name>QObject</name>
     <message>
-        <location filename="../scriptingpython.cpp" line="730"/>
-        <source>Invalid use of %1 function. Expected %2 arguments, but got %3.</source>
-        <translation>%1 함수의 사용이 잘못되었습니다. %2개의 인수가 필요하지만 %3개가 제공되었습니다.</translation>
+      <location filename="../scriptingpython.cpp" line="730"/>
+      <source>Invalid use of %1 function. Expected %2 arguments, but got %3.</source>
+      <translation>%1 함수의 사용이 잘못되었습니다. %2개의 인수가 필요하지만 %3개가 제공되었습니다.</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="739"/>
-        <source>Unknown error from function %1.</source>
-        <translation>%1 함수에서 알 수 없는 오류가 발생했습니다.</translation>
+      <location filename="../scriptingpython.cpp" line="739"/>
+      <source>Unknown error from function %1.</source>
+      <translation>%1 함수에서 알 수 없는 오류가 발생했습니다.</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="780"/>
-        <source>Could not calculate string representation of the Python object passed as argument to the function %1.</source>
-        <translation>%1 함수에 인수로 전달된 Python 객체의 문자열 표현을 계산할 수 없습니다.</translation>
+      <location filename="../scriptingpython.cpp" line="780"/>
+      <source>Could not calculate string representation of the Python object passed as argument to the function %1.</source>
+      <translation>%1 함수에 인수로 전달된 Python 객체의 문자열 표현을 계산할 수 없습니다.</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="802"/>
-        <source>Could not find execution context for function %1. This is a bug of Python plugin. Please report it.</source>
-        <translation>%1 함수의 실행 컨텍스트를 찾을 수 없습니다. 이것은 Python 플러그인의 버그입니다. 보고해 주십시오.</translation>
+      <location filename="../scriptingpython.cpp" line="802"/>
+      <source>Could not find execution context for function %1. This is a bug of Python plugin. Please report it.</source>
+      <translation>%1 함수의 실행 컨텍스트를 찾을 수 없습니다. 이것은 Python 플러그인의 버그입니다. 보고해 주십시오.</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="827"/>
-        <source>Error from Python function %1: %2</source>
-        <translation>Python 함수 %1의 오류: %2</translation>
+      <location filename="../scriptingpython.cpp" line="827"/>
+      <source>Error from Python function %1: %2</source>
+      <translation>Python 함수 %1의 오류: %2</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>ScriptingPython</name>
     <message>
-        <location filename="../scriptingpython.ui" line="20"/>
-        <source>Python library path</source>
-        <translation>Python 라이브러리 경로</translation>
+      <location filename="../scriptingpython.ui" line="20"/>
+      <source>Python library path</source>
+      <translation>Python 라이브러리 경로</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.ui" line="29"/>
-        <source>Choose a Python library file</source>
-        <translation>Python 라이브러리 파일 선택</translation>
+      <location filename="../scriptingpython.ui" line="29"/>
+      <source>Choose a Python library file</source>
+      <translation>Python 라이브러리 파일 선택</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="370"/>
-        <source>The plugin is not configured properly.</source>
-        <translation>플러그인이 올바르게 구성되지 않았습니다.</translation>
+      <location filename="../scriptingpython.cpp" line="370"/>
+      <source>The plugin is not configured properly.</source>
+      <translation>플러그인이 올바르게 구성되지 않았습니다.</translation>
     </message>
-</context>
+  </context>
 </TS>
