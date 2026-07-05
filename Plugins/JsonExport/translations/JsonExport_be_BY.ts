96f713e931620794bcf62f30f32b98d444633c63
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 20:46:57 +0200

    New translations jsonexport.ts (Belarusian)
    [ci skip]

--- Plugins/JsonExport/translations/JsonExport_be_BY.ts
+++ Plugins/JsonExport/translations/JsonExport_be_BY.ts
@@ -1,22 +1,22 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="be" sourcelanguage="en">
-<context>
+  <context>
     <name>JsonExportConfig</name>
     <message>
-        <location filename="../jsonexport.ui" line="20"/>
-        <source>Output format</source>
-        <translation>Фармат вываду</translation>
+      <location filename="../jsonexport.ui" line="20"/>
+      <source>Output format</source>
+      <translation>Фармат вываду</translation>
     </message>
     <message>
-        <location filename="../jsonexport.ui" line="26"/>
-        <source>Format document (new lines, indentation)</source>
-        <translation>Фарматаваць дакумент (новыя радкі, водступы)</translation>
+      <location filename="../jsonexport.ui" line="26"/>
+      <source>Format document (new lines, indentation)</source>
+      <translation>Фарматаваць дакумент (новыя радкі, водступы)</translation>
     </message>
     <message>
-        <location filename="../jsonexport.ui" line="42"/>
-        <source>Compress (everything in one line)</source>
-        <translation>Сціснуць (усё ў адзін радок)</translation>
+      <location filename="../jsonexport.ui" line="42"/>
+      <source>Compress (everything in one line)</source>
+      <translation>Сціснуць (усё ў адзін радок)</translation>
     </message>
-</context>
+  </context>
 </TS>
