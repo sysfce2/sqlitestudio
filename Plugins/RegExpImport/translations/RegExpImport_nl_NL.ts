85ca66798bdeeccbcec33e9b4649b5f86749b11c
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 13:16:47 +0200

    New translations regexpimport.ts (Dutch)
    [ci skip]

--- Plugins/RegExpImport/translations/RegExpImport_nl_NL.ts
+++ Plugins/RegExpImport/translations/RegExpImport_nl_NL.ts
@@ -1,86 +1,86 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="nl" sourcelanguage="en">
-<context>
+  <context>
     <name>RegExpImport</name>
     <message>
-        <location filename="../regexpimport.cpp" line="37"/>
-        <source>Text files (*.txt);;All files (*)</source>
-        <translation>Tekstbestanden (*.txt);;Alle bestanden (*)</translation>
+      <location filename="../regexpimport.cpp" line="37"/>
+      <source>Text files (*.txt);;All files (*)</source>
+      <translation>Tekstbestanden (*.txt);;Alle bestanden (*)</translation>
     </message>
     <message>
-        <location filename="../regexpimport.cpp" line="53"/>
-        <source>Cannot read file %1</source>
-        <translation>Kan bestand %1 niet lezen</translation>
+      <location filename="../regexpimport.cpp" line="53"/>
+      <source>Cannot read file %1</source>
+      <translation>Kan bestand %1 niet lezen</translation>
     </message>
     <message>
-        <location filename="../regexpimport.cpp" line="161"/>
-        <source>Enter the regular expression pattern.</source>
-        <translation>Voer het patroon voor de reguliere expressie in.</translation>
+      <location filename="../regexpimport.cpp" line="161"/>
+      <source>Enter the regular expression pattern.</source>
+      <translation>Voer het patroon voor de reguliere expressie in.</translation>
     </message>
     <message>
-        <location filename="../regexpimport.cpp" line="169"/>
-        <source>Invalid pattern: %1</source>
-        <translation>Ongeldig patroon: %1</translation>
+      <location filename="../regexpimport.cpp" line="169"/>
+      <source>Invalid pattern: %1</source>
+      <translation>Ongeldig patroon: %1</translation>
     </message>
     <message>
-        <location filename="../regexpimport.cpp" line="189"/>
-        <source>Requested capture index %1 is out of range.</source>
-        <translation>De gevraagde capture-index %1 valt buiten het bereik.</translation>
+      <location filename="../regexpimport.cpp" line="189"/>
+      <source>Requested capture index %1 is out of range.</source>
+      <translation>De gevraagde capture-index %1 valt buiten het bereik.</translation>
     </message>
     <message>
-        <location filename="../regexpimport.cpp" line="196"/>
-        <source>&lt;p&gt;Requested capture group name &apos;%1&apos;, but it&apos;s not defined in the pattern: &lt;pre&gt;%2&lt;/pre&gt;&lt;/p&gt;</source>
-        <translation>&lt;p&gt;Gevraagde naam van capturegroep &apos;%1&apos;, maar deze is niet gedefinieerd in het patroon: &lt;pre&gt;%2&lt;/pre&gt;&lt;/p&gt;</translation>
+      <location filename="../regexpimport.cpp" line="196"/>
+      <source>&lt;p&gt;Requested capture group name &apos;%1&apos;, but it&apos;s not defined in the pattern: &lt;pre&gt;%2&lt;/pre&gt;&lt;/p&gt;</source>
+      <translation>&lt;p&gt;Gevraagde naam van capturegroep &apos;%1&apos;, maar deze is niet gedefinieerd in het patroon: &lt;pre&gt;%2&lt;/pre&gt;&lt;/p&gt;</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>RegExpImportConfig</name>
     <message>
-        <location filename="../regexpimport.ui" line="20"/>
-        <source>Capture groups</source>
-        <translation>Capturegroepen</translation>
+      <location filename="../regexpimport.ui" line="20"/>
+      <source>Capture groups</source>
+      <translation>Capturegroepen</translation>
     </message>
     <message>
-        <location filename="../regexpimport.ui" line="26"/>
-        <source>Treat all RegExp capture groups as columns</source>
-        <translation>Alle RegExp-capturegroepen als kolommen behandelen</translation>
+      <location filename="../regexpimport.ui" line="26"/>
+      <source>Treat all RegExp capture groups as columns</source>
+      <translation>Alle RegExp-capturegroepen als kolommen behandelen</translation>
     </message>
     <message>
-        <location filename="../regexpimport.ui" line="39"/>
-        <source>Import only following groups:</source>
-        <translation>Alleen de volgende groepen importeren:</translation>
+      <location filename="../regexpimport.ui" line="39"/>
+      <source>Import only following groups:</source>
+      <translation>Alleen de volgende groepen importeren:</translation>
     </message>
     <message>
-        <location filename="../regexpimport.ui" line="52"/>
-        <source>&lt;p&gt;Enter comma separated list of capture group indexes. The 0 index refers to the entire matched string.&lt;/p&gt;
+      <location filename="../regexpimport.ui" line="52"/>
+      <source>&lt;p&gt;Enter comma separated list of capture group indexes. The 0 index refers to the entire matched string.&lt;/p&gt;
 &lt;p&gt;If you used named groups in the pattern, you can use names instead of indexes. You can mix indexes and names in this list.&lt;/p&gt;</source>
-        <translation>&lt;p&gt;Voer een door komma&apos;s gescheiden lijst met capturegroepindexen in. De index 0 verwijst naar de volledige overeenkomende tekenreeks.&lt;/p&gt;
+      <translation>&lt;p&gt;Voer een door komma&apos;s gescheiden lijst met capturegroepindexen in. De index 0 verwijst naar de volledige overeenkomende tekenreeks.&lt;/p&gt;
 &lt;p&gt;Als u benoemde groepen in het patroon hebt gebruikt, kunt u namen gebruiken in plaats van indexen. U kunt indexen en namen in deze lijst combineren.&lt;/p&gt;</translation>
     </message>
     <message>
-        <location filename="../regexpimport.ui" line="56"/>
-        <source>Example: 1, 3, 4</source>
-        <translation>Voorbeeld: 1, 3, 4</translation>
+      <location filename="../regexpimport.ui" line="56"/>
+      <source>Example: 1, 3, 4</source>
+      <translation>Voorbeeld: 1, 3, 4</translation>
     </message>
     <message>
-        <location filename="../regexpimport.ui" line="69"/>
-        <source>Pattern:</source>
-        <translation>Patroon:</translation>
+      <location filename="../regexpimport.ui" line="69"/>
+      <source>Pattern:</source>
+      <translation>Patroon:</translation>
     </message>
     <message>
-        <location filename="../regexpimport.ui" line="76"/>
-        <source>&lt;p&gt;Use Regular Expression groups to enclose parts of the expression that you want to import. If you want to use a group, that you don&apos;t want to import, then use &quot;import only following groups&quot; option below.
+      <location filename="../regexpimport.ui" line="76"/>
+      <source>&lt;p&gt;Use Regular Expression groups to enclose parts of the expression that you want to import. If you want to use a group, that you don&apos;t want to import, then use &quot;import only following groups&quot; option below.
 
 You can use named groups and refer to them in group list below. To name a group use: &lt;pre&gt;(?&amp;lt;myGroupName&amp;gt;\s+\d+\s+)&lt;/pre&gt;&lt;/p&gt;</source>
-        <translation>&lt;p&gt;Gebruik reguliere-expressiegroepen om delen van de expressie die u wilt importeren te omsluiten. Als u een groep wilt gebruiken die u niet wilt importeren, gebruik dan de optie &quot;alleen de volgende groepen importeren&quot; hieronder.
+      <translation>&lt;p&gt;Gebruik reguliere-expressiegroepen om delen van de expressie die u wilt importeren te omsluiten. Als u een groep wilt gebruiken die u niet wilt importeren, gebruik dan de optie &quot;alleen de volgende groepen importeren&quot; hieronder.
 
 U kunt benoemde groepen gebruiken en ernaar verwijzen in de groepslijst hieronder. Gebruik om een groep een naam te geven: &lt;pre&gt;(?&amp;lt;myGroupName&amp;gt;\s+\d+\s+)&lt;/pre&gt;&lt;/p&gt;</translation>
     </message>
     <message>
-        <location filename="../regexpimport.ui" line="81"/>
-        <source>Example: (\d+)\s+((\d+)\w+)\s+(\w+)</source>
-        <translation>Voorbeeld: (\d+)\s+((\d+)\w+)\s+(\w+)</translation>
+      <location filename="../regexpimport.ui" line="81"/>
+      <source>Example: (\d+)\s+((\d+)\w+)\s+(\w+)</source>
+      <translation>Voorbeeld: (\d+)\s+((\d+)\w+)\s+(\w+)</translation>
     </message>
-</context>
+  </context>
 </TS>
