bb73202b303332b40557cbf4d0483dc8f788f72c
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 20:56:31 +0200

    New translations cellrendererimage.ts (Greek)
    [ci skip]

--- Plugins/CellRendererImage/translations/CellRendererImage_el_GR.ts
+++ Plugins/CellRendererImage/translations/CellRendererImage_el_GR.ts
@@ -1,36 +1,36 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="el" sourcelanguage="en">
-<context>
+  <context>
     <name>CellRendererImage</name>
     <message>
-        <location filename="../cellrendererimage.ui" line="20"/>
-        <source>Preferred image cell size</source>
-        <translation>Προτιμώμενο μέγεθος κελιού εικόνας</translation>
+      <location filename="../cellrendererimage.ui" line="20"/>
+      <source>Preferred image cell size</source>
+      <translation>Προτιμώμενο μέγεθος κελιού εικόνας</translation>
     </message>
     <message>
-        <location filename="../cellrendererimage.ui" line="42"/>
-        <source>Height</source>
-        <translation>Ύψος</translation>
+      <location filename="../cellrendererimage.ui" line="42"/>
+      <source>Height</source>
+      <translation>Ύψος</translation>
     </message>
     <message>
-        <location filename="../cellrendererimage.ui" line="49"/>
-        <source>Width</source>
-        <translation>Πλάτος</translation>
+      <location filename="../cellrendererimage.ui" line="49"/>
+      <source>Width</source>
+      <translation>Πλάτος</translation>
     </message>
     <message>
-        <location filename="../cellrendererimage.ui" line="72"/>
-        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:700;&quot;&gt;Tip:&lt;/span&gt; Set these values to &lt;span style=&quot; font-weight:700;&quot;&gt;1&lt;/span&gt; if you want cells to remain as small as required for regular &lt;span style=&quot; font-weight:700;&quot;&gt;text&lt;/span&gt; rendering, without affecting the overall grid layout.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:700;&quot;&gt;Συμβουλή:&lt;/span&gt; Ορίστε αυτές τις τιμές σε &lt;span style=&quot; font-weight:700;&quot;&gt;1&lt;/span&gt; αν θέλετε τα κελιά να παραμείνουν τόσο μικρά όσο απαιτείται για την κανονική απόδοση &lt;span style=&quot; font-weight:700;&quot;&gt;κειμένου&lt;/span&gt;, χωρίς να επηρεάζεται η συνολική διάταξη του πλέγματος.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+      <location filename="../cellrendererimage.ui" line="72"/>
+      <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:700;&quot;&gt;Tip:&lt;/span&gt; Set these values to &lt;span style=&quot; font-weight:700;&quot;&gt;1&lt;/span&gt; if you want cells to remain as small as required for regular &lt;span style=&quot; font-weight:700;&quot;&gt;text&lt;/span&gt; rendering, without affecting the overall grid layout.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+      <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:700;&quot;&gt;Συμβουλή:&lt;/span&gt; Ορίστε αυτές τις τιμές σε &lt;span style=&quot; font-weight:700;&quot;&gt;1&lt;/span&gt; αν θέλετε τα κελιά να παραμείνουν τόσο μικρά όσο απαιτείται για την κανονική απόδοση &lt;span style=&quot; font-weight:700;&quot;&gt;κειμένου&lt;/span&gt;, χωρίς να επηρεάζεται η συνολική διάταξη του πλέγματος.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>CellRendererImagePlugin</name>
     <message>
-        <location filename="../cellrendererimageplugin.cpp" line="42"/>
-        <source>Image</source>
-        <comment>cell preview renderer name</comment>
-        <translation>Εικόνα</translation>
+      <location filename="../cellrendererimageplugin.cpp" line="42"/>
+      <source>Image</source>
+      <comment>cell preview renderer name</comment>
+      <translation>Εικόνα</translation>
     </message>
-</context>
+  </context>
 </TS>
