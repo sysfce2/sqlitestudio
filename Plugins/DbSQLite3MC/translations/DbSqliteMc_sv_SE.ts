bc7fb2ebd1901baf6d2e694d59445010f9233f6b
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 23:00:51 +0200

    New translations dbsqlitemc.ts (Swedish)
    [ci skip]

--- Plugins/DbSQLite3MC/translations/DbSqliteMc_sv_SE.ts
+++ Plugins/DbSQLite3MC/translations/DbSqliteMc_sv_SE.ts
@@ -1,46 +1,46 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="sv-SE" sourcelanguage="en">
-<context>
+  <context>
     <name>DbSqliteMc</name>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="42"/>
-        <source>Password (key)</source>
-        <translation>Lösenord (nyckel)</translation>
+      <location filename="../dbsqlitemc.cpp" line="42"/>
+      <source>Password (key)</source>
+      <translation>Lösenord (nyckel)</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="43"/>
-        <source>Leave empty to create or connect to decrypted database.</source>
-        <translation>Lämna tomt för att skapa eller ansluta till en dekrypterad databas.</translation>
+      <location filename="../dbsqlitemc.cpp" line="43"/>
+      <source>Leave empty to create or connect to decrypted database.</source>
+      <translation>Lämna tomt för att skapa eller ansluta till en dekrypterad databas.</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="44"/>
-        <source>Encryption password</source>
-        <translation>Krypteringslösenord</translation>
+      <location filename="../dbsqlitemc.cpp" line="44"/>
+      <source>Encryption password</source>
+      <translation>Krypteringslösenord</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="50"/>
-        <source>Cipher</source>
-        <translation>Chiffer</translation>
+      <location filename="../dbsqlitemc.cpp" line="50"/>
+      <source>Cipher</source>
+      <translation>Chiffer</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="51"/>
-        <source>Cipher determines encryption algorithm used to encrypt the database.</source>
-        <translation>Chiffer avgör vilken krypteringsalgoritm som används för att kryptera databasen.</translation>
+      <location filename="../dbsqlitemc.cpp" line="51"/>
+      <source>Cipher determines encryption algorithm used to encrypt the database.</source>
+      <translation>Chiffer avgör vilken krypteringsalgoritm som används för att kryptera databasen.</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="61"/>
-        <source>Cipher configuration (optional)</source>
-        <translation>Chifferkonfiguration (valfritt)</translation>
+      <location filename="../dbsqlitemc.cpp" line="61"/>
+      <source>Cipher configuration (optional)</source>
+      <translation>Chifferkonfiguration (valfritt)</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="62"/>
-        <source>PRAGMA statements to customize SQLite3 Multiple Ciphers configuration, such as KDF iterations, legacy mode, etc.
+      <location filename="../dbsqlitemc.cpp" line="62"/>
+      <source>PRAGMA statements to customize SQLite3 Multiple Ciphers configuration, such as KDF iterations, legacy mode, etc.
 They will be executed upon each opening of the database.
 See documentation for SQLite3 Multiple Ciphers for details.</source>
-        <translation>PRAGMA-satser för att anpassa konfigurationen av SQLite3 Multiple Ciphers, till exempel KDF-iterationer, äldre läge osv.
+      <translation>PRAGMA-satser för att anpassa konfigurationen av SQLite3 Multiple Ciphers, till exempel KDF-iterationer, äldre läge osv.
 De kommer att köras varje gång databasen öppnas.
 Se dokumentationen för SQLite3 Multiple Ciphers för mer information.</translation>
     </message>
-</context>
+  </context>
 </TS>
