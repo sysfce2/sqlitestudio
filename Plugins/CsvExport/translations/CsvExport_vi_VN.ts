6913f9250e2066ab260557e407ec1b62a2e01e77
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 23:03:12 +0200

    New translations csvexport.ts (Vietnamese)
    [ci skip]

--- Plugins/CsvExport/translations/CsvExport_vi_VN.ts
+++ Plugins/CsvExport/translations/CsvExport_vi_VN.ts
@@ -1,57 +1,57 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="vi" sourcelanguage="en">
-<context>
+  <context>
     <name>CsvExport</name>
     <message>
-        <location filename="../CsvExport.ui" line="20"/>
-        <source>Column names in first row</source>
-        <translation>Tên cột ở hàng đầu tiên</translation>
+      <location filename="../CsvExport.ui" line="20"/>
+      <source>Column names in first row</source>
+      <translation>Tên cột ở hàng đầu tiên</translation>
     </message>
     <message>
-        <location filename="../CsvExport.ui" line="30"/>
-        <source>Column separator:</source>
-        <translation>Dấu phân tách cột:</translation>
+      <location filename="../CsvExport.ui" line="30"/>
+      <source>Column separator:</source>
+      <translation>Dấu phân tách cột:</translation>
     </message>
     <message>
-        <location filename="../CsvExport.ui" line="41"/>
-        <source>, (comma)</source>
-        <translation>, (dấu phẩy)</translation>
+      <location filename="../CsvExport.ui" line="41"/>
+      <source>, (comma)</source>
+      <translation>, (dấu phẩy)</translation>
     </message>
     <message>
-        <location filename="../CsvExport.ui" line="46"/>
-        <source>; (semicolon)</source>
-        <translation>; (dấu chấm phẩy)</translation>
+      <location filename="../CsvExport.ui" line="46"/>
+      <source>; (semicolon)</source>
+      <translation>; (dấu chấm phẩy)</translation>
     </message>
     <message>
-        <location filename="../CsvExport.ui" line="51"/>
-        <source>\t (tab)</source>
-        <translation>\t (tab)</translation>
+      <location filename="../CsvExport.ui" line="51"/>
+      <source>\t (tab)</source>
+      <translation>\t (tab)</translation>
     </message>
     <message>
-        <location filename="../CsvExport.ui" line="56"/>
-        <source>  (whitespace)</source>
-        <translation>  (khoảng trắng)</translation>
+      <location filename="../CsvExport.ui" line="56"/>
+      <source>  (whitespace)</source>
+      <translation>  (khoảng trắng)</translation>
     </message>
     <message>
-        <location filename="../CsvExport.ui" line="61"/>
-        <source>Custom:</source>
-        <translation>Tùy chỉnh:</translation>
+      <location filename="../CsvExport.ui" line="61"/>
+      <source>Custom:</source>
+      <translation>Tùy chỉnh:</translation>
     </message>
     <message>
-        <location filename="../CsvExport.ui" line="82"/>
-        <source>Export NULL values as:</source>
-        <translation>Xuất giá trị NULL dưới dạng:</translation>
+      <location filename="../CsvExport.ui" line="82"/>
+      <source>Export NULL values as:</source>
+      <translation>Xuất giá trị NULL dưới dạng:</translation>
     </message>
     <message>
-        <location filename="../CsvExport.ui" line="89"/>
-        <source>Empty string</source>
-        <translation>Chuỗi rỗng</translation>
+      <location filename="../CsvExport.ui" line="89"/>
+      <source>Empty string</source>
+      <translation>Chuỗi rỗng</translation>
     </message>
     <message>
-        <location filename="../csvexport.cpp" line="41"/>
-        <source>Enter the custom separator character.</source>
-        <translation>Nhập ký tự phân tách tùy chỉnh.</translation>
+      <location filename="../csvexport.cpp" line="41"/>
+      <source>Enter the custom separator character.</source>
+      <translation>Nhập ký tự phân tách tùy chỉnh.</translation>
     </message>
-</context>
+  </context>
 </TS>
