1a9629ca9ede5dbcab67de1ae69c4c9d06ecced5
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 13:19:03 +0200

    New translations sqlformattersimple.ts (Arabic)
    [ci skip]

--- Plugins/SqlFormatterSimple/translations/SqlFormatterSimple_ar_SA.ts
+++ Plugins/SqlFormatterSimple/translations/SqlFormatterSimple_ar_SA.ts
@@ -1,17 +1,17 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="ar" sourcelanguage="en">
-<context>
+  <context>
     <name>SqlFormatterSimplePlugin</name>
     <message>
-        <location filename="../SqlFormatterSimple.ui" line="20"/>
-        <source>Upper case keywords</source>
-        <translation>تحويل الكلمات المفتاحية إلى أحرف كبيرة</translation>
+      <location filename="../SqlFormatterSimple.ui" line="20"/>
+      <source>Upper case keywords</source>
+      <translation>تحويل الكلمات المفتاحية إلى أحرف كبيرة</translation>
     </message>
     <message>
-        <location filename="../SqlFormatterSimple.ui" line="30"/>
-        <source>Reduce multiple whitespaces to single whitespace</source>
-        <translation>تقليل المسافات البيضاء المتعددة إلى مسافة بيضاء واحدة</translation>
+      <location filename="../SqlFormatterSimple.ui" line="30"/>
+      <source>Reduce multiple whitespaces to single whitespace</source>
+      <translation>تقليل المسافات البيضاء المتعددة إلى مسافة بيضاء واحدة</translation>
     </message>
-</context>
+  </context>
 </TS>
