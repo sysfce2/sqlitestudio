1fb172efb2033631af7d5b5722b209ebf130fb4b
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 13:14:04 +0200

    New translations jsonexport.ts (Danish)
    [ci skip]

--- Plugins/JsonExport/translations/JsonExport_da_DK.ts
+++ Plugins/JsonExport/translations/JsonExport_da_DK.ts
@@ -1,22 +1,22 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="da" sourcelanguage="en">
-<context>
+  <context>
     <name>JsonExportConfig</name>
     <message>
-        <location filename="../jsonexport.ui" line="20"/>
-        <source>Output format</source>
-        <translation>Outputformat</translation>
+      <location filename="../jsonexport.ui" line="20"/>
+      <source>Output format</source>
+      <translation>Outputformat</translation>
     </message>
     <message>
-        <location filename="../jsonexport.ui" line="26"/>
-        <source>Format document (new lines, indentation)</source>
-        <translation>Formatér dokument (nye linjer, indrykning)</translation>
+      <location filename="../jsonexport.ui" line="26"/>
+      <source>Format document (new lines, indentation)</source>
+      <translation>Formatér dokument (nye linjer, indrykning)</translation>
     </message>
     <message>
-        <location filename="../jsonexport.ui" line="42"/>
-        <source>Compress (everything in one line)</source>
-        <translation>Komprimer (alt på én linje)</translation>
+      <location filename="../jsonexport.ui" line="42"/>
+      <source>Compress (everything in one line)</source>
+      <translation>Komprimer (alt på én linje)</translation>
     </message>
-</context>
+  </context>
 </TS>
