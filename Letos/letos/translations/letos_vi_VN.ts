5b0a159fe7ca883462ac057162c88f632757d7ab
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 23:03:35 +0200

    New translations letos.ts (Vietnamese)
    [ci skip]

--- Letos/letos/translations/letos_vi_VN.ts
+++ Letos/letos/translations/letos_vi_VN.ts
@@ -1,97 +1,97 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="vi" sourcelanguage="en">
-<context>
+  <context>
     <name>QObject</name>
     <message>
-        <location filename="../main.cpp" line="52"/>
-        <source>GUI interface to Letos, a SQLite manager.</source>
-        <translation>Giao diện đồ họa cho Letos, một trình quản lý SQLite.</translation>
+      <location filename="../main.cpp" line="52"/>
+      <source>GUI interface to Letos, a SQLite manager.</source>
+      <translation>Giao diện đồ họa cho Letos, một trình quản lý SQLite.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="57"/>
-        <source>Starts the application in safe mode without restoring the previous session. Use this to bypass issues caused by a corrupted session.</source>
-        <translation>Khởi động ứng dụng ở chế độ an toàn mà không khôi phục phiên trước đó. Dùng tùy chọn này để bỏ qua các sự cố do phiên bị hỏng gây ra.</translation>
+      <location filename="../main.cpp" line="57"/>
+      <source>Starts the application in safe mode without restoring the previous session. Use this to bypass issues caused by a corrupted session.</source>
+      <translation>Khởi động ứng dụng ở chế độ an toàn mà không khôi phục phiên trước đó. Dùng tùy chọn này để bỏ qua các sự cố do phiên bị hỏng gây ra.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="58"/>
-        <source>Do not load any plugins. Can be used alongside safe mode to further isolate potential issues.</source>
-        <translation>Không tải bất kỳ plugin nào. Có thể dùng cùng với chế độ an toàn để cô lập thêm các sự cố tiềm ẩn.</translation>
+      <location filename="../main.cpp" line="58"/>
+      <source>Do not load any plugins. Can be used alongside safe mode to further isolate potential issues.</source>
+      <translation>Không tải bất kỳ plugin nào. Có thể dùng cùng với chế độ an toàn để cô lập thêm các sự cố tiềm ẩn.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="59"/>
-        <source>Enables debug messages in console (accessible with F12).</source>
-        <translation>Bật thông báo debug trong bảng điều khiển (có thể truy cập bằng F12).</translation>
+      <location filename="../main.cpp" line="59"/>
+      <source>Enables debug messages in console (accessible with F12).</source>
+      <translation>Bật thông báo debug trong bảng điều khiển (có thể truy cập bằng F12).</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="60"/>
-        <source>Redirects debug messages into standard output (forces debug mode).</source>
-        <translation>Chuyển hướng thông báo debug vào đầu ra chuẩn (buộc bật chế độ debug).</translation>
+      <location filename="../main.cpp" line="60"/>
+      <source>Redirects debug messages into standard output (forces debug mode).</source>
+      <translation>Chuyển hướng thông báo debug vào đầu ra chuẩn (buộc bật chế độ debug).</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="61"/>
-        <source>Redirects debug messages into given file (forces debug mode).</source>
-        <translation>Chuyển hướng thông báo debug vào tệp đã cho (buộc bật chế độ debug).</translation>
+      <location filename="../main.cpp" line="61"/>
+      <source>Redirects debug messages into given file (forces debug mode).</source>
+      <translation>Chuyển hướng thông báo debug vào tệp đã cho (buộc bật chế độ debug).</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="61"/>
-        <source>log file</source>
-        <translation>tệp nhật ký</translation>
+      <location filename="../main.cpp" line="61"/>
+      <source>log file</source>
+      <translation>tệp nhật ký</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="62"/>
-        <source>Enables Lemon parser debug messages for SQL code assistant.</source>
-        <translation>Bật thông báo debug của parser Lemon cho trợ lý mã SQL.</translation>
+      <location filename="../main.cpp" line="62"/>
+      <source>Enables Lemon parser debug messages for SQL code assistant.</source>
+      <translation>Bật thông báo debug của parser Lemon cho trợ lý mã SQL.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="63"/>
-        <source>Enables debugging of every single SQL query being sent to any database.</source>
-        <translation>Bật debug cho từng truy vấn SQL được gửi đến bất kỳ cơ sở dữ liệu nào.</translation>
+      <location filename="../main.cpp" line="63"/>
+      <source>Enables debugging of every single SQL query being sent to any database.</source>
+      <translation>Bật debug cho từng truy vấn SQL được gửi đến bất kỳ cơ sở dữ liệu nào.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="64"/>
-        <source>Limits SQL query messages to only the given &lt;database&gt;.</source>
-        <translation>Chỉ giới hạn thông báo truy vấn SQL cho &lt;cơ sở dữ liệu&gt; đã cho.</translation>
+      <location filename="../main.cpp" line="64"/>
+      <source>Limits SQL query messages to only the given &lt;database&gt;.</source>
+      <translation>Chỉ giới hạn thông báo truy vấn SQL cho &lt;cơ sở dữ liệu&gt; đã cho.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="64"/>
-        <source>database</source>
-        <translation>cơ sở dữ liệu</translation>
+      <location filename="../main.cpp" line="64"/>
+      <source>database</source>
+      <translation>cơ sở dữ liệu</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="56"/>
-        <source>Points to the master configuration file. Read manual at wiki page for more details.</source>
-        <translation>Trỏ đến tệp cấu hình chính. Đọc hướng dẫn trên trang wiki để biết thêm chi tiết.</translation>
+      <location filename="../main.cpp" line="56"/>
+      <source>Points to the master configuration file. Read manual at wiki page for more details.</source>
+      <translation>Trỏ đến tệp cấu hình chính. Đọc hướng dẫn trên trang wiki để biết thêm chi tiết.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="56"/>
-        <source>settings file</source>
-        <translation>tệp cài đặt</translation>
+      <location filename="../main.cpp" line="56"/>
+      <source>settings file</source>
+      <translation>tệp cài đặt</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="65"/>
-        <source>Enables debugging of Letos&apos;s query executor.</source>
-        <translation>Bật debug cho bộ thực thi truy vấn của Letos&apos;.</translation>
+      <location filename="../main.cpp" line="65"/>
+      <source>Enables debugging of Letos&apos;s query executor.</source>
+      <translation>Bật debug cho bộ thực thi truy vấn của Letos&apos;.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="66"/>
-        <source>Lists plugins installed in the Letos and quits.</source>
-        <translation>Liệt kê các plugin đã cài đặt trong Letos rồi thoát.</translation>
+      <location filename="../main.cpp" line="66"/>
+      <source>Lists plugins installed in the Letos and quits.</source>
+      <translation>Liệt kê các plugin đã cài đặt trong Letos rồi thoát.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="79"/>
-        <source>file</source>
-        <translation>tệp</translation>
+      <location filename="../main.cpp" line="79"/>
+      <source>file</source>
+      <translation>tệp</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="79"/>
-        <source>Database file to open</source>
-        <translation>Tệp cơ sở dữ liệu cần mở</translation>
+      <location filename="../main.cpp" line="79"/>
+      <source>Database file to open</source>
+      <translation>Tệp cơ sở dữ liệu cần mở</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="163"/>
-        <source>Select configuration directory</source>
-        <translation>Chọn thư mục cấu hình</translation>
+      <location filename="../main.cpp" line="163"/>
+      <source>Select configuration directory</source>
+      <translation>Chọn thư mục cấu hình</translation>
     </message>
-</context>
+  </context>
 </TS>
