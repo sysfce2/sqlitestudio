f7e9374eabb1bb3c6f7584455b779375c2e2effe
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 22:56:10 +0200

    New translations letos.ts (Finnish)
    [ci skip]

--- Letos/letos/translations/letos_fi_FI.ts
+++ Letos/letos/translations/letos_fi_FI.ts
@@ -1,97 +1,97 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="fi" sourcelanguage="en">
-<context>
+  <context>
     <name>QObject</name>
     <message>
-        <location filename="../main.cpp" line="52"/>
-        <source>GUI interface to Letos, a SQLite manager.</source>
-        <translation>Graafinen käyttöliittymä Letosille, SQLite-hallintaohjelmalle.</translation>
+      <location filename="../main.cpp" line="52"/>
+      <source>GUI interface to Letos, a SQLite manager.</source>
+      <translation>Graafinen käyttöliittymä Letosille, SQLite-hallintaohjelmalle.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="57"/>
-        <source>Starts the application in safe mode without restoring the previous session. Use this to bypass issues caused by a corrupted session.</source>
-        <translation>Käynnistää sovelluksen vikasietotilassa palauttamatta edellistä istuntoa. Käytä tätä ohittaaksesi vioittuneen istunnon aiheuttamat ongelmat.</translation>
+      <location filename="../main.cpp" line="57"/>
+      <source>Starts the application in safe mode without restoring the previous session. Use this to bypass issues caused by a corrupted session.</source>
+      <translation>Käynnistää sovelluksen vikasietotilassa palauttamatta edellistä istuntoa. Käytä tätä ohittaaksesi vioittuneen istunnon aiheuttamat ongelmat.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="58"/>
-        <source>Do not load any plugins. Can be used alongside safe mode to further isolate potential issues.</source>
-        <translation>Ei lataa mitään liitännäisiä. Tätä voidaan käyttää vikasietotilan rinnalla mahdollisten ongelmien eristämiseksi tarkemmin.</translation>
+      <location filename="../main.cpp" line="58"/>
+      <source>Do not load any plugins. Can be used alongside safe mode to further isolate potential issues.</source>
+      <translation>Ei lataa mitään liitännäisiä. Tätä voidaan käyttää vikasietotilan rinnalla mahdollisten ongelmien eristämiseksi tarkemmin.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="59"/>
-        <source>Enables debug messages in console (accessible with F12).</source>
-        <translation>Ottaa debug-viestit käyttöön konsolissa (käytettävissä F12-näppäimellä).</translation>
+      <location filename="../main.cpp" line="59"/>
+      <source>Enables debug messages in console (accessible with F12).</source>
+      <translation>Ottaa debug-viestit käyttöön konsolissa (käytettävissä F12-näppäimellä).</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="60"/>
-        <source>Redirects debug messages into standard output (forces debug mode).</source>
-        <translation>Ohjaa debug-viestit vakiotulosteeseen (pakottaa debug-tilan).</translation>
+      <location filename="../main.cpp" line="60"/>
+      <source>Redirects debug messages into standard output (forces debug mode).</source>
+      <translation>Ohjaa debug-viestit vakiotulosteeseen (pakottaa debug-tilan).</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="61"/>
-        <source>Redirects debug messages into given file (forces debug mode).</source>
-        <translation>Ohjaa debug-viestit annettuun tiedostoon (pakottaa debug-tilan).</translation>
+      <location filename="../main.cpp" line="61"/>
+      <source>Redirects debug messages into given file (forces debug mode).</source>
+      <translation>Ohjaa debug-viestit annettuun tiedostoon (pakottaa debug-tilan).</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="61"/>
-        <source>log file</source>
-        <translation>lokitiedosto</translation>
+      <location filename="../main.cpp" line="61"/>
+      <source>log file</source>
+      <translation>lokitiedosto</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="62"/>
-        <source>Enables Lemon parser debug messages for SQL code assistant.</source>
-        <translation>Ottaa Lemon-jäsentimen debug-viestit käyttöön SQL-koodiavustajalle.</translation>
+      <location filename="../main.cpp" line="62"/>
+      <source>Enables Lemon parser debug messages for SQL code assistant.</source>
+      <translation>Ottaa Lemon-jäsentimen debug-viestit käyttöön SQL-koodiavustajalle.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="63"/>
-        <source>Enables debugging of every single SQL query being sent to any database.</source>
-        <translation>Ottaa käyttöön jokaisen mihin tahansa tietokantaan lähetettävän SQL-kyselyn debuggaamisen.</translation>
+      <location filename="../main.cpp" line="63"/>
+      <source>Enables debugging of every single SQL query being sent to any database.</source>
+      <translation>Ottaa käyttöön jokaisen mihin tahansa tietokantaan lähetettävän SQL-kyselyn debuggaamisen.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="64"/>
-        <source>Limits SQL query messages to only the given &lt;database&gt;.</source>
-        <translation>Rajoittaa SQL-kyselyviestit vain annettuun &lt;tietokantaan&gt;.</translation>
+      <location filename="../main.cpp" line="64"/>
+      <source>Limits SQL query messages to only the given &lt;database&gt;.</source>
+      <translation>Rajoittaa SQL-kyselyviestit vain annettuun &lt;tietokantaan&gt;.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="64"/>
-        <source>database</source>
-        <translation>tietokanta</translation>
+      <location filename="../main.cpp" line="64"/>
+      <source>database</source>
+      <translation>tietokanta</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="56"/>
-        <source>Points to the master configuration file. Read manual at wiki page for more details.</source>
-        <translation>Osoittaa päämääritystiedostoon. Lue lisätietoja wiki-sivun käyttöohjeesta.</translation>
+      <location filename="../main.cpp" line="56"/>
+      <source>Points to the master configuration file. Read manual at wiki page for more details.</source>
+      <translation>Osoittaa päämääritystiedostoon. Lue lisätietoja wiki-sivun käyttöohjeesta.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="56"/>
-        <source>settings file</source>
-        <translation>asetustiedosto</translation>
+      <location filename="../main.cpp" line="56"/>
+      <source>settings file</source>
+      <translation>asetustiedosto</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="65"/>
-        <source>Enables debugging of Letos&apos;s query executor.</source>
-        <translation>Ottaa käyttöön Letos&apos;in kyselyjen suorittajan debuggaamisen.</translation>
+      <location filename="../main.cpp" line="65"/>
+      <source>Enables debugging of Letos&apos;s query executor.</source>
+      <translation>Ottaa käyttöön Letos&apos;in kyselyjen suorittajan debuggaamisen.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="66"/>
-        <source>Lists plugins installed in the Letos and quits.</source>
-        <translation>Listaa Letosiin asennetut liitännäiset ja lopettaa.</translation>
+      <location filename="../main.cpp" line="66"/>
+      <source>Lists plugins installed in the Letos and quits.</source>
+      <translation>Listaa Letosiin asennetut liitännäiset ja lopettaa.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="79"/>
-        <source>file</source>
-        <translation>tiedosto</translation>
+      <location filename="../main.cpp" line="79"/>
+      <source>file</source>
+      <translation>tiedosto</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="79"/>
-        <source>Database file to open</source>
-        <translation>Avattava tietokantatiedosto</translation>
+      <location filename="../main.cpp" line="79"/>
+      <source>Database file to open</source>
+      <translation>Avattava tietokantatiedosto</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="163"/>
-        <source>Select configuration directory</source>
-        <translation>Valitse määrityshakemisto</translation>
+      <location filename="../main.cpp" line="163"/>
+      <source>Select configuration directory</source>
+      <translation>Valitse määrityshakemisto</translation>
     </message>
-</context>
+  </context>
 </TS>
