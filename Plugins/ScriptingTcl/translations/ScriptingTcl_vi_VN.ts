2e9a154cd1b0eb348106eac00e84ed2c954de5e7
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 13:17:38 +0200

    New translations scriptingtcl.ts (Vietnamese)
    [ci skip]

--- Plugins/ScriptingTcl/translations/ScriptingTcl_vi_VN.ts
+++ Plugins/ScriptingTcl/translations/ScriptingTcl_vi_VN.ts
@@ -1,27 +1,27 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="vi" sourcelanguage="en">
-<context>
+  <context>
     <name>ScriptingTcl</name>
     <message>
-        <location filename="../scriptingtcl.cpp" line="428"/>
-        <source>No database available in current context, while called Tcl&apos;s &apos;%1&apos; command.</source>
-        <translation>Không có cơ sở dữ liệu nào khả dụng trong ngữ cảnh hiện tại, khi gọi lệnh &apos;%1&apos; của Tcl.</translation>
+      <location filename="../scriptingtcl.cpp" line="428"/>
+      <source>No database available in current context, while called Tcl&apos;s &apos;%1&apos; command.</source>
+      <translation>Không có cơ sở dữ liệu nào khả dụng trong ngữ cảnh hiện tại, khi gọi lệnh &apos;%1&apos; của Tcl.</translation>
     </message>
     <message>
-        <location filename="../scriptingtcl.cpp" line="450"/>
-        <source>Invalid &apos;%1&apos; command syntax. Should be: %2</source>
-        <translation>Cú pháp lệnh &apos;%1&apos; không hợp lệ. Phải là: %2</translation>
+      <location filename="../scriptingtcl.cpp" line="450"/>
+      <source>Invalid &apos;%1&apos; command syntax. Should be: %2</source>
+      <translation>Cú pháp lệnh &apos;%1&apos; không hợp lệ. Phải là: %2</translation>
     </message>
     <message>
-        <location filename="../scriptingtcl.cpp" line="462"/>
-        <source>Error from Tcl&apos;s &apos;%1&apos; command: %2</source>
-        <translation>Lỗi từ lệnh &apos;%1&apos; của Tcl: %2</translation>
+      <location filename="../scriptingtcl.cpp" line="462"/>
+      <source>Error from Tcl&apos;s &apos;%1&apos; command: %2</source>
+      <translation>Lỗi từ lệnh &apos;%1&apos; của Tcl: %2</translation>
     </message>
     <message>
-        <location filename="../scriptingtcl.cpp" line="593"/>
-        <source>Error from Tcl&apos;s&apos; &apos;%1&apos; command: %2</source>
-        <translation>Lỗi từ lệnh &apos;%1&apos; của Tcl: %2</translation>
+      <location filename="../scriptingtcl.cpp" line="593"/>
+      <source>Error from Tcl&apos;s&apos; &apos;%1&apos; command: %2</source>
+      <translation>Lỗi từ lệnh &apos;%1&apos; của Tcl: %2</translation>
     </message>
-</context>
+  </context>
 </TS>
