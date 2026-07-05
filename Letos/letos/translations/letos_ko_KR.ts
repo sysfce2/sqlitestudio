84acf79a644fa4027b513235c100653e618152eb
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 20:56:37 +0200

    New translations letos.ts (Korean)
    [ci skip]

--- Letos/letos/translations/letos_ko_KR.ts
+++ Letos/letos/translations/letos_ko_KR.ts
@@ -1,97 +1,97 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="ko" sourcelanguage="en">
-<context>
+  <context>
     <name>QObject</name>
     <message>
-        <location filename="../main.cpp" line="52"/>
-        <source>GUI interface to Letos, a SQLite manager.</source>
-        <translation>SQLite 관리자인 Letos의 GUI 인터페이스입니다.</translation>
+      <location filename="../main.cpp" line="52"/>
+      <source>GUI interface to Letos, a SQLite manager.</source>
+      <translation>SQLite 관리자인 Letos의 GUI 인터페이스입니다.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="57"/>
-        <source>Starts the application in safe mode without restoring the previous session. Use this to bypass issues caused by a corrupted session.</source>
-        <translation>이전 세션을 복원하지 않고 애플리케이션을 안전 모드로 시작합니다. 손상된 세션으로 인해 발생한 문제를 우회하려면 사용하세요.</translation>
+      <location filename="../main.cpp" line="57"/>
+      <source>Starts the application in safe mode without restoring the previous session. Use this to bypass issues caused by a corrupted session.</source>
+      <translation>이전 세션을 복원하지 않고 애플리케이션을 안전 모드로 시작합니다. 손상된 세션으로 인해 발생한 문제를 우회하려면 사용하세요.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="58"/>
-        <source>Do not load any plugins. Can be used alongside safe mode to further isolate potential issues.</source>
-        <translation>플러그인을 로드하지 않습니다. 안전 모드와 함께 사용하여 잠재적인 문제를 추가로 격리할 수 있습니다.</translation>
+      <location filename="../main.cpp" line="58"/>
+      <source>Do not load any plugins. Can be used alongside safe mode to further isolate potential issues.</source>
+      <translation>플러그인을 로드하지 않습니다. 안전 모드와 함께 사용하여 잠재적인 문제를 추가로 격리할 수 있습니다.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="59"/>
-        <source>Enables debug messages in console (accessible with F12).</source>
-        <translation>콘솔에서 debug 메시지를 활성화합니다(F12로 접근 가능).</translation>
+      <location filename="../main.cpp" line="59"/>
+      <source>Enables debug messages in console (accessible with F12).</source>
+      <translation>콘솔에서 debug 메시지를 활성화합니다(F12로 접근 가능).</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="60"/>
-        <source>Redirects debug messages into standard output (forces debug mode).</source>
-        <translation>디버그 메시지를 표준 출력으로 리디렉션합니다(디버그 모드를 강제로 사용).</translation>
+      <location filename="../main.cpp" line="60"/>
+      <source>Redirects debug messages into standard output (forces debug mode).</source>
+      <translation>디버그 메시지를 표준 출력으로 리디렉션합니다(디버그 모드를 강제로 사용).</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="61"/>
-        <source>Redirects debug messages into given file (forces debug mode).</source>
-        <translation>디버그 메시지를 지정한 파일로 리디렉션합니다(디버그 모드를 강제로 사용).</translation>
+      <location filename="../main.cpp" line="61"/>
+      <source>Redirects debug messages into given file (forces debug mode).</source>
+      <translation>디버그 메시지를 지정한 파일로 리디렉션합니다(디버그 모드를 강제로 사용).</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="61"/>
-        <source>log file</source>
-        <translation>로그 파일</translation>
+      <location filename="../main.cpp" line="61"/>
+      <source>log file</source>
+      <translation>로그 파일</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="62"/>
-        <source>Enables Lemon parser debug messages for SQL code assistant.</source>
-        <translation>SQL 코드 어시스턴트용 Lemon 파서 debug 메시지를 활성화합니다.</translation>
+      <location filename="../main.cpp" line="62"/>
+      <source>Enables Lemon parser debug messages for SQL code assistant.</source>
+      <translation>SQL 코드 어시스턴트용 Lemon 파서 debug 메시지를 활성화합니다.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="63"/>
-        <source>Enables debugging of every single SQL query being sent to any database.</source>
-        <translation>데이터베이스로 전송되는 모든 단일 SQL 쿼리의 debugging을 활성화합니다.</translation>
+      <location filename="../main.cpp" line="63"/>
+      <source>Enables debugging of every single SQL query being sent to any database.</source>
+      <translation>데이터베이스로 전송되는 모든 단일 SQL 쿼리의 debugging을 활성화합니다.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="64"/>
-        <source>Limits SQL query messages to only the given &lt;database&gt;.</source>
-        <translation>SQL 쿼리 메시지를 지정한 &lt;데이터베이스&gt;로만 제한합니다.</translation>
+      <location filename="../main.cpp" line="64"/>
+      <source>Limits SQL query messages to only the given &lt;database&gt;.</source>
+      <translation>SQL 쿼리 메시지를 지정한 &lt;데이터베이스&gt;로만 제한합니다.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="64"/>
-        <source>database</source>
-        <translation>데이터베이스</translation>
+      <location filename="../main.cpp" line="64"/>
+      <source>database</source>
+      <translation>데이터베이스</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="56"/>
-        <source>Points to the master configuration file. Read manual at wiki page for more details.</source>
-        <translation>마스터 구성 파일을 가리킵니다. 자세한 내용은 wiki 페이지의 매뉴얼을 읽으세요.</translation>
+      <location filename="../main.cpp" line="56"/>
+      <source>Points to the master configuration file. Read manual at wiki page for more details.</source>
+      <translation>마스터 구성 파일을 가리킵니다. 자세한 내용은 wiki 페이지의 매뉴얼을 읽으세요.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="56"/>
-        <source>settings file</source>
-        <translation>설정 파일</translation>
+      <location filename="../main.cpp" line="56"/>
+      <source>settings file</source>
+      <translation>설정 파일</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="65"/>
-        <source>Enables debugging of Letos&apos;s query executor.</source>
-        <translation>Letos&apos;의 쿼리 실행기 디버깅을 활성화합니다.</translation>
+      <location filename="../main.cpp" line="65"/>
+      <source>Enables debugging of Letos&apos;s query executor.</source>
+      <translation>Letos&apos;의 쿼리 실행기 디버깅을 활성화합니다.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="66"/>
-        <source>Lists plugins installed in the Letos and quits.</source>
-        <translation>Letos에 설치된 플러그인 목록을 표시하고 종료합니다.</translation>
+      <location filename="../main.cpp" line="66"/>
+      <source>Lists plugins installed in the Letos and quits.</source>
+      <translation>Letos에 설치된 플러그인 목록을 표시하고 종료합니다.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="79"/>
-        <source>file</source>
-        <translation>파일</translation>
+      <location filename="../main.cpp" line="79"/>
+      <source>file</source>
+      <translation>파일</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="79"/>
-        <source>Database file to open</source>
-        <translation>열 데이터베이스 파일</translation>
+      <location filename="../main.cpp" line="79"/>
+      <source>Database file to open</source>
+      <translation>열 데이터베이스 파일</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="163"/>
-        <source>Select configuration directory</source>
-        <translation>구성 디렉터리 선택</translation>
+      <location filename="../main.cpp" line="163"/>
+      <source>Select configuration directory</source>
+      <translation>구성 디렉터리 선택</translation>
     </message>
-</context>
+  </context>
 </TS>
