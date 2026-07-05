6b28fbce512e2a2bb3a7d5b5940369aa7dfdd289
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 13:20:36 +0200

    New translations scriptingpython.ts (Slovak)
    [ci skip]

--- Plugins/ScriptingPython/translations/ScriptingPython_sk_SK.ts
+++ Plugins/ScriptingPython/translations/ScriptingPython_sk_SK.ts
@@ -1,50 +1,50 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="sk" sourcelanguage="en">
-<context>
+  <context>
     <name>QObject</name>
     <message>
-        <location filename="../scriptingpython.cpp" line="730"/>
-        <source>Invalid use of %1 function. Expected %2 arguments, but got %3.</source>
-        <translation>Neplatné použitie funkcie %1. Očakávalo sa %2 argumentov, ale prijatých bolo %3.</translation>
+      <location filename="../scriptingpython.cpp" line="730"/>
+      <source>Invalid use of %1 function. Expected %2 arguments, but got %3.</source>
+      <translation>Neplatné použitie funkcie %1. Očakávalo sa %2 argumentov, ale prijatých bolo %3.</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="739"/>
-        <source>Unknown error from function %1.</source>
-        <translation>Neznáma chyba z funkcie %1.</translation>
+      <location filename="../scriptingpython.cpp" line="739"/>
+      <source>Unknown error from function %1.</source>
+      <translation>Neznáma chyba z funkcie %1.</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="780"/>
-        <source>Could not calculate string representation of the Python object passed as argument to the function %1.</source>
-        <translation>Nepodarilo sa vypočítať reťazcovú reprezentáciu objektu Pythonu odovzdaného ako argument funkcii %1.</translation>
+      <location filename="../scriptingpython.cpp" line="780"/>
+      <source>Could not calculate string representation of the Python object passed as argument to the function %1.</source>
+      <translation>Nepodarilo sa vypočítať reťazcovú reprezentáciu objektu Pythonu odovzdaného ako argument funkcii %1.</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="802"/>
-        <source>Could not find execution context for function %1. This is a bug of Python plugin. Please report it.</source>
-        <translation>Nepodarilo sa nájsť kontext vykonávania funkcie %1. Toto je chyba zásuvného modulu Pythonu. Nahláste ju, prosím.</translation>
+      <location filename="../scriptingpython.cpp" line="802"/>
+      <source>Could not find execution context for function %1. This is a bug of Python plugin. Please report it.</source>
+      <translation>Nepodarilo sa nájsť kontext vykonávania funkcie %1. Toto je chyba zásuvného modulu Pythonu. Nahláste ju, prosím.</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="827"/>
-        <source>Error from Python function %1: %2</source>
-        <translation>Chyba z funkcie Pythonu %1: %2</translation>
+      <location filename="../scriptingpython.cpp" line="827"/>
+      <source>Error from Python function %1: %2</source>
+      <translation>Chyba z funkcie Pythonu %1: %2</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>ScriptingPython</name>
     <message>
-        <location filename="../scriptingpython.ui" line="20"/>
-        <source>Python library path</source>
-        <translation>Cesta ku knižnici Pythonu</translation>
+      <location filename="../scriptingpython.ui" line="20"/>
+      <source>Python library path</source>
+      <translation>Cesta ku knižnici Pythonu</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.ui" line="29"/>
-        <source>Choose a Python library file</source>
-        <translation>Vyberte súbor knižnice Pythonu</translation>
+      <location filename="../scriptingpython.ui" line="29"/>
+      <source>Choose a Python library file</source>
+      <translation>Vyberte súbor knižnice Pythonu</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="370"/>
-        <source>The plugin is not configured properly.</source>
-        <translation>Zásuvný modul nie je správne nakonfigurovaný.</translation>
+      <location filename="../scriptingpython.cpp" line="370"/>
+      <source>The plugin is not configured properly.</source>
+      <translation>Zásuvný modul nie je správne nakonfigurovaný.</translation>
     </message>
-</context>
+  </context>
 </TS>
