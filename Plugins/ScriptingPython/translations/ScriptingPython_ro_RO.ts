74a982967e4524d2ba326ce3fcdb75bb1e15f03f
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 20:52:27 +0200

    New translations scriptingpython.ts (Romanian)
    [ci skip]

--- Plugins/ScriptingPython/translations/ScriptingPython_ro_RO.ts
+++ Plugins/ScriptingPython/translations/ScriptingPython_ro_RO.ts
@@ -1,50 +1,50 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="ro" sourcelanguage="en">
-<context>
+  <context>
     <name>QObject</name>
     <message>
-        <location filename="../scriptingpython.cpp" line="730"/>
-        <source>Invalid use of %1 function. Expected %2 arguments, but got %3.</source>
-        <translation>Utilizare nevalidă a funcției %1. Se așteptau %2 argumente, dar s-au primit %3.</translation>
+      <location filename="../scriptingpython.cpp" line="730"/>
+      <source>Invalid use of %1 function. Expected %2 arguments, but got %3.</source>
+      <translation>Utilizare nevalidă a funcției %1. Se așteptau %2 argumente, dar s-au primit %3.</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="739"/>
-        <source>Unknown error from function %1.</source>
-        <translation>Eroare necunoscută de la funcția %1.</translation>
+      <location filename="../scriptingpython.cpp" line="739"/>
+      <source>Unknown error from function %1.</source>
+      <translation>Eroare necunoscută de la funcția %1.</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="780"/>
-        <source>Could not calculate string representation of the Python object passed as argument to the function %1.</source>
-        <translation>Nu s-a putut calcula reprezentarea ca șir a obiectului Python transmis ca argument funcției %1.</translation>
+      <location filename="../scriptingpython.cpp" line="780"/>
+      <source>Could not calculate string representation of the Python object passed as argument to the function %1.</source>
+      <translation>Nu s-a putut calcula reprezentarea ca șir a obiectului Python transmis ca argument funcției %1.</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="802"/>
-        <source>Could not find execution context for function %1. This is a bug of Python plugin. Please report it.</source>
-        <translation>Nu s-a putut găsi contextul de execuție pentru funcția %1. Aceasta este o eroare a pluginului Python. Vă rugăm să o raportați.</translation>
+      <location filename="../scriptingpython.cpp" line="802"/>
+      <source>Could not find execution context for function %1. This is a bug of Python plugin. Please report it.</source>
+      <translation>Nu s-a putut găsi contextul de execuție pentru funcția %1. Aceasta este o eroare a pluginului Python. Vă rugăm să o raportați.</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="827"/>
-        <source>Error from Python function %1: %2</source>
-        <translation>Eroare de la funcția Python %1: %2</translation>
+      <location filename="../scriptingpython.cpp" line="827"/>
+      <source>Error from Python function %1: %2</source>
+      <translation>Eroare de la funcția Python %1: %2</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>ScriptingPython</name>
     <message>
-        <location filename="../scriptingpython.ui" line="20"/>
-        <source>Python library path</source>
-        <translation>Calea bibliotecii Python</translation>
+      <location filename="../scriptingpython.ui" line="20"/>
+      <source>Python library path</source>
+      <translation>Calea bibliotecii Python</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.ui" line="29"/>
-        <source>Choose a Python library file</source>
-        <translation>Alegeți un fișier de bibliotecă Python</translation>
+      <location filename="../scriptingpython.ui" line="29"/>
+      <source>Choose a Python library file</source>
+      <translation>Alegeți un fișier de bibliotecă Python</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="370"/>
-        <source>The plugin is not configured properly.</source>
-        <translation>Pluginul nu este configurat corespunzător.</translation>
+      <location filename="../scriptingpython.cpp" line="370"/>
+      <source>The plugin is not configured properly.</source>
+      <translation>Pluginul nu este configurat corespunzător.</translation>
     </message>
-</context>
+  </context>
 </TS>
