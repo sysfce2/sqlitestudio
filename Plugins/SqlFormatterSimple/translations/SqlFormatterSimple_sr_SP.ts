c2b48b8023f5ce886a17d9b63514389ace52e57a
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 23:00:19 +0200

    New translations sqlformattersimple.ts (Serbian (Cyrillic))
    [ci skip]

--- Plugins/SqlFormatterSimple/translations/SqlFormatterSimple_sr_SP.ts
+++ Plugins/SqlFormatterSimple/translations/SqlFormatterSimple_sr_SP.ts
@@ -1,17 +1,17 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="sr" sourcelanguage="en">
-<context>
+  <context>
     <name>SqlFormatterSimplePlugin</name>
     <message>
-        <location filename="../SqlFormatterSimple.ui" line="20"/>
-        <source>Upper case keywords</source>
-        <translation>Кључне речи великим словима</translation>
+      <location filename="../SqlFormatterSimple.ui" line="20"/>
+      <source>Upper case keywords</source>
+      <translation>Кључне речи великим словима</translation>
     </message>
     <message>
-        <location filename="../SqlFormatterSimple.ui" line="30"/>
-        <source>Reduce multiple whitespaces to single whitespace</source>
-        <translation>Сведи више узастопних белина на једну белину</translation>
+      <location filename="../SqlFormatterSimple.ui" line="30"/>
+      <source>Reduce multiple whitespaces to single whitespace</source>
+      <translation>Сведи више узастопних белина на једну белину</translation>
     </message>
-</context>
+  </context>
 </TS>
