422aa31935b9992bc5bbfc312e4900c4a7bcd13b
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 13:16:14 +0200

    New translations printing.ts (Slovak)
    [ci skip]

--- Plugins/Printing/translations/Printing_sk_SK.ts
+++ Plugins/Printing/translations/Printing_sk_SK.ts
@@ -1,40 +1,40 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="sk" sourcelanguage="en">
-<context>
+  <context>
     <name>Printing</name>
     <message>
-        <location filename="../printing.cpp" line="35"/>
-        <source>Print data</source>
-        <translation>Tlačiť údaje</translation>
+      <location filename="../printing.cpp" line="36"/>
+      <source>Print data</source>
+      <translation>Tlačiť údaje</translation>
     </message>
     <message>
-        <location filename="../printing.cpp" line="37"/>
-        <source>Print query</source>
-        <translation>Tlačiť dopyt</translation>
+      <location filename="../printing.cpp" line="38"/>
+      <source>Print query</source>
+      <translation>Tlačiť dopyt</translation>
     </message>
     <message>
-        <location filename="../printing.cpp" line="74"/>
-        <source>No data to print.</source>
-        <translation>Žiadne údaje na tlač.</translation>
+      <location filename="../printing.cpp" line="75"/>
+      <source>No data to print.</source>
+      <translation>Žiadne údaje na tlač.</translation>
     </message>
     <message>
-        <location filename="../printing.cpp" line="82"/>
-        <source>Printing data.</source>
-        <translation>Tlačia sa údaje.</translation>
+      <location filename="../printing.cpp" line="83"/>
+      <source>Printing data.</source>
+      <translation>Tlačia sa údaje.</translation>
     </message>
     <message>
-        <location filename="../printing.cpp" line="108"/>
-        <source>Printing query.</source>
-        <translation>Tlačí sa dopyt.</translation>
+      <location filename="../printing.cpp" line="109"/>
+      <source>Printing query.</source>
+      <translation>Tlačí sa dopyt.</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>PrintingExport</name>
     <message>
-        <location filename="../printingexport.cpp" line="27"/>
-        <source>Printing</source>
-        <translation>Tlač</translation>
+      <location filename="../printingexport.cpp" line="27"/>
+      <source>Printing</source>
+      <translation>Tlač</translation>
     </message>
-</context>
+  </context>
 </TS>
