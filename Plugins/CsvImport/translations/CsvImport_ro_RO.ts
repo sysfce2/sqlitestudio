5ec9f033d17c71cc5868684747cbdc58c639b9c9
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 22:51:17 +0200

    New translations csvimport.ts (Romanian)
    [ci skip]

--- Plugins/CsvImport/translations/CsvImport_ro_RO.ts
+++ Plugins/CsvImport/translations/CsvImport_ro_RO.ts
@@ -1,90 +1,90 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="ro" sourcelanguage="en">
-<context>
+  <context>
     <name>CsvImport</name>
     <message>
-        <location filename="../csvimport.cpp" line="32"/>
-        <source>Cannot read file %1</source>
-        <translation>Nu se poate citi fișierul %1</translation>
+      <location filename="../csvimport.cpp" line="32"/>
+      <source>Cannot read file %1</source>
+      <translation>Nu se poate citi fișierul %1</translation>
     </message>
     <message>
-        <location filename="../csvimport.cpp" line="64"/>
-        <source>Could not find any data in the file %1.</source>
-        <translation>Nu s-au putut găsi date în fișierul %1.</translation>
+      <location filename="../csvimport.cpp" line="64"/>
+      <source>Could not find any data in the file %1.</source>
+      <translation>Nu s-au putut găsi date în fișierul %1.</translation>
     </message>
     <message>
-        <location filename="../csvimport.cpp" line="170"/>
-        <source>Enter the custom separator character.</source>
-        <translation>Introduceți caracterul separator personalizat.</translation>
+      <location filename="../csvimport.cpp" line="170"/>
+      <source>Enter the custom separator character.</source>
+      <translation>Introduceți caracterul separator personalizat.</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>csvImportOptions</name>
     <message>
-        <location filename="../CsvImportOptions.ui" line="37"/>
-        <source>, (comma)</source>
-        <translation>, (virgulă)</translation>
+      <location filename="../CsvImportOptions.ui" line="37"/>
+      <source>, (comma)</source>
+      <translation>, (virgulă)</translation>
     </message>
     <message>
-        <location filename="../CsvImportOptions.ui" line="42"/>
-        <source>; (semicolon)</source>
-        <translation>; (punct și virgulă)</translation>
+      <location filename="../CsvImportOptions.ui" line="42"/>
+      <source>; (semicolon)</source>
+      <translation>; (punct și virgulă)</translation>
     </message>
     <message>
-        <location filename="../CsvImportOptions.ui" line="47"/>
-        <source>\t (tab)</source>
-        <translation>\t (tabulator)</translation>
+      <location filename="../CsvImportOptions.ui" line="47"/>
+      <source>\t (tab)</source>
+      <translation>\t (tabulator)</translation>
     </message>
     <message>
-        <location filename="../CsvImportOptions.ui" line="52"/>
-        <source>  (whitespace)</source>
-        <translation>  (spațiu alb)</translation>
+      <location filename="../CsvImportOptions.ui" line="52"/>
+      <source>  (whitespace)</source>
+      <translation>  (spațiu alb)</translation>
     </message>
     <message>
-        <location filename="../CsvImportOptions.ui" line="57"/>
-        <source>Custom:</source>
-        <translation>Personalizat:</translation>
+      <location filename="../CsvImportOptions.ui" line="57"/>
+      <source>Custom:</source>
+      <translation>Personalizat:</translation>
     </message>
     <message>
-        <location filename="../CsvImportOptions.ui" line="65"/>
-        <source>Column separator:</source>
-        <translation>Separator de coloane:</translation>
+      <location filename="../CsvImportOptions.ui" line="65"/>
+      <source>Column separator:</source>
+      <translation>Separator de coloane:</translation>
     </message>
     <message>
-        <location filename="../CsvImportOptions.ui" line="108"/>
-        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;If enabled, the &amp;quot; character will be interpreted as standard CSV value quotation mark, causing the string to be treated as single value until the next (ending) &amp;quot; character. If disabled, the &amp;quot; will be treated as any other character. Usually you want this to be enabled.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Dacă este activată, caracterul &amp;quot; va fi interpretat ca marcaj standard de citare a valorilor CSV, făcând ca șirul să fie tratat ca o singură valoare până la următorul caracter &amp;quot; (de închidere). Dacă este dezactivată, &amp;quot; va fi tratat ca orice alt caracter. De obicei, doriți ca această opțiune să fie activată.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+      <location filename="../CsvImportOptions.ui" line="108"/>
+      <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;If enabled, the &amp;quot; character will be interpreted as standard CSV value quotation mark, causing the string to be treated as single value until the next (ending) &amp;quot; character. If disabled, the &amp;quot; will be treated as any other character. Usually you want this to be enabled.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+      <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Dacă este activată, caracterul &amp;quot; va fi interpretat ca marcaj standard de citare a valorilor CSV, făcând ca șirul să fie tratat ca o singură valoare până la următorul caracter &amp;quot; (de închidere). Dacă este dezactivată, &amp;quot; va fi tratat ca orice alt caracter. De obicei, doriți ca această opțiune să fie activată.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
-        <location filename="../CsvImportOptions.ui" line="111"/>
-        <source>Interpret &quot; as a value quotation mark</source>
-        <translation>Interpretează &quot; ca marcaj de citare a valorii</translation>
+      <location filename="../CsvImportOptions.ui" line="111"/>
+      <source>Interpret &quot; as a value quotation mark</source>
+      <translation>Interpretează &quot; ca marcaj de citare a valorii</translation>
     </message>
     <message>
-        <location filename="../CsvImportOptions.ui" line="20"/>
-        <source>&lt;p&gt;Enable this if the first data line in your CSV file represents column names. You don&apos;t want column names to be imported into the table as a regular data.&lt;/p&gt;</source>
-        <translation>&lt;p&gt;Activați această opțiune dacă prima linie de date din fișierul dvs. CSV reprezintă numele coloanelor. Nu doriți ca numele coloanelor să fie importate în tabel ca date obișnuite.&lt;/p&gt;</translation>
+      <location filename="../CsvImportOptions.ui" line="20"/>
+      <source>&lt;p&gt;Enable this if the first data line in your CSV file represents column names. You don&apos;t want column names to be imported into the table as a regular data.&lt;/p&gt;</source>
+      <translation>&lt;p&gt;Activați această opțiune dacă prima linie de date din fișierul dvs. CSV reprezintă numele coloanelor. Nu doriți ca numele coloanelor să fie importate în tabel ca date obișnuite.&lt;/p&gt;</translation>
     </message>
     <message>
-        <location filename="../CsvImportOptions.ui" line="23"/>
-        <source>First line represents CSV column names</source>
-        <translation>Prima linie reprezintă numele coloanelor CSV</translation>
+      <location filename="../CsvImportOptions.ui" line="23"/>
+      <source>First line represents CSV column names</source>
+      <translation>Prima linie reprezintă numele coloanelor CSV</translation>
     </message>
     <message>
-        <location filename="../CsvImportOptions.ui" line="72"/>
-        <source>If enabled, then text value provided on the right will be interpreted as NULL</source>
-        <translation>Dacă este activată, valoarea text furnizată în dreapta va fi interpretată ca NULL</translation>
+      <location filename="../CsvImportOptions.ui" line="72"/>
+      <source>If enabled, then text value provided on the right will be interpreted as NULL</source>
+      <translation>Dacă este activată, valoarea text furnizată în dreapta va fi interpretată ca NULL</translation>
     </message>
     <message>
-        <location filename="../CsvImportOptions.ui" line="75"/>
-        <source>NULL values:</source>
-        <translation>Valori NULL:</translation>
+      <location filename="../CsvImportOptions.ui" line="75"/>
+      <source>NULL values:</source>
+      <translation>Valori NULL:</translation>
     </message>
     <message>
-        <location filename="../CsvImportOptions.ui" line="85"/>
-        <source>If your CSV data contains null values, define how are they represented in the CSV.</source>
-        <translation>Dacă datele dvs. CSV conțin valori null, definiți cum sunt reprezentate acestea în CSV.</translation>
+      <location filename="../CsvImportOptions.ui" line="85"/>
+      <source>If your CSV data contains null values, define how are they represented in the CSV.</source>
+      <translation>Dacă datele dvs. CSV conțin valori null, definiți cum sunt reprezentate acestea în CSV.</translation>
     </message>
-</context>
+  </context>
 </TS>
