ea7e0af56100a0246a0399d3f5d97a5405dad833
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 13:24:27 +0200

    New translations letos.ts (Swedish)
    [ci skip]

--- Letos/letos/translations/letos_sv_SE.ts
+++ Letos/letos/translations/letos_sv_SE.ts
@@ -1,97 +1,97 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="sv-SE" sourcelanguage="en">
-<context>
+  <context>
     <name>QObject</name>
     <message>
-        <location filename="../main.cpp" line="52"/>
-        <source>GUI interface to Letos, a SQLite manager.</source>
-        <translation>GUI-gränssnitt till Letos, en SQLite-hanterare.</translation>
+      <location filename="../main.cpp" line="52"/>
+      <source>GUI interface to Letos, a SQLite manager.</source>
+      <translation>GUI-gränssnitt till Letos, en SQLite-hanterare.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="57"/>
-        <source>Starts the application in safe mode without restoring the previous session. Use this to bypass issues caused by a corrupted session.</source>
-        <translation>Startar programmet i säkert läge utan att återställa föregående session. Använd detta för att kringgå problem som orsakas av en skadad session.</translation>
+      <location filename="../main.cpp" line="57"/>
+      <source>Starts the application in safe mode without restoring the previous session. Use this to bypass issues caused by a corrupted session.</source>
+      <translation>Startar programmet i säkert läge utan att återställa föregående session. Använd detta för att kringgå problem som orsakas av en skadad session.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="58"/>
-        <source>Do not load any plugins. Can be used alongside safe mode to further isolate potential issues.</source>
-        <translation>Ladda inte några plugins. Kan användas tillsammans med säkert läge för att ytterligare isolera möjliga problem.</translation>
+      <location filename="../main.cpp" line="58"/>
+      <source>Do not load any plugins. Can be used alongside safe mode to further isolate potential issues.</source>
+      <translation>Ladda inte några plugins. Kan användas tillsammans med säkert läge för att ytterligare isolera möjliga problem.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="59"/>
-        <source>Enables debug messages in console (accessible with F12).</source>
-        <translation>Aktiverar debugmeddelanden i konsolen (tillgänglig med F12).</translation>
+      <location filename="../main.cpp" line="59"/>
+      <source>Enables debug messages in console (accessible with F12).</source>
+      <translation>Aktiverar debugmeddelanden i konsolen (tillgänglig med F12).</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="60"/>
-        <source>Redirects debug messages into standard output (forces debug mode).</source>
-        <translation>Omdirigerar debugmeddelanden till standardutdata (tvingar debugläge).</translation>
+      <location filename="../main.cpp" line="60"/>
+      <source>Redirects debug messages into standard output (forces debug mode).</source>
+      <translation>Omdirigerar debugmeddelanden till standardutdata (tvingar debugläge).</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="61"/>
-        <source>Redirects debug messages into given file (forces debug mode).</source>
-        <translation>Omdirigerar debugmeddelanden till angiven fil (tvingar debugläge).</translation>
+      <location filename="../main.cpp" line="61"/>
+      <source>Redirects debug messages into given file (forces debug mode).</source>
+      <translation>Omdirigerar debugmeddelanden till angiven fil (tvingar debugläge).</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="61"/>
-        <source>log file</source>
-        <translation>loggfil</translation>
+      <location filename="../main.cpp" line="61"/>
+      <source>log file</source>
+      <translation>loggfil</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="62"/>
-        <source>Enables Lemon parser debug messages for SQL code assistant.</source>
-        <translation>Aktiverar debugmeddelanden från Lemon-parsern för SQL-kodassistenten.</translation>
+      <location filename="../main.cpp" line="62"/>
+      <source>Enables Lemon parser debug messages for SQL code assistant.</source>
+      <translation>Aktiverar debugmeddelanden från Lemon-parsern för SQL-kodassistenten.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="63"/>
-        <source>Enables debugging of every single SQL query being sent to any database.</source>
-        <translation>Aktiverar debug för varje enskild SQL-fråga som skickas till någon databas.</translation>
+      <location filename="../main.cpp" line="63"/>
+      <source>Enables debugging of every single SQL query being sent to any database.</source>
+      <translation>Aktiverar debug för varje enskild SQL-fråga som skickas till någon databas.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="64"/>
-        <source>Limits SQL query messages to only the given &lt;database&gt;.</source>
-        <translation>Begränsar SQL-frågemeddelanden till endast den angivna &lt;databasen&gt;.</translation>
+      <location filename="../main.cpp" line="64"/>
+      <source>Limits SQL query messages to only the given &lt;database&gt;.</source>
+      <translation>Begränsar SQL-frågemeddelanden till endast den angivna &lt;databasen&gt;.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="64"/>
-        <source>database</source>
-        <translation>databas</translation>
+      <location filename="../main.cpp" line="64"/>
+      <source>database</source>
+      <translation>databas</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="56"/>
-        <source>Points to the master configuration file. Read manual at wiki page for more details.</source>
-        <translation>Pekar på huvudkonfigurationsfilen. Läs manualen på wikisidan för mer information.</translation>
+      <location filename="../main.cpp" line="56"/>
+      <source>Points to the master configuration file. Read manual at wiki page for more details.</source>
+      <translation>Pekar på huvudkonfigurationsfilen. Läs manualen på wikisidan för mer information.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="56"/>
-        <source>settings file</source>
-        <translation>inställningsfil</translation>
+      <location filename="../main.cpp" line="56"/>
+      <source>settings file</source>
+      <translation>inställningsfil</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="65"/>
-        <source>Enables debugging of Letos&apos;s query executor.</source>
-        <translation>Aktiverar debug av Letos&apos;s frågeexekverare.</translation>
+      <location filename="../main.cpp" line="65"/>
+      <source>Enables debugging of Letos&apos;s query executor.</source>
+      <translation>Aktiverar debug av Letos&apos;s frågeexekverare.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="66"/>
-        <source>Lists plugins installed in the Letos and quits.</source>
-        <translation>Listar plugins installerade i Letos och avslutar.</translation>
+      <location filename="../main.cpp" line="66"/>
+      <source>Lists plugins installed in the Letos and quits.</source>
+      <translation>Listar plugins installerade i Letos och avslutar.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="79"/>
-        <source>file</source>
-        <translation>fil</translation>
+      <location filename="../main.cpp" line="79"/>
+      <source>file</source>
+      <translation>fil</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="79"/>
-        <source>Database file to open</source>
-        <translation>Databasfil att öppna</translation>
+      <location filename="../main.cpp" line="79"/>
+      <source>Database file to open</source>
+      <translation>Databasfil att öppna</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="152"/>
-        <source>Select configuration directory</source>
-        <translation>Välj konfigurationskatalog</translation>
+      <location filename="../main.cpp" line="163"/>
+      <source>Select configuration directory</source>
+      <translation>Välj konfigurationskatalog</translation>
     </message>
-</context>
+  </context>
 </TS>
