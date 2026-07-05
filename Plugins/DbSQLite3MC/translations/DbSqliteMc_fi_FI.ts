272eb4c9bf666db4eb4ba0fca60c0ec8d4a07cc1
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 13:21:40 +0200

    New translations dbsqlitemc.ts (Finnish)
    [ci skip]

--- Plugins/DbSQLite3MC/translations/DbSqliteMc_fi_FI.ts
+++ Plugins/DbSQLite3MC/translations/DbSqliteMc_fi_FI.ts
@@ -1,46 +1,46 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="fi" sourcelanguage="en">
-<context>
+  <context>
     <name>DbSqliteMc</name>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="42"/>
-        <source>Password (key)</source>
-        <translation>Salasana (avain)</translation>
+      <location filename="../dbsqlitemc.cpp" line="42"/>
+      <source>Password (key)</source>
+      <translation>Salasana (avain)</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="43"/>
-        <source>Leave empty to create or connect to decrypted database.</source>
-        <translation>Jätä tyhjäksi luodaksesi salauksen purkamattoman tietokannan tai muodostaaksesi siihen yhteyden.</translation>
+      <location filename="../dbsqlitemc.cpp" line="43"/>
+      <source>Leave empty to create or connect to decrypted database.</source>
+      <translation>Jätä tyhjäksi luodaksesi salauksen purkamattoman tietokannan tai muodostaaksesi siihen yhteyden.</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="44"/>
-        <source>Encryption password</source>
-        <translation>Salaussalasana</translation>
+      <location filename="../dbsqlitemc.cpp" line="44"/>
+      <source>Encryption password</source>
+      <translation>Salaussalasana</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="50"/>
-        <source>Cipher</source>
-        <translation>Salausmenetelmä</translation>
+      <location filename="../dbsqlitemc.cpp" line="50"/>
+      <source>Cipher</source>
+      <translation>Salausmenetelmä</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="51"/>
-        <source>Cipher determines encryption algorithm used to encrypt the database.</source>
-        <translation>Salausmenetelmä määrittää tietokannan salaamiseen käytettävän salausalgoritmin.</translation>
+      <location filename="../dbsqlitemc.cpp" line="51"/>
+      <source>Cipher determines encryption algorithm used to encrypt the database.</source>
+      <translation>Salausmenetelmä määrittää tietokannan salaamiseen käytettävän salausalgoritmin.</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="61"/>
-        <source>Cipher configuration (optional)</source>
-        <translation>Salausmenetelmän määritys (valinnainen)</translation>
+      <location filename="../dbsqlitemc.cpp" line="61"/>
+      <source>Cipher configuration (optional)</source>
+      <translation>Salausmenetelmän määritys (valinnainen)</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="62"/>
-        <source>PRAGMA statements to customize SQLite3 Multiple Ciphers configuration, such as KDF iterations, legacy mode, etc.
+      <location filename="../dbsqlitemc.cpp" line="62"/>
+      <source>PRAGMA statements to customize SQLite3 Multiple Ciphers configuration, such as KDF iterations, legacy mode, etc.
 They will be executed upon each opening of the database.
 See documentation for SQLite3 Multiple Ciphers for details.</source>
-        <translation>PRAGMA-lauseet SQLite3 Multiple Ciphers -määrityksen mukauttamiseen, kuten KDF-iteraatiot, vanha yhteensopivuustila jne.
+      <translation>PRAGMA-lauseet SQLite3 Multiple Ciphers -määrityksen mukauttamiseen, kuten KDF-iteraatiot, vanha yhteensopivuustila jne.
 Ne suoritetaan aina, kun tietokanta avataan.
 Katso lisätietoja SQLite3 Multiple Ciphers -dokumentaatiosta.</translation>
     </message>
-</context>
+  </context>
 </TS>
