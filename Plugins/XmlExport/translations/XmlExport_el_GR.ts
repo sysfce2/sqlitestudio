8a5162c7fc34f5f71d618f8dcef88080b7c8a239
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 20:51:52 +0200

    New translations xmlexport.ts (Greek)
    [ci skip]

--- Plugins/XmlExport/translations/XmlExport_el_GR.ts
+++ Plugins/XmlExport/translations/XmlExport_el_GR.ts
@@ -1,70 +1,70 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="el" sourcelanguage="en">
-<context>
+  <context>
     <name>XmlExport</name>
     <message>
-        <location filename="../xmlexport.cpp" line="37"/>
-        <source>Enter the namespace to use (for example: http://my.namespace.org)</source>
-        <translation>Εισαγάγετε τον χώρο ονομάτων που θα χρησιμοποιηθεί (για παράδειγμα: http://my.namespace.org)</translation>
+      <location filename="../xmlexport.cpp" line="37"/>
+      <source>Enter the namespace to use (for example: http://my.namespace.org)</source>
+      <translation>Εισαγάγετε τον χώρο ονομάτων που θα χρησιμοποιηθεί (για παράδειγμα: http://my.namespace.org)</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>XmlExportConfig</name>
     <message>
-        <location filename="../XmlExport.ui" line="20"/>
-        <source>Output format</source>
-        <translation>Μορφή εξόδου</translation>
+      <location filename="../XmlExport.ui" line="20"/>
+      <source>Output format</source>
+      <translation>Μορφή εξόδου</translation>
     </message>
     <message>
-        <location filename="../XmlExport.ui" line="26"/>
-        <source>Format document (new lines, indentation)</source>
-        <translation>Μορφοποίηση εγγράφου (νέες γραμμές, εσοχές)</translation>
+      <location filename="../XmlExport.ui" line="26"/>
+      <source>Format document (new lines, indentation)</source>
+      <translation>Μορφοποίηση εγγράφου (νέες γραμμές, εσοχές)</translation>
     </message>
     <message>
-        <location filename="../XmlExport.ui" line="42"/>
-        <source>Compress (everything in one line)</source>
-        <translation>Συμπίεση (όλα σε μία γραμμή)</translation>
+      <location filename="../XmlExport.ui" line="42"/>
+      <source>Compress (everything in one line)</source>
+      <translation>Συμπίεση (όλα σε μία γραμμή)</translation>
     </message>
     <message>
-        <location filename="../XmlExport.ui" line="58"/>
-        <source>Special characters escaping</source>
-        <translation>Διαφυγή ειδικών χαρακτήρων</translation>
+      <location filename="../XmlExport.ui" line="58"/>
+      <source>Special characters escaping</source>
+      <translation>Διαφυγή ειδικών χαρακτήρων</translation>
     </message>
     <message>
-        <location filename="../XmlExport.ui" line="64"/>
-        <source>&lt;p&gt;Ampersands will be used for shorter values and CDATA will be used for larger values. This applies only to values that require character escaping. Other values will be exported as they are.&lt;/p&gt;</source>
-        <translation>&lt;p&gt;Για μικρότερες τιμές θα χρησιμοποιούνται ακολουθίες διαφυγής με ampersand και για μεγαλύτερες τιμές θα χρησιμοποιείται CDATA. Αυτό ισχύει μόνο για τιμές που απαιτούν διαφυγή χαρακτήρων. Οι άλλες τιμές θα εξαχθούν ως έχουν.&lt;/p&gt;</translation>
+      <location filename="../XmlExport.ui" line="64"/>
+      <source>&lt;p&gt;Ampersands will be used for shorter values and CDATA will be used for larger values. This applies only to values that require character escaping. Other values will be exported as they are.&lt;/p&gt;</source>
+      <translation>&lt;p&gt;Για μικρότερες τιμές θα χρησιμοποιούνται ακολουθίες διαφυγής με ampersand και για μεγαλύτερες τιμές θα χρησιμοποιείται CDATA. Αυτό ισχύει μόνο για τιμές που απαιτούν διαφυγή χαρακτήρων. Οι άλλες τιμές θα εξαχθούν ως έχουν.&lt;/p&gt;</translation>
     </message>
     <message>
-        <location filename="../XmlExport.ui" line="67"/>
-        <source>Use CDATA and ampersands</source>
-        <translation>Χρήση CDATA και ampersand</translation>
+      <location filename="../XmlExport.ui" line="67"/>
+      <source>Use CDATA and ampersands</source>
+      <translation>Χρήση CDATA και ampersand</translation>
     </message>
     <message>
-        <location filename="../XmlExport.ui" line="83"/>
-        <source>&lt;p&gt;Every value requiring character escepe will be enclosed in CDATA block.&lt;/p&gt;</source>
-        <translation>&lt;p&gt;Κάθε τιμή που απαιτεί διαφυγή χαρακτήρων θα περικλείεται σε μπλοκ CDATA.&lt;/p&gt;</translation>
+      <location filename="../XmlExport.ui" line="83"/>
+      <source>&lt;p&gt;Every value requiring character escepe will be enclosed in CDATA block.&lt;/p&gt;</source>
+      <translation>&lt;p&gt;Κάθε τιμή που απαιτεί διαφυγή χαρακτήρων θα περικλείεται σε μπλοκ CDATA.&lt;/p&gt;</translation>
     </message>
     <message>
-        <location filename="../XmlExport.ui" line="86"/>
-        <source>Always use CDATA</source>
-        <translation>Να χρησιμοποιείται πάντα CDATA</translation>
+      <location filename="../XmlExport.ui" line="86"/>
+      <source>Always use CDATA</source>
+      <translation>Να χρησιμοποιείται πάντα CDATA</translation>
     </message>
     <message>
-        <location filename="../XmlExport.ui" line="99"/>
-        <source>&lt;p&gt;Every character that require esceping will be replaced with its ampersand escape sequence. No CDATA blocks will be used.&lt;/p&gt;</source>
-        <translation>&lt;p&gt;Κάθε χαρακτήρας που απαιτεί διαφυγή θα αντικαθίσταται με την ακολουθία διαφυγής του με ampersand. Δεν θα χρησιμοποιούνται μπλοκ CDATA.&lt;/p&gt;</translation>
+      <location filename="../XmlExport.ui" line="99"/>
+      <source>&lt;p&gt;Every character that require esceping will be replaced with its ampersand escape sequence. No CDATA blocks will be used.&lt;/p&gt;</source>
+      <translation>&lt;p&gt;Κάθε χαρακτήρας που απαιτεί διαφυγή θα αντικαθίσταται με την ακολουθία διαφυγής του με ampersand. Δεν θα χρησιμοποιούνται μπλοκ CDATA.&lt;/p&gt;</translation>
     </message>
     <message>
-        <location filename="../XmlExport.ui" line="102"/>
-        <source>Always use ampersand</source>
-        <translation>Να χρησιμοποιείται πάντα ampersand</translation>
+      <location filename="../XmlExport.ui" line="102"/>
+      <source>Always use ampersand</source>
+      <translation>Να χρησιμοποιείται πάντα ampersand</translation>
     </message>
     <message>
-        <location filename="../XmlExport.ui" line="118"/>
-        <source>Define XML namespace</source>
-        <translation>Ορισμός χώρου ονομάτων XML</translation>
+      <location filename="../XmlExport.ui" line="118"/>
+      <source>Define XML namespace</source>
+      <translation>Ορισμός χώρου ονομάτων XML</translation>
     </message>
-</context>
+  </context>
 </TS>
