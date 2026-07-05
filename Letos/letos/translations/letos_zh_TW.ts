f5ce1cf2cecd3cb61e904547050a35245ab61e71
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 23:02:42 +0200

    New translations letos.ts (Chinese Traditional)
    [ci skip]

--- Letos/letos/translations/letos_zh_TW.ts
+++ Letos/letos/translations/letos_zh_TW.ts
@@ -1,97 +1,97 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="zh-TW" sourcelanguage="en">
-<context>
+  <context>
     <name>QObject</name>
     <message>
-        <location filename="../main.cpp" line="52"/>
-        <source>GUI interface to Letos, a SQLite manager.</source>
-        <translation>Letos 圖形介面，一款 SQLite 管理軟體。</translation>
+      <location filename="../main.cpp" line="52"/>
+      <source>GUI interface to Letos, a SQLite manager.</source>
+      <translation>Letos 圖形介面，一款 SQLite 管理軟體。</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="57"/>
-        <source>Starts the application in safe mode without restoring the previous session. Use this to bypass issues caused by a corrupted session.</source>
-        <translation>以安全模式啟動應用程式，不還原先前的工作階段。使用此選項可繞過因工作階段損毀所造成的問題。</translation>
+      <location filename="../main.cpp" line="57"/>
+      <source>Starts the application in safe mode without restoring the previous session. Use this to bypass issues caused by a corrupted session.</source>
+      <translation>以安全模式啟動應用程式，不還原先前的工作階段。使用此選項可繞過因工作階段損毀所造成的問題。</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="58"/>
-        <source>Do not load any plugins. Can be used alongside safe mode to further isolate potential issues.</source>
-        <translation>不要載入任何外掛。可與安全模式搭配使用，以進一步隔離潛在問題。</translation>
+      <location filename="../main.cpp" line="58"/>
+      <source>Do not load any plugins. Can be used alongside safe mode to further isolate potential issues.</source>
+      <translation>不要載入任何外掛。可與安全模式搭配使用，以進一步隔離潛在問題。</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="59"/>
-        <source>Enables debug messages in console (accessible with F12).</source>
-        <translation>啟用控制檯中的除錯資訊 (透過 F12 訪問)。</translation>
+      <location filename="../main.cpp" line="59"/>
+      <source>Enables debug messages in console (accessible with F12).</source>
+      <translation>啟用控制檯中的除錯資訊 (透過 F12 訪問)。</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="60"/>
-        <source>Redirects debug messages into standard output (forces debug mode).</source>
-        <translation>重定向除錯資訊到標準輸出 (強制除錯模式)。</translation>
+      <location filename="../main.cpp" line="60"/>
+      <source>Redirects debug messages into standard output (forces debug mode).</source>
+      <translation>重定向除錯資訊到標準輸出 (強制除錯模式)。</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="61"/>
-        <source>Redirects debug messages into given file (forces debug mode).</source>
-        <translation>重定向除錯資訊到指定檔案 (強制除錯模式)。</translation>
+      <location filename="../main.cpp" line="61"/>
+      <source>Redirects debug messages into given file (forces debug mode).</source>
+      <translation>重定向除錯資訊到指定檔案 (強制除錯模式)。</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="61"/>
-        <source>log file</source>
-        <translation>日誌檔案</translation>
+      <location filename="../main.cpp" line="61"/>
+      <source>log file</source>
+      <translation>日誌檔案</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="62"/>
-        <source>Enables Lemon parser debug messages for SQL code assistant.</source>
-        <translation>啟用 SQL 程式碼助理的 Lemon 解析器除錯資訊。</translation>
+      <location filename="../main.cpp" line="62"/>
+      <source>Enables Lemon parser debug messages for SQL code assistant.</source>
+      <translation>啟用 SQL 程式碼助理的 Lemon 解析器除錯資訊。</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="63"/>
-        <source>Enables debugging of every single SQL query being sent to any database.</source>
-        <translation>啟用傳送到任何資料庫的每條 SQL 查詢的除錯。</translation>
+      <location filename="../main.cpp" line="63"/>
+      <source>Enables debugging of every single SQL query being sent to any database.</source>
+      <translation>啟用傳送到任何資料庫的每條 SQL 查詢的除錯。</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="64"/>
-        <source>Limits SQL query messages to only the given &lt;database&gt;.</source>
-        <translation>將 SQL 查詢訊息限制為指定的&lt;資料庫&gt;。</translation>
+      <location filename="../main.cpp" line="64"/>
+      <source>Limits SQL query messages to only the given &lt;database&gt;.</source>
+      <translation>將 SQL 查詢訊息限制為指定的&lt;資料庫&gt;。</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="64"/>
-        <source>database</source>
-        <translation>資料庫</translation>
+      <location filename="../main.cpp" line="64"/>
+      <source>database</source>
+      <translation>資料庫</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="56"/>
-        <source>Points to the master configuration file. Read manual at wiki page for more details.</source>
-        <translation>指向主設定檔檔案。更多細節見 Wiki 頁面上的手冊。</translation>
+      <location filename="../main.cpp" line="56"/>
+      <source>Points to the master configuration file. Read manual at wiki page for more details.</source>
+      <translation>指向主設定檔檔案。更多細節見 Wiki 頁面上的手冊。</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="56"/>
-        <source>settings file</source>
-        <translation>設定檔</translation>
+      <location filename="../main.cpp" line="56"/>
+      <source>settings file</source>
+      <translation>設定檔</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="65"/>
-        <source>Enables debugging of Letos&apos;s query executor.</source>
-        <translation>啟用 Letos&apos;s 查詢執行器的除錯。</translation>
+      <location filename="../main.cpp" line="65"/>
+      <source>Enables debugging of Letos&apos;s query executor.</source>
+      <translation>啟用 Letos&apos;s 查詢執行器的除錯。</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="66"/>
-        <source>Lists plugins installed in the Letos and quits.</source>
-        <translation>列出 Letos 中已安裝的外掛然後退出。</translation>
+      <location filename="../main.cpp" line="66"/>
+      <source>Lists plugins installed in the Letos and quits.</source>
+      <translation>列出 Letos 中已安裝的外掛然後退出。</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="79"/>
-        <source>file</source>
-        <translation>檔案</translation>
+      <location filename="../main.cpp" line="79"/>
+      <source>file</source>
+      <translation>檔案</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="79"/>
-        <source>Database file to open</source>
-        <translation>要開啟的資料庫檔案</translation>
+      <location filename="../main.cpp" line="79"/>
+      <source>Database file to open</source>
+      <translation>要開啟的資料庫檔案</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="163"/>
-        <source>Select configuration directory</source>
-        <translation>選擇設定檔目錄</translation>
+      <location filename="../main.cpp" line="163"/>
+      <source>Select configuration directory</source>
+      <translation>選擇設定檔目錄</translation>
     </message>
-</context>
+  </context>
 </TS>
