b200448abd80d252f57472a4ed1302d1fb3a104d
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 13:10:47 +0200

    New translations sqlenterpriseformatter.ts (Persian)
    [ci skip]

--- Plugins/SqlEnterpriseFormatter/translations/SqlEnterpriseFormatter_fa_IR.ts
+++ Plugins/SqlEnterpriseFormatter/translations/SqlEnterpriseFormatter_fa_IR.ts
@@ -1,228 +1,228 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="fa" sourcelanguage="en">
-<context>
+  <context>
     <name>QObject</name>
     <message>
-        <location filename="../sqlenterpriseformatter.cpp" line="89"/>
-        <location filename="../sqlenterpriseformatter.cpp" line="96"/>
-        <source>name</source>
-        <comment>example name wrapper</comment>
-        <translation>نام</translation>
+      <location filename="../sqlenterpriseformatter.cpp" line="89"/>
+      <location filename="../sqlenterpriseformatter.cpp" line="96"/>
+      <source>name</source>
+      <comment>example name wrapper</comment>
+      <translation>نام</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>SqlEnterpriseFormatter</name>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="46"/>
-        <source>Indentation</source>
-        <translation>تورفتگی</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="46"/>
+      <source>Indentation</source>
+      <translation>تورفتگی</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="58"/>
-        <source>Line up keywords in multi-line queries</source>
-        <translation>هم‌تراز کردن کلیدواژه‌ها در پرس‌وجوهای چندخطی</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="58"/>
+      <source>Line up keywords in multi-line queries</source>
+      <translation>هم‌تراز کردن کلیدواژه‌ها در پرس‌وجوهای چندخطی</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="68"/>
-        <source>Indent contents of parenthesis block</source>
-        <translation>تورفتگی دادن به محتوای بلوک پرانتز</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="68"/>
+      <source>Indent contents of parenthesis block</source>
+      <translation>تورفتگی دادن به محتوای بلوک پرانتز</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="91"/>
-        <source>Tab size:</source>
-        <translation>اندازهٔ تب:</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="91"/>
+      <source>Tab size:</source>
+      <translation>اندازهٔ تب:</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="106"/>
-        <source>New lines</source>
-        <translation>خطوط جدید</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="106"/>
+      <source>New lines</source>
+      <translation>خطوط جدید</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="160"/>
-        <source>Before opening parenthesis in column definitions</source>
-        <translation>پیش از پرانتز باز در تعریف‌های ستون</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="160"/>
+      <source>Before opening parenthesis in column definitions</source>
+      <translation>پیش از پرانتز باز در تعریف‌های ستون</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="170"/>
-        <source>After opening parenthesis in column definitions</source>
-        <translation>پس از پرانتز باز در تعریف‌های ستون</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="170"/>
+      <source>After opening parenthesis in column definitions</source>
+      <translation>پس از پرانتز باز در تعریف‌های ستون</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="180"/>
-        <source>Before closing parenthesis in column definitions</source>
-        <translation>پیش از پرانتز بسته در تعریف‌های ستون</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="180"/>
+      <source>Before closing parenthesis in column definitions</source>
+      <translation>پیش از پرانتز بسته در تعریف‌های ستون</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="190"/>
-        <source>After closing parenthesis in column definitions</source>
-        <translation>پس از پرانتز بسته در تعریف‌های ستون</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="190"/>
+      <source>After closing parenthesis in column definitions</source>
+      <translation>پس از پرانتز بسته در تعریف‌های ستون</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="200"/>
-        <source>Before opening parenthesis in expressions</source>
-        <translation>پیش از پرانتز باز در عبارت‌ها</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="200"/>
+      <source>Before opening parenthesis in expressions</source>
+      <translation>پیش از پرانتز باز در عبارت‌ها</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="210"/>
-        <source>After opening parenthesis in expressions</source>
-        <translation>پس از پرانتز باز در عبارت‌ها</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="210"/>
+      <source>After opening parenthesis in expressions</source>
+      <translation>پس از پرانتز باز در عبارت‌ها</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="220"/>
-        <source>Before closing parenthesis in expressions</source>
-        <translation>پیش از پرانتز بسته در عبارت‌ها</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="220"/>
+      <source>Before closing parenthesis in expressions</source>
+      <translation>پیش از پرانتز بسته در عبارت‌ها</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="230"/>
-        <source>After closing parenthesis in expressions</source>
-        <translation>پس از پرانتز بسته در عبارت‌ها</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="230"/>
+      <source>After closing parenthesis in expressions</source>
+      <translation>پس از پرانتز بسته در عبارت‌ها</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="240"/>
-        <source>After JOIN keywords in FROM clause</source>
-        <translation>پس از کلیدواژه‌های JOIN در بند FROM</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="240"/>
+      <source>After JOIN keywords in FROM clause</source>
+      <translation>پس از کلیدواژه‌های JOIN در بند FROM</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="250"/>
-        <source>Put each column constraint in CREATE TABLE into new line</source>
-        <translation>هر محدودیت ستون را در CREATE TABLE در خط جدید قرار بده</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="250"/>
+      <source>Put each column constraint in CREATE TABLE into new line</source>
+      <translation>هر محدودیت ستون را در CREATE TABLE در خط جدید قرار بده</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="260"/>
-        <source>After comma</source>
-        <translation>پس از ویرگول</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="260"/>
+      <source>After comma</source>
+      <translation>پس از ویرگول</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="270"/>
-        <source>After comma in expressions</source>
-        <translation>پس از ویرگول در عبارت‌ها</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="270"/>
+      <source>After comma in expressions</source>
+      <translation>پس از ویرگول در عبارت‌ها</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="280"/>
-        <source>After semicolon</source>
-        <translation>پس از نقطه‌ویرگول</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="280"/>
+      <source>After semicolon</source>
+      <translation>پس از نقطه‌ویرگول</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="290"/>
-        <location filename="../sqlenterpriseformatter.ui" line="512"/>
-        <source>Never before semicolon</source>
-        <translation>هرگز پیش از نقطه‌ویرگول</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="290"/>
+      <location filename="../sqlenterpriseformatter.ui" line="512"/>
+      <source>Never before semicolon</source>
+      <translation>هرگز پیش از نقطه‌ویرگول</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="338"/>
-        <source>White spaces</source>
-        <translation>فاصله‌ها</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="338"/>
+      <source>White spaces</source>
+      <translation>فاصله‌ها</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="392"/>
-        <source>Before comma in lists</source>
-        <translation>پیش از ویرگول در فهرست‌ها</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="392"/>
+      <source>Before comma in lists</source>
+      <translation>پیش از ویرگول در فهرست‌ها</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="402"/>
-        <source>After comma in lists</source>
-        <translation>پس از ویرگول در فهرست‌ها</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="402"/>
+      <source>After comma in lists</source>
+      <translation>پس از ویرگول در فهرست‌ها</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="412"/>
-        <source>Before opening parenthesis</source>
-        <translation>پیش از پرانتز باز</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="412"/>
+      <source>Before opening parenthesis</source>
+      <translation>پیش از پرانتز باز</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="422"/>
-        <source>After opening parenthesis</source>
-        <translation>پس از پرانتز باز</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="422"/>
+      <source>After opening parenthesis</source>
+      <translation>پس از پرانتز باز</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="432"/>
-        <source>Before closing parenthesis</source>
-        <translation>پیش از پرانتز بسته</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="432"/>
+      <source>Before closing parenthesis</source>
+      <translation>پیش از پرانتز بسته</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="442"/>
-        <source>After closing parenthesis</source>
-        <translation>پس از پرانتز بسته</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="442"/>
+      <source>After closing parenthesis</source>
+      <translation>پس از پرانتز بسته</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="452"/>
-        <source>No space between SQL function name and opening parenthesis</source>
-        <translation>بدون فاصله بین نام تابع SQL و پرانتز باز</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="452"/>
+      <source>No space between SQL function name and opening parenthesis</source>
+      <translation>بدون فاصله بین نام تابع SQL و پرانتز باز</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="462"/>
-        <source>Before dot operator (in path to database object)</source>
-        <translation>پیش از عملگر نقطه (در مسیر به شیء پایگاه داده)</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="462"/>
+      <source>Before dot operator (in path to database object)</source>
+      <translation>پیش از عملگر نقطه (در مسیر به شیء پایگاه داده)</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="472"/>
-        <source>After dot operator (in path to database object)</source>
-        <translation>پس از عملگر نقطه (در مسیر به شیء پایگاه داده)</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="472"/>
+      <source>After dot operator (in path to database object)</source>
+      <translation>پس از عملگر نقطه (در مسیر به شیء پایگاه داده)</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="482"/>
-        <source>Before mathematical operator</source>
-        <translation>پیش از عملگر ریاضی</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="482"/>
+      <source>Before mathematical operator</source>
+      <translation>پیش از عملگر ریاضی</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="492"/>
-        <source>After mathematical operator</source>
-        <translation>پس از عملگر ریاضی</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="492"/>
+      <source>After mathematical operator</source>
+      <translation>پس از عملگر ریاضی</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="502"/>
-        <source>Never before comma</source>
-        <translation>هرگز پیش از ویرگول</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="502"/>
+      <source>Never before comma</source>
+      <translation>هرگز پیش از ویرگول</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="558"/>
-        <source>Names</source>
-        <translation>نام‌ها</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="558"/>
+      <source>Names</source>
+      <translation>نام‌ها</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="583"/>
-        <source>Preferred name wrapper</source>
-        <translation>دربرگیرندهٔ نام ترجیحی</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="583"/>
+      <source>Preferred name wrapper</source>
+      <translation>دربرگیرندهٔ نام ترجیحی</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="600"/>
-        <source>Always use name wrapping</source>
-        <translation>همیشه از دربرگیری نام استفاده کن</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="600"/>
+      <source>Always use name wrapping</source>
+      <translation>همیشه از دربرگیری نام استفاده کن</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="610"/>
-        <source>Uppercase data type names</source>
-        <translation>نام‌های نوع داده را با حروف بزرگ بنویس</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="610"/>
+      <source>Uppercase data type names</source>
+      <translation>نام‌های نوع داده را با حروف بزرگ بنویس</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="620"/>
-        <source>Uppercase keywords</source>
-        <translation>کلیدواژه‌ها را با حروف بزرگ بنویس</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="620"/>
+      <source>Uppercase keywords</source>
+      <translation>کلیدواژه‌ها را با حروف بزرگ بنویس</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="631"/>
-        <source>Comments</source>
-        <translation>نظرها</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="631"/>
+      <source>Comments</source>
+      <translation>نظرها</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="637"/>
-        <source>Preferred comment marker (where possible):</source>
-        <translation>نشانگر نظر ترجیحی (در صورت امکان):</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="637"/>
+      <source>Preferred comment marker (where possible):</source>
+      <translation>نشانگر نظر ترجیحی (در صورت امکان):</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="654"/>
-        <source>Move all comments to the line end</source>
-        <translation>همهٔ نظرها را به انتهای خط منتقل کن</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="654"/>
+      <source>Move all comments to the line end</source>
+      <translation>همهٔ نظرها را به انتهای خط منتقل کن</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="664"/>
-        <source>Line up comments at the line end</source>
-        <translation>نظرها را در انتهای خط هم‌تراز کن</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="664"/>
+      <source>Line up comments at the line end</source>
+      <translation>نظرها را در انتهای خط هم‌تراز کن</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="695"/>
-        <source>Preview</source>
-        <translation>پیش‌نمایش</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="695"/>
+      <source>Preview</source>
+      <translation>پیش‌نمایش</translation>
     </message>
-</context>
+  </context>
 </TS>
