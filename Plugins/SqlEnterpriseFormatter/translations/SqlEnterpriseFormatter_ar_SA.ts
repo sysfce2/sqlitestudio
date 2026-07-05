1e46902d0352341034e21e378dd9ea23ae7616f9
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 20:50:01 +0200

    New translations sqlenterpriseformatter.ts (Arabic)
    [ci skip]

--- Plugins/SqlEnterpriseFormatter/translations/SqlEnterpriseFormatter_ar_SA.ts
+++ Plugins/SqlEnterpriseFormatter/translations/SqlEnterpriseFormatter_ar_SA.ts
@@ -1,228 +1,228 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="ar" sourcelanguage="en">
-<context>
+  <context>
     <name>QObject</name>
     <message>
-        <location filename="../sqlenterpriseformatter.cpp" line="89"/>
-        <location filename="../sqlenterpriseformatter.cpp" line="96"/>
-        <source>name</source>
-        <comment>example name wrapper</comment>
-        <translation>الاسم</translation>
+      <location filename="../sqlenterpriseformatter.cpp" line="89"/>
+      <location filename="../sqlenterpriseformatter.cpp" line="96"/>
+      <source>name</source>
+      <comment>example name wrapper</comment>
+      <translation>الاسم</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>SqlEnterpriseFormatter</name>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="46"/>
-        <source>Indentation</source>
-        <translation>المسافة البادئة</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="46"/>
+      <source>Indentation</source>
+      <translation>المسافة البادئة</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="58"/>
-        <source>Line up keywords in multi-line queries</source>
-        <translation>محاذاة الكلمات المفتاحية في الاستعلامات متعددة الأسطر</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="58"/>
+      <source>Line up keywords in multi-line queries</source>
+      <translation>محاذاة الكلمات المفتاحية في الاستعلامات متعددة الأسطر</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="68"/>
-        <source>Indent contents of parenthesis block</source>
-        <translation>إضافة مسافة بادئة إلى محتويات كتلة الأقواس</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="68"/>
+      <source>Indent contents of parenthesis block</source>
+      <translation>إضافة مسافة بادئة إلى محتويات كتلة الأقواس</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="91"/>
-        <source>Tab size:</source>
-        <translation>حجم علامة الجدولة:</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="91"/>
+      <source>Tab size:</source>
+      <translation>حجم علامة الجدولة:</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="106"/>
-        <source>New lines</source>
-        <translation>أسطر جديدة</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="106"/>
+      <source>New lines</source>
+      <translation>أسطر جديدة</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="160"/>
-        <source>Before opening parenthesis in column definitions</source>
-        <translation>قبل القوس الافتتاحي في تعريفات الأعمدة</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="160"/>
+      <source>Before opening parenthesis in column definitions</source>
+      <translation>قبل القوس الافتتاحي في تعريفات الأعمدة</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="170"/>
-        <source>After opening parenthesis in column definitions</source>
-        <translation>بعد القوس الافتتاحي في تعريفات الأعمدة</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="170"/>
+      <source>After opening parenthesis in column definitions</source>
+      <translation>بعد القوس الافتتاحي في تعريفات الأعمدة</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="180"/>
-        <source>Before closing parenthesis in column definitions</source>
-        <translation>قبل القوس الختامي في تعريفات الأعمدة</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="180"/>
+      <source>Before closing parenthesis in column definitions</source>
+      <translation>قبل القوس الختامي في تعريفات الأعمدة</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="190"/>
-        <source>After closing parenthesis in column definitions</source>
-        <translation>بعد القوس الختامي في تعريفات الأعمدة</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="190"/>
+      <source>After closing parenthesis in column definitions</source>
+      <translation>بعد القوس الختامي في تعريفات الأعمدة</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="200"/>
-        <source>Before opening parenthesis in expressions</source>
-        <translation>قبل القوس الافتتاحي في التعبيرات</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="200"/>
+      <source>Before opening parenthesis in expressions</source>
+      <translation>قبل القوس الافتتاحي في التعبيرات</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="210"/>
-        <source>After opening parenthesis in expressions</source>
-        <translation>بعد القوس الافتتاحي في التعبيرات</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="210"/>
+      <source>After opening parenthesis in expressions</source>
+      <translation>بعد القوس الافتتاحي في التعبيرات</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="220"/>
-        <source>Before closing parenthesis in expressions</source>
-        <translation>قبل القوس الختامي في التعبيرات</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="220"/>
+      <source>Before closing parenthesis in expressions</source>
+      <translation>قبل القوس الختامي في التعبيرات</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="230"/>
-        <source>After closing parenthesis in expressions</source>
-        <translation>بعد القوس الختامي في التعبيرات</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="230"/>
+      <source>After closing parenthesis in expressions</source>
+      <translation>بعد القوس الختامي في التعبيرات</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="240"/>
-        <source>After JOIN keywords in FROM clause</source>
-        <translation>بعد كلمات JOIN المفتاحية في عبارة FROM</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="240"/>
+      <source>After JOIN keywords in FROM clause</source>
+      <translation>بعد كلمات JOIN المفتاحية في عبارة FROM</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="250"/>
-        <source>Put each column constraint in CREATE TABLE into new line</source>
-        <translation>وضع كل قيد عمود في CREATE TABLE في سطر جديد</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="250"/>
+      <source>Put each column constraint in CREATE TABLE into new line</source>
+      <translation>وضع كل قيد عمود في CREATE TABLE في سطر جديد</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="260"/>
-        <source>After comma</source>
-        <translation>بعد الفاصلة</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="260"/>
+      <source>After comma</source>
+      <translation>بعد الفاصلة</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="270"/>
-        <source>After comma in expressions</source>
-        <translation>بعد الفاصلة في التعبيرات</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="270"/>
+      <source>After comma in expressions</source>
+      <translation>بعد الفاصلة في التعبيرات</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="280"/>
-        <source>After semicolon</source>
-        <translation>بعد الفاصلة المنقوطة</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="280"/>
+      <source>After semicolon</source>
+      <translation>بعد الفاصلة المنقوطة</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="290"/>
-        <location filename="../sqlenterpriseformatter.ui" line="512"/>
-        <source>Never before semicolon</source>
-        <translation>أبدًا قبل الفاصلة المنقوطة</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="290"/>
+      <location filename="../sqlenterpriseformatter.ui" line="512"/>
+      <source>Never before semicolon</source>
+      <translation>أبدًا قبل الفاصلة المنقوطة</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="338"/>
-        <source>White spaces</source>
-        <translation>المسافات البيضاء</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="338"/>
+      <source>White spaces</source>
+      <translation>المسافات البيضاء</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="392"/>
-        <source>Before comma in lists</source>
-        <translation>قبل الفاصلة في القوائم</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="392"/>
+      <source>Before comma in lists</source>
+      <translation>قبل الفاصلة في القوائم</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="402"/>
-        <source>After comma in lists</source>
-        <translation>بعد الفاصلة في القوائم</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="402"/>
+      <source>After comma in lists</source>
+      <translation>بعد الفاصلة في القوائم</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="412"/>
-        <source>Before opening parenthesis</source>
-        <translation>قبل القوس الافتتاحي</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="412"/>
+      <source>Before opening parenthesis</source>
+      <translation>قبل القوس الافتتاحي</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="422"/>
-        <source>After opening parenthesis</source>
-        <translation>بعد القوس الافتتاحي</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="422"/>
+      <source>After opening parenthesis</source>
+      <translation>بعد القوس الافتتاحي</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="432"/>
-        <source>Before closing parenthesis</source>
-        <translation>قبل القوس الختامي</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="432"/>
+      <source>Before closing parenthesis</source>
+      <translation>قبل القوس الختامي</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="442"/>
-        <source>After closing parenthesis</source>
-        <translation>بعد القوس الختامي</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="442"/>
+      <source>After closing parenthesis</source>
+      <translation>بعد القوس الختامي</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="452"/>
-        <source>No space between SQL function name and opening parenthesis</source>
-        <translation>لا توجد مسافة بين اسم دالة SQL والقوس الافتتاحي</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="452"/>
+      <source>No space between SQL function name and opening parenthesis</source>
+      <translation>لا توجد مسافة بين اسم دالة SQL والقوس الافتتاحي</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="462"/>
-        <source>Before dot operator (in path to database object)</source>
-        <translation>قبل عامل النقطة (في المسار إلى كائن قاعدة البيانات)</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="462"/>
+      <source>Before dot operator (in path to database object)</source>
+      <translation>قبل عامل النقطة (في المسار إلى كائن قاعدة البيانات)</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="472"/>
-        <source>After dot operator (in path to database object)</source>
-        <translation>بعد عامل النقطة (في المسار إلى كائن قاعدة البيانات)</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="472"/>
+      <source>After dot operator (in path to database object)</source>
+      <translation>بعد عامل النقطة (في المسار إلى كائن قاعدة البيانات)</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="482"/>
-        <source>Before mathematical operator</source>
-        <translation>قبل العامل الرياضي</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="482"/>
+      <source>Before mathematical operator</source>
+      <translation>قبل العامل الرياضي</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="492"/>
-        <source>After mathematical operator</source>
-        <translation>بعد العامل الرياضي</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="492"/>
+      <source>After mathematical operator</source>
+      <translation>بعد العامل الرياضي</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="502"/>
-        <source>Never before comma</source>
-        <translation>أبدًا قبل الفاصلة</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="502"/>
+      <source>Never before comma</source>
+      <translation>أبدًا قبل الفاصلة</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="558"/>
-        <source>Names</source>
-        <translation>الأسماء</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="558"/>
+      <source>Names</source>
+      <translation>الأسماء</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="583"/>
-        <source>Preferred name wrapper</source>
-        <translation>مغلّف الاسم المفضّل</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="583"/>
+      <source>Preferred name wrapper</source>
+      <translation>مغلّف الاسم المفضّل</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="600"/>
-        <source>Always use name wrapping</source>
-        <translation>استخدام تغليف الأسماء دائمًا</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="600"/>
+      <source>Always use name wrapping</source>
+      <translation>استخدام تغليف الأسماء دائمًا</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="610"/>
-        <source>Uppercase data type names</source>
-        <translation>تحويل أسماء أنواع البيانات إلى أحرف كبيرة</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="610"/>
+      <source>Uppercase data type names</source>
+      <translation>تحويل أسماء أنواع البيانات إلى أحرف كبيرة</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="620"/>
-        <source>Uppercase keywords</source>
-        <translation>تحويل الكلمات المفتاحية إلى أحرف كبيرة</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="620"/>
+      <source>Uppercase keywords</source>
+      <translation>تحويل الكلمات المفتاحية إلى أحرف كبيرة</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="631"/>
-        <source>Comments</source>
-        <translation>التعليقات</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="631"/>
+      <source>Comments</source>
+      <translation>التعليقات</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="637"/>
-        <source>Preferred comment marker (where possible):</source>
-        <translation>علامة التعليق المفضّلة (حيثما أمكن):</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="637"/>
+      <source>Preferred comment marker (where possible):</source>
+      <translation>علامة التعليق المفضّلة (حيثما أمكن):</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="654"/>
-        <source>Move all comments to the line end</source>
-        <translation>نقل جميع التعليقات إلى نهاية السطر</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="654"/>
+      <source>Move all comments to the line end</source>
+      <translation>نقل جميع التعليقات إلى نهاية السطر</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="664"/>
-        <source>Line up comments at the line end</source>
-        <translation>محاذاة التعليقات عند نهاية السطر</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="664"/>
+      <source>Line up comments at the line end</source>
+      <translation>محاذاة التعليقات عند نهاية السطر</translation>
     </message>
     <message>
-        <location filename="../sqlenterpriseformatter.ui" line="695"/>
-        <source>Preview</source>
-        <translation>معاينة</translation>
+      <location filename="../sqlenterpriseformatter.ui" line="695"/>
+      <source>Preview</source>
+      <translation>معاينة</translation>
     </message>
-</context>
+  </context>
 </TS>
