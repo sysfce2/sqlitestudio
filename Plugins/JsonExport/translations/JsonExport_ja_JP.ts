4f191be0be1ae0fc31d3d6ea0f4032b501088e22
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 13:14:12 +0200

    New translations jsonexport.ts (Japanese)
    [ci skip]

--- Plugins/JsonExport/translations/JsonExport_ja_JP.ts
+++ Plugins/JsonExport/translations/JsonExport_ja_JP.ts
@@ -1,22 +1,22 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="ja" sourcelanguage="en">
-<context>
+  <context>
     <name>JsonExportConfig</name>
     <message>
-        <location filename="../jsonexport.ui" line="20"/>
-        <source>Output format</source>
-        <translation>出力形式</translation>
+      <location filename="../jsonexport.ui" line="20"/>
+      <source>Output format</source>
+      <translation>出力形式</translation>
     </message>
     <message>
-        <location filename="../jsonexport.ui" line="26"/>
-        <source>Format document (new lines, indentation)</source>
-        <translation>ドキュメントを整形（改行、インデント）</translation>
+      <location filename="../jsonexport.ui" line="26"/>
+      <source>Format document (new lines, indentation)</source>
+      <translation>ドキュメントを整形（改行、インデント）</translation>
     </message>
     <message>
-        <location filename="../jsonexport.ui" line="42"/>
-        <source>Compress (everything in one line)</source>
-        <translation>圧縮（すべてを1行にする）</translation>
+      <location filename="../jsonexport.ui" line="42"/>
+      <source>Compress (everything in one line)</source>
+      <translation>圧縮（すべてを1行にする）</translation>
     </message>
-</context>
+  </context>
 </TS>
