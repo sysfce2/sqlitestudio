4eb1499939af26ccf07902eeff9a91d2590ccc9a
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 13:17:29 +0200

    New translations scriptingtcl.ts (Slovak)
    [ci skip]

--- Plugins/ScriptingTcl/translations/ScriptingTcl_sk_SK.ts
+++ Plugins/ScriptingTcl/translations/ScriptingTcl_sk_SK.ts
@@ -1,27 +1,27 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="sk" sourcelanguage="en">
-<context>
+  <context>
     <name>ScriptingTcl</name>
     <message>
-        <location filename="../scriptingtcl.cpp" line="428"/>
-        <source>No database available in current context, while called Tcl&apos;s &apos;%1&apos; command.</source>
-        <translation>V aktuálnom kontexte nie je dostupná žiadna databáza pri volaní príkazu Tcl&apos;s &apos;%1&apos;.</translation>
+      <location filename="../scriptingtcl.cpp" line="428"/>
+      <source>No database available in current context, while called Tcl&apos;s &apos;%1&apos; command.</source>
+      <translation>V aktuálnom kontexte nie je dostupná žiadna databáza pri volaní príkazu Tcl&apos;s &apos;%1&apos;.</translation>
     </message>
     <message>
-        <location filename="../scriptingtcl.cpp" line="450"/>
-        <source>Invalid &apos;%1&apos; command syntax. Should be: %2</source>
-        <translation>Neplatná syntax príkazu &apos;%1&apos;. Malo by byť: %2</translation>
+      <location filename="../scriptingtcl.cpp" line="450"/>
+      <source>Invalid &apos;%1&apos; command syntax. Should be: %2</source>
+      <translation>Neplatná syntax príkazu &apos;%1&apos;. Malo by byť: %2</translation>
     </message>
     <message>
-        <location filename="../scriptingtcl.cpp" line="462"/>
-        <source>Error from Tcl&apos;s &apos;%1&apos; command: %2</source>
-        <translation>Chyba z príkazu Tcl&apos;s &apos;%1&apos;: %2</translation>
+      <location filename="../scriptingtcl.cpp" line="462"/>
+      <source>Error from Tcl&apos;s &apos;%1&apos; command: %2</source>
+      <translation>Chyba z príkazu Tcl&apos;s &apos;%1&apos;: %2</translation>
     </message>
     <message>
-        <location filename="../scriptingtcl.cpp" line="593"/>
-        <source>Error from Tcl&apos;s&apos; &apos;%1&apos; command: %2</source>
-        <translation>Chyba z príkazu Tcl&apos;s&apos; &apos;%1&apos;: %2</translation>
+      <location filename="../scriptingtcl.cpp" line="593"/>
+      <source>Error from Tcl&apos;s&apos; &apos;%1&apos; command: %2</source>
+      <translation>Chyba z príkazu Tcl&apos;s&apos; &apos;%1&apos;: %2</translation>
     </message>
-</context>
+  </context>
 </TS>
