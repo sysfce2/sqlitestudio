41c3457178578dcb3a65a54d0cf794a6b75eee2d
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 20:51:49 +0200

    New translations xmlexport.ts (Czech)
    [ci skip]

--- Plugins/XmlExport/translations/XmlExport_cs_CZ.ts
+++ Plugins/XmlExport/translations/XmlExport_cs_CZ.ts
@@ -1,70 +1,70 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="cs" sourcelanguage="en">
-<context>
+  <context>
     <name>XmlExport</name>
     <message>
-        <location filename="../xmlexport.cpp" line="37"/>
-        <source>Enter the namespace to use (for example: http://my.namespace.org)</source>
-        <translation>Zadejte jmenný prostor, který se má použít (například: http://my.namespace.org)</translation>
+      <location filename="../xmlexport.cpp" line="37"/>
+      <source>Enter the namespace to use (for example: http://my.namespace.org)</source>
+      <translation>Zadejte jmenný prostor, který se má použít (například: http://my.namespace.org)</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>XmlExportConfig</name>
     <message>
-        <location filename="../XmlExport.ui" line="20"/>
-        <source>Output format</source>
-        <translation>Výstupní formát</translation>
+      <location filename="../XmlExport.ui" line="20"/>
+      <source>Output format</source>
+      <translation>Výstupní formát</translation>
     </message>
     <message>
-        <location filename="../XmlExport.ui" line="26"/>
-        <source>Format document (new lines, indentation)</source>
-        <translation>Formátovat dokument (nové řádky, odsazení)</translation>
+      <location filename="../XmlExport.ui" line="26"/>
+      <source>Format document (new lines, indentation)</source>
+      <translation>Formátovat dokument (nové řádky, odsazení)</translation>
     </message>
     <message>
-        <location filename="../XmlExport.ui" line="42"/>
-        <source>Compress (everything in one line)</source>
-        <translation>Komprimovat (vše na jednom řádku)</translation>
+      <location filename="../XmlExport.ui" line="42"/>
+      <source>Compress (everything in one line)</source>
+      <translation>Komprimovat (vše na jednom řádku)</translation>
     </message>
     <message>
-        <location filename="../XmlExport.ui" line="58"/>
-        <source>Special characters escaping</source>
-        <translation>Escapování speciálních znaků</translation>
+      <location filename="../XmlExport.ui" line="58"/>
+      <source>Special characters escaping</source>
+      <translation>Escapování speciálních znaků</translation>
     </message>
     <message>
-        <location filename="../XmlExport.ui" line="64"/>
-        <source>&lt;p&gt;Ampersands will be used for shorter values and CDATA will be used for larger values. This applies only to values that require character escaping. Other values will be exported as they are.&lt;/p&gt;</source>
-        <translation>&lt;p&gt;Pro kratší hodnoty se použijí ampersandy a pro delší hodnoty se použije CDATA. Platí to pouze pro hodnoty, které vyžadují escapování znaků. Ostatní hodnoty budou exportovány beze změny.&lt;/p&gt;</translation>
+      <location filename="../XmlExport.ui" line="64"/>
+      <source>&lt;p&gt;Ampersands will be used for shorter values and CDATA will be used for larger values. This applies only to values that require character escaping. Other values will be exported as they are.&lt;/p&gt;</source>
+      <translation>&lt;p&gt;Pro kratší hodnoty se použijí ampersandy a pro delší hodnoty se použije CDATA. Platí to pouze pro hodnoty, které vyžadují escapování znaků. Ostatní hodnoty budou exportovány beze změny.&lt;/p&gt;</translation>
     </message>
     <message>
-        <location filename="../XmlExport.ui" line="67"/>
-        <source>Use CDATA and ampersands</source>
-        <translation>Použít CDATA a ampersandy</translation>
+      <location filename="../XmlExport.ui" line="67"/>
+      <source>Use CDATA and ampersands</source>
+      <translation>Použít CDATA a ampersandy</translation>
     </message>
     <message>
-        <location filename="../XmlExport.ui" line="83"/>
-        <source>&lt;p&gt;Every value requiring character escepe will be enclosed in CDATA block.&lt;/p&gt;</source>
-        <translation>&lt;p&gt;Každá hodnota vyžadující escapování znaků bude uzavřena v bloku CDATA.&lt;/p&gt;</translation>
+      <location filename="../XmlExport.ui" line="83"/>
+      <source>&lt;p&gt;Every value requiring character escepe will be enclosed in CDATA block.&lt;/p&gt;</source>
+      <translation>&lt;p&gt;Každá hodnota vyžadující escapování znaků bude uzavřena v bloku CDATA.&lt;/p&gt;</translation>
     </message>
     <message>
-        <location filename="../XmlExport.ui" line="86"/>
-        <source>Always use CDATA</source>
-        <translation>Vždy použít CDATA</translation>
+      <location filename="../XmlExport.ui" line="86"/>
+      <source>Always use CDATA</source>
+      <translation>Vždy použít CDATA</translation>
     </message>
     <message>
-        <location filename="../XmlExport.ui" line="99"/>
-        <source>&lt;p&gt;Every character that require esceping will be replaced with its ampersand escape sequence. No CDATA blocks will be used.&lt;/p&gt;</source>
-        <translation>&lt;p&gt;Každý znak, který vyžaduje escapování, bude nahrazen svou ampersandovou escape sekvencí. Bloky CDATA nebudou použity.&lt;/p&gt;</translation>
+      <location filename="../XmlExport.ui" line="99"/>
+      <source>&lt;p&gt;Every character that require esceping will be replaced with its ampersand escape sequence. No CDATA blocks will be used.&lt;/p&gt;</source>
+      <translation>&lt;p&gt;Každý znak, který vyžaduje escapování, bude nahrazen svou ampersandovou escape sekvencí. Bloky CDATA nebudou použity.&lt;/p&gt;</translation>
     </message>
     <message>
-        <location filename="../XmlExport.ui" line="102"/>
-        <source>Always use ampersand</source>
-        <translation>Vždy použít ampersand</translation>
+      <location filename="../XmlExport.ui" line="102"/>
+      <source>Always use ampersand</source>
+      <translation>Vždy použít ampersand</translation>
     </message>
     <message>
-        <location filename="../XmlExport.ui" line="118"/>
-        <source>Define XML namespace</source>
-        <translation>Definovat jmenný prostor XML</translation>
+      <location filename="../XmlExport.ui" line="118"/>
+      <source>Define XML namespace</source>
+      <translation>Definovat jmenný prostor XML</translation>
     </message>
-</context>
+  </context>
 </TS>
