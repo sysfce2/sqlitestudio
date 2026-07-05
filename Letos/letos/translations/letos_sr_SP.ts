6d235b0035273669dd6a523db70f90109d19fb00
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 23:00:30 +0200

    New translations letos.ts (Serbian (Cyrillic))
    [ci skip]

--- Letos/letos/translations/letos_sr_SP.ts
+++ Letos/letos/translations/letos_sr_SP.ts
@@ -1,97 +1,97 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="sr" sourcelanguage="en">
-<context>
+  <context>
     <name>QObject</name>
     <message>
-        <location filename="../main.cpp" line="52"/>
-        <source>GUI interface to Letos, a SQLite manager.</source>
-        <translation>Графички кориснички интерфејс за Letos, SQLite менаџер.</translation>
+      <location filename="../main.cpp" line="52"/>
+      <source>GUI interface to Letos, a SQLite manager.</source>
+      <translation>Графички кориснички интерфејс за Letos, SQLite менаџер.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="57"/>
-        <source>Starts the application in safe mode without restoring the previous session. Use this to bypass issues caused by a corrupted session.</source>
-        <translation>Покреће апликацију у безбедном режиму без враћања претходне сесије. Користите ово да заобиђете проблеме изазване оштећеном сесијом.</translation>
+      <location filename="../main.cpp" line="57"/>
+      <source>Starts the application in safe mode without restoring the previous session. Use this to bypass issues caused by a corrupted session.</source>
+      <translation>Покреће апликацију у безбедном режиму без враћања претходне сесије. Користите ово да заобиђете проблеме изазване оштећеном сесијом.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="58"/>
-        <source>Do not load any plugins. Can be used alongside safe mode to further isolate potential issues.</source>
-        <translation>Не учитава никакве плагине. Може се користити заједно са безбедним режимом ради даље изолације потенцијалних проблема.</translation>
+      <location filename="../main.cpp" line="58"/>
+      <source>Do not load any plugins. Can be used alongside safe mode to further isolate potential issues.</source>
+      <translation>Не учитава никакве плагине. Може се користити заједно са безбедним режимом ради даље изолације потенцијалних проблема.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="59"/>
-        <source>Enables debug messages in console (accessible with F12).</source>
-        <translation>Омогућава дебаг поруке у конзоли (доступно преко F12).</translation>
+      <location filename="../main.cpp" line="59"/>
+      <source>Enables debug messages in console (accessible with F12).</source>
+      <translation>Омогућава дебаг поруке у конзоли (доступно преко F12).</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="60"/>
-        <source>Redirects debug messages into standard output (forces debug mode).</source>
-        <translation>Преусмерава дебаг поруке на стандардни излаз (форсира дебаг режим).</translation>
+      <location filename="../main.cpp" line="60"/>
+      <source>Redirects debug messages into standard output (forces debug mode).</source>
+      <translation>Преусмерава дебаг поруке на стандардни излаз (форсира дебаг режим).</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="61"/>
-        <source>Redirects debug messages into given file (forces debug mode).</source>
-        <translation>Преусмерава дебаг поруке у наведену датотеку (форсира дебаг режим).</translation>
+      <location filename="../main.cpp" line="61"/>
+      <source>Redirects debug messages into given file (forces debug mode).</source>
+      <translation>Преусмерава дебаг поруке у наведену датотеку (форсира дебаг режим).</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="61"/>
-        <source>log file</source>
-        <translation>лог датотека</translation>
+      <location filename="../main.cpp" line="61"/>
+      <source>log file</source>
+      <translation>лог датотека</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="62"/>
-        <source>Enables Lemon parser debug messages for SQL code assistant.</source>
-        <translation>Омогућава дебаг поруке Lemon парсера за асистента за SQL код.</translation>
+      <location filename="../main.cpp" line="62"/>
+      <source>Enables Lemon parser debug messages for SQL code assistant.</source>
+      <translation>Омогућава дебаг поруке Lemon парсера за асистента за SQL код.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="63"/>
-        <source>Enables debugging of every single SQL query being sent to any database.</source>
-        <translation>Омогућава дебаговање сваког појединачног SQL упита који се шаље било којој бази података.</translation>
+      <location filename="../main.cpp" line="63"/>
+      <source>Enables debugging of every single SQL query being sent to any database.</source>
+      <translation>Омогућава дебаговање сваког појединачног SQL упита који се шаље било којој бази података.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="64"/>
-        <source>Limits SQL query messages to only the given &lt;database&gt;.</source>
-        <translation>Ограничава поруке SQL упита само на наведену &lt;базу података&gt;.</translation>
+      <location filename="../main.cpp" line="64"/>
+      <source>Limits SQL query messages to only the given &lt;database&gt;.</source>
+      <translation>Ограничава поруке SQL упита само на наведену &lt;базу података&gt;.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="64"/>
-        <source>database</source>
-        <translation>база података</translation>
+      <location filename="../main.cpp" line="64"/>
+      <source>database</source>
+      <translation>база података</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="56"/>
-        <source>Points to the master configuration file. Read manual at wiki page for more details.</source>
-        <translation>Указује на главну конфигурациону датотеку. За више детаља прочитајте приручник на вики страници.</translation>
+      <location filename="../main.cpp" line="56"/>
+      <source>Points to the master configuration file. Read manual at wiki page for more details.</source>
+      <translation>Указује на главну конфигурациону датотеку. За више детаља прочитајте приручник на вики страници.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="56"/>
-        <source>settings file</source>
-        <translation>датотека подешавања</translation>
+      <location filename="../main.cpp" line="56"/>
+      <source>settings file</source>
+      <translation>датотека подешавања</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="65"/>
-        <source>Enables debugging of Letos&apos;s query executor.</source>
-        <translation>Омогућава дебаговање Letos&apos; извршаваоца упита.</translation>
+      <location filename="../main.cpp" line="65"/>
+      <source>Enables debugging of Letos&apos;s query executor.</source>
+      <translation>Омогућава дебаговање Letos&apos; извршаваоца упита.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="66"/>
-        <source>Lists plugins installed in the Letos and quits.</source>
-        <translation>Исписује плагине инсталиране у Letos-у и излази.</translation>
+      <location filename="../main.cpp" line="66"/>
+      <source>Lists plugins installed in the Letos and quits.</source>
+      <translation>Исписује плагине инсталиране у Letos-у и излази.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="79"/>
-        <source>file</source>
-        <translation>датотека</translation>
+      <location filename="../main.cpp" line="79"/>
+      <source>file</source>
+      <translation>датотека</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="79"/>
-        <source>Database file to open</source>
-        <translation>Датотека базе података за отварање</translation>
+      <location filename="../main.cpp" line="79"/>
+      <source>Database file to open</source>
+      <translation>Датотека базе података за отварање</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="163"/>
-        <source>Select configuration directory</source>
-        <translation>Изаберите конфигурациони директоријум</translation>
+      <location filename="../main.cpp" line="163"/>
+      <source>Select configuration directory</source>
+      <translation>Изаберите конфигурациони директоријум</translation>
     </message>
-</context>
+  </context>
 </TS>
