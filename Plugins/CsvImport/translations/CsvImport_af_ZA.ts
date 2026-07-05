8bf21c344ac57af8819aac6045b5668659e594fb
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 20:45:20 +0200

    New translations csvimport.ts (Afrikaans)
    [ci skip]

--- Plugins/CsvImport/translations/CsvImport_af_ZA.ts
+++ Plugins/CsvImport/translations/CsvImport_af_ZA.ts
@@ -1,90 +1,90 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="af" sourcelanguage="en">
-<context>
+  <context>
     <name>CsvImport</name>
     <message>
-        <location filename="../csvimport.cpp" line="32"/>
-        <source>Cannot read file %1</source>
-        <translation>Kan nie lêer %1 lees nie</translation>
+      <location filename="../csvimport.cpp" line="32"/>
+      <source>Cannot read file %1</source>
+      <translation>Kan nie lêer %1 lees nie</translation>
     </message>
     <message>
-        <location filename="../csvimport.cpp" line="64"/>
-        <source>Could not find any data in the file %1.</source>
-        <translation>Kon geen data in die lêer %1 vind nie.</translation>
+      <location filename="../csvimport.cpp" line="64"/>
+      <source>Could not find any data in the file %1.</source>
+      <translation>Kon geen data in die lêer %1 vind nie.</translation>
     </message>
     <message>
-        <location filename="../csvimport.cpp" line="170"/>
-        <source>Enter the custom separator character.</source>
-        <translation>Voer die pasgemaakte skeidingsteken in.</translation>
+      <location filename="../csvimport.cpp" line="170"/>
+      <source>Enter the custom separator character.</source>
+      <translation>Voer die pasgemaakte skeidingsteken in.</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>csvImportOptions</name>
     <message>
-        <location filename="../CsvImportOptions.ui" line="37"/>
-        <source>, (comma)</source>
-        <translation>, (komma)</translation>
+      <location filename="../CsvImportOptions.ui" line="37"/>
+      <source>, (comma)</source>
+      <translation>, (komma)</translation>
     </message>
     <message>
-        <location filename="../CsvImportOptions.ui" line="42"/>
-        <source>; (semicolon)</source>
-        <translation>; (kommapunt)</translation>
+      <location filename="../CsvImportOptions.ui" line="42"/>
+      <source>; (semicolon)</source>
+      <translation>; (kommapunt)</translation>
     </message>
     <message>
-        <location filename="../CsvImportOptions.ui" line="47"/>
-        <source>\t (tab)</source>
-        <translation>\t (tab)</translation>
+      <location filename="../CsvImportOptions.ui" line="47"/>
+      <source>\t (tab)</source>
+      <translation>\t (tab)</translation>
     </message>
     <message>
-        <location filename="../CsvImportOptions.ui" line="52"/>
-        <source>  (whitespace)</source>
-        <translation>  (witspasie)</translation>
+      <location filename="../CsvImportOptions.ui" line="52"/>
+      <source>  (whitespace)</source>
+      <translation>  (witspasie)</translation>
     </message>
     <message>
-        <location filename="../CsvImportOptions.ui" line="57"/>
-        <source>Custom:</source>
-        <translation>Pasgemaak:</translation>
+      <location filename="../CsvImportOptions.ui" line="57"/>
+      <source>Custom:</source>
+      <translation>Pasgemaak:</translation>
     </message>
     <message>
-        <location filename="../CsvImportOptions.ui" line="65"/>
-        <source>Column separator:</source>
-        <translation>Kolomskeidingsteken:</translation>
+      <location filename="../CsvImportOptions.ui" line="65"/>
+      <source>Column separator:</source>
+      <translation>Kolomskeidingsteken:</translation>
     </message>
     <message>
-        <location filename="../CsvImportOptions.ui" line="108"/>
-        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;If enabled, the &amp;quot; character will be interpreted as standard CSV value quotation mark, causing the string to be treated as single value until the next (ending) &amp;quot; character. If disabled, the &amp;quot; will be treated as any other character. Usually you want this to be enabled.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Indien geaktiveer, sal die &amp;quot;-teken as die standaard CSV-waarde-aanhalingsteken geïnterpreteer word, wat veroorsaak dat die string as ’n enkele waarde behandel word tot by die volgende (sluitende) &amp;quot;-teken. Indien gedeaktiveer, sal die &amp;quot; soos enige ander teken behandel word. Gewoonlik wil jy hê dit moet geaktiveer wees.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+      <location filename="../CsvImportOptions.ui" line="108"/>
+      <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;If enabled, the &amp;quot; character will be interpreted as standard CSV value quotation mark, causing the string to be treated as single value until the next (ending) &amp;quot; character. If disabled, the &amp;quot; will be treated as any other character. Usually you want this to be enabled.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+      <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Indien geaktiveer, sal die &amp;quot;-teken as die standaard CSV-waarde-aanhalingsteken geïnterpreteer word, wat veroorsaak dat die string as ’n enkele waarde behandel word tot by die volgende (sluitende) &amp;quot;-teken. Indien gedeaktiveer, sal die &amp;quot; soos enige ander teken behandel word. Gewoonlik wil jy hê dit moet geaktiveer wees.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
-        <location filename="../CsvImportOptions.ui" line="111"/>
-        <source>Interpret &quot; as a value quotation mark</source>
-        <translation>Interpreteer &quot; as ’n waarde-aanhalingsteken</translation>
+      <location filename="../CsvImportOptions.ui" line="111"/>
+      <source>Interpret &quot; as a value quotation mark</source>
+      <translation>Interpreteer &quot; as ’n waarde-aanhalingsteken</translation>
     </message>
     <message>
-        <location filename="../CsvImportOptions.ui" line="20"/>
-        <source>&lt;p&gt;Enable this if the first data line in your CSV file represents column names. You don&apos;t want column names to be imported into the table as a regular data.&lt;/p&gt;</source>
-        <translation>&lt;p&gt;Aktiveer dit as die eerste datareël in jou CSV-lêer kolomname verteenwoordig. Jy wil nie hê kolomname moet as gewone data in die tabel ingevoer word nie.&lt;/p&gt;</translation>
+      <location filename="../CsvImportOptions.ui" line="20"/>
+      <source>&lt;p&gt;Enable this if the first data line in your CSV file represents column names. You don&apos;t want column names to be imported into the table as a regular data.&lt;/p&gt;</source>
+      <translation>&lt;p&gt;Aktiveer dit as die eerste datareël in jou CSV-lêer kolomname verteenwoordig. Jy wil nie hê kolomname moet as gewone data in die tabel ingevoer word nie.&lt;/p&gt;</translation>
     </message>
     <message>
-        <location filename="../CsvImportOptions.ui" line="23"/>
-        <source>First line represents CSV column names</source>
-        <translation>Eerste reël verteenwoordig CSV-kolomname</translation>
+      <location filename="../CsvImportOptions.ui" line="23"/>
+      <source>First line represents CSV column names</source>
+      <translation>Eerste reël verteenwoordig CSV-kolomname</translation>
     </message>
     <message>
-        <location filename="../CsvImportOptions.ui" line="72"/>
-        <source>If enabled, then text value provided on the right will be interpreted as NULL</source>
-        <translation>Indien geaktiveer, sal die tekswaarde wat regs verskaf word as NULL geïnterpreteer word</translation>
+      <location filename="../CsvImportOptions.ui" line="72"/>
+      <source>If enabled, then text value provided on the right will be interpreted as NULL</source>
+      <translation>Indien geaktiveer, sal die tekswaarde wat regs verskaf word as NULL geïnterpreteer word</translation>
     </message>
     <message>
-        <location filename="../CsvImportOptions.ui" line="75"/>
-        <source>NULL values:</source>
-        <translation>NULL-waardes:</translation>
+      <location filename="../CsvImportOptions.ui" line="75"/>
+      <source>NULL values:</source>
+      <translation>NULL-waardes:</translation>
     </message>
     <message>
-        <location filename="../CsvImportOptions.ui" line="85"/>
-        <source>If your CSV data contains null values, define how are they represented in the CSV.</source>
-        <translation>As jou CSV-data null-waardes bevat, definieer hoe hulle in die CSV voorgestel word.</translation>
+      <location filename="../CsvImportOptions.ui" line="85"/>
+      <source>If your CSV data contains null values, define how are they represented in the CSV.</source>
+      <translation>As jou CSV-data null-waardes bevat, definieer hoe hulle in die CSV voorgestel word.</translation>
     </message>
-</context>
+  </context>
 </TS>
