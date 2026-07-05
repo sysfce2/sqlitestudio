d3fce14bedd94206aaf34a7c15ed05c241e872e7
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 20:54:06 +0200

    New translations dbsqlitemc.ts (Slovak)
    [ci skip]

--- Plugins/DbSQLite3MC/translations/DbSqliteMc_sk_SK.ts
+++ Plugins/DbSQLite3MC/translations/DbSqliteMc_sk_SK.ts
@@ -1,46 +1,46 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="sk" sourcelanguage="en">
-<context>
+  <context>
     <name>DbSqliteMc</name>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="42"/>
-        <source>Password (key)</source>
-        <translation>Heslo (kľúč)</translation>
+      <location filename="../dbsqlitemc.cpp" line="42"/>
+      <source>Password (key)</source>
+      <translation>Heslo (kľúč)</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="43"/>
-        <source>Leave empty to create or connect to decrypted database.</source>
-        <translation>Ponechajte prázdne, ak chcete vytvoriť nešifrovanú databázu alebo sa k nej pripojiť.</translation>
+      <location filename="../dbsqlitemc.cpp" line="43"/>
+      <source>Leave empty to create or connect to decrypted database.</source>
+      <translation>Ponechajte prázdne, ak chcete vytvoriť nešifrovanú databázu alebo sa k nej pripojiť.</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="44"/>
-        <source>Encryption password</source>
-        <translation>Heslo šifrovania</translation>
+      <location filename="../dbsqlitemc.cpp" line="44"/>
+      <source>Encryption password</source>
+      <translation>Heslo šifrovania</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="50"/>
-        <source>Cipher</source>
-        <translation>Šifra</translation>
+      <location filename="../dbsqlitemc.cpp" line="50"/>
+      <source>Cipher</source>
+      <translation>Šifra</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="51"/>
-        <source>Cipher determines encryption algorithm used to encrypt the database.</source>
-        <translation>Šifra určuje šifrovací algoritmus použitý na zašifrovanie databázy.</translation>
+      <location filename="../dbsqlitemc.cpp" line="51"/>
+      <source>Cipher determines encryption algorithm used to encrypt the database.</source>
+      <translation>Šifra určuje šifrovací algoritmus použitý na zašifrovanie databázy.</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="61"/>
-        <source>Cipher configuration (optional)</source>
-        <translation>Konfigurácia šifry (voliteľné)</translation>
+      <location filename="../dbsqlitemc.cpp" line="61"/>
+      <source>Cipher configuration (optional)</source>
+      <translation>Konfigurácia šifry (voliteľné)</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="62"/>
-        <source>PRAGMA statements to customize SQLite3 Multiple Ciphers configuration, such as KDF iterations, legacy mode, etc.
+      <location filename="../dbsqlitemc.cpp" line="62"/>
+      <source>PRAGMA statements to customize SQLite3 Multiple Ciphers configuration, such as KDF iterations, legacy mode, etc.
 They will be executed upon each opening of the database.
 See documentation for SQLite3 Multiple Ciphers for details.</source>
-        <translation>Príkazy PRAGMA na prispôsobenie konfigurácie SQLite3 Multiple Ciphers, ako sú iterácie KDF, režim kompatibility so staršími verziami atď.
+      <translation>Príkazy PRAGMA na prispôsobenie konfigurácie SQLite3 Multiple Ciphers, ako sú iterácie KDF, režim kompatibility so staršími verziami atď.
 Vykonajú sa pri každom otvorení databázy.
 Podrobnosti nájdete v dokumentácii k SQLite3 Multiple Ciphers.</translation>
     </message>
-</context>
+  </context>
 </TS>
