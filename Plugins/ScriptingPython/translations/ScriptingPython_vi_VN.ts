d4b33f92c6ead7c4b9bb71383d3c8ececad0d693
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 20:53:20 +0200

    New translations scriptingpython.ts (Vietnamese)
    [ci skip]

--- Plugins/ScriptingPython/translations/ScriptingPython_vi_VN.ts
+++ Plugins/ScriptingPython/translations/ScriptingPython_vi_VN.ts
@@ -1,50 +1,50 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="vi" sourcelanguage="en">
-<context>
+  <context>
     <name>QObject</name>
     <message>
-        <location filename="../scriptingpython.cpp" line="730"/>
-        <source>Invalid use of %1 function. Expected %2 arguments, but got %3.</source>
-        <translation>Sử dụng hàm %1 không hợp lệ. Dự kiến %2 đối số, nhưng nhận được %3.</translation>
+      <location filename="../scriptingpython.cpp" line="730"/>
+      <source>Invalid use of %1 function. Expected %2 arguments, but got %3.</source>
+      <translation>Sử dụng hàm %1 không hợp lệ. Dự kiến %2 đối số, nhưng nhận được %3.</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="739"/>
-        <source>Unknown error from function %1.</source>
-        <translation>Lỗi không xác định từ hàm %1.</translation>
+      <location filename="../scriptingpython.cpp" line="739"/>
+      <source>Unknown error from function %1.</source>
+      <translation>Lỗi không xác định từ hàm %1.</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="780"/>
-        <source>Could not calculate string representation of the Python object passed as argument to the function %1.</source>
-        <translation>Không thể tính toán biểu diễn chuỗi của đối tượng Python được truyền làm đối số cho hàm %1.</translation>
+      <location filename="../scriptingpython.cpp" line="780"/>
+      <source>Could not calculate string representation of the Python object passed as argument to the function %1.</source>
+      <translation>Không thể tính toán biểu diễn chuỗi của đối tượng Python được truyền làm đối số cho hàm %1.</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="802"/>
-        <source>Could not find execution context for function %1. This is a bug of Python plugin. Please report it.</source>
-        <translation>Không thể tìm thấy ngữ cảnh thực thi cho hàm %1. Đây là lỗi của plugin Python. Vui lòng báo cáo lỗi này.</translation>
+      <location filename="../scriptingpython.cpp" line="802"/>
+      <source>Could not find execution context for function %1. This is a bug of Python plugin. Please report it.</source>
+      <translation>Không thể tìm thấy ngữ cảnh thực thi cho hàm %1. Đây là lỗi của plugin Python. Vui lòng báo cáo lỗi này.</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="827"/>
-        <source>Error from Python function %1: %2</source>
-        <translation>Lỗi từ hàm Python %1: %2</translation>
+      <location filename="../scriptingpython.cpp" line="827"/>
+      <source>Error from Python function %1: %2</source>
+      <translation>Lỗi từ hàm Python %1: %2</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>ScriptingPython</name>
     <message>
-        <location filename="../scriptingpython.ui" line="20"/>
-        <source>Python library path</source>
-        <translation>Đường dẫn thư viện Python</translation>
+      <location filename="../scriptingpython.ui" line="20"/>
+      <source>Python library path</source>
+      <translation>Đường dẫn thư viện Python</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.ui" line="29"/>
-        <source>Choose a Python library file</source>
-        <translation>Chọn tệp thư viện Python</translation>
+      <location filename="../scriptingpython.ui" line="29"/>
+      <source>Choose a Python library file</source>
+      <translation>Chọn tệp thư viện Python</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="370"/>
-        <source>The plugin is not configured properly.</source>
-        <translation>Plugin chưa được cấu hình đúng cách.</translation>
+      <location filename="../scriptingpython.cpp" line="370"/>
+      <source>The plugin is not configured properly.</source>
+      <translation>Plugin chưa được cấu hình đúng cách.</translation>
     </message>
-</context>
+  </context>
 </TS>
