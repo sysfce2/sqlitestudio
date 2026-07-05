108b2ebfcfc2dd8f6753a467e7928c585b4a2e65
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 23:04:11 +0200

    New translations printing.ts (Persian)
    [ci skip]

--- Plugins/Printing/translations/Printing_fa_IR.ts
+++ Plugins/Printing/translations/Printing_fa_IR.ts
@@ -1,40 +1,40 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="fa" sourcelanguage="en">
-<context>
+  <context>
     <name>Printing</name>
     <message>
-        <location filename="../printing.cpp" line="36"/>
-        <source>Print data</source>
-        <translation>چاپ داده‌ها</translation>
+      <location filename="../printing.cpp" line="36"/>
+      <source>Print data</source>
+      <translation>چاپ داده‌ها</translation>
     </message>
     <message>
-        <location filename="../printing.cpp" line="38"/>
-        <source>Print query</source>
-        <translation>چاپ پرس‌وجو</translation>
+      <location filename="../printing.cpp" line="38"/>
+      <source>Print query</source>
+      <translation>چاپ پرس‌وجو</translation>
     </message>
     <message>
-        <location filename="../printing.cpp" line="75"/>
-        <source>No data to print.</source>
-        <translation>داده‌ای برای چاپ وجود ندارد.</translation>
+      <location filename="../printing.cpp" line="75"/>
+      <source>No data to print.</source>
+      <translation>داده‌ای برای چاپ وجود ندارد.</translation>
     </message>
     <message>
-        <location filename="../printing.cpp" line="83"/>
-        <source>Printing data.</source>
-        <translation>در حال چاپ داده‌ها.</translation>
+      <location filename="../printing.cpp" line="83"/>
+      <source>Printing data.</source>
+      <translation>در حال چاپ داده‌ها.</translation>
     </message>
     <message>
-        <location filename="../printing.cpp" line="109"/>
-        <source>Printing query.</source>
-        <translation>در حال چاپ پرس‌وجو.</translation>
+      <location filename="../printing.cpp" line="109"/>
+      <source>Printing query.</source>
+      <translation>در حال چاپ پرس‌وجو.</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>PrintingExport</name>
     <message>
-        <location filename="../printingexport.cpp" line="27"/>
-        <source>Printing</source>
-        <translation>چاپ</translation>
+      <location filename="../printingexport.cpp" line="27"/>
+      <source>Printing</source>
+      <translation>چاپ</translation>
     </message>
-</context>
+  </context>
 </TS>
