95ca6c754f152b4c359800519a41970b33e2ccdd
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 20:47:25 +0200

    New translations jsonexport.ts (Dutch)
    [ci skip]

--- Plugins/JsonExport/translations/JsonExport_nl_NL.ts
+++ Plugins/JsonExport/translations/JsonExport_nl_NL.ts
@@ -1,22 +1,22 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="nl" sourcelanguage="en">
-<context>
+  <context>
     <name>JsonExportConfig</name>
     <message>
-        <location filename="../jsonexport.ui" line="20"/>
-        <source>Output format</source>
-        <translation>Uitvoerformaat</translation>
+      <location filename="../jsonexport.ui" line="20"/>
+      <source>Output format</source>
+      <translation>Uitvoerformaat</translation>
     </message>
     <message>
-        <location filename="../jsonexport.ui" line="26"/>
-        <source>Format document (new lines, indentation)</source>
-        <translation>Document opmaken (nieuwe regels, inspringing)</translation>
+      <location filename="../jsonexport.ui" line="26"/>
+      <source>Format document (new lines, indentation)</source>
+      <translation>Document opmaken (nieuwe regels, inspringing)</translation>
     </message>
     <message>
-        <location filename="../jsonexport.ui" line="42"/>
-        <source>Compress (everything in one line)</source>
-        <translation>Comprimeren (alles op één regel)</translation>
+      <location filename="../jsonexport.ui" line="42"/>
+      <source>Compress (everything in one line)</source>
+      <translation>Comprimeren (alles op één regel)</translation>
     </message>
-</context>
+  </context>
 </TS>
