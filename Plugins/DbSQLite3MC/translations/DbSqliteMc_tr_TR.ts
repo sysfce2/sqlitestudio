9fdfd8e8f1addeb67f551d92ce0c8084915209c7
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 13:21:55 +0200

    New translations dbsqlitemc.ts (Turkish)
    [ci skip]

--- Plugins/DbSQLite3MC/translations/DbSqliteMc_tr_TR.ts
+++ Plugins/DbSQLite3MC/translations/DbSqliteMc_tr_TR.ts
@@ -1,46 +1,46 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="tr" sourcelanguage="en">
-<context>
+  <context>
     <name>DbSqliteMc</name>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="42"/>
-        <source>Password (key)</source>
-        <translation>Parola (anahtar)</translation>
+      <location filename="../dbsqlitemc.cpp" line="42"/>
+      <source>Password (key)</source>
+      <translation>Parola (anahtar)</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="43"/>
-        <source>Leave empty to create or connect to decrypted database.</source>
-        <translation>Şifresi çözülmüş bir veritabanı oluşturmak veya buna bağlanmak için boş bırakın.</translation>
+      <location filename="../dbsqlitemc.cpp" line="43"/>
+      <source>Leave empty to create or connect to decrypted database.</source>
+      <translation>Şifresi çözülmüş bir veritabanı oluşturmak veya buna bağlanmak için boş bırakın.</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="44"/>
-        <source>Encryption password</source>
-        <translation>Şifreleme parolası</translation>
+      <location filename="../dbsqlitemc.cpp" line="44"/>
+      <source>Encryption password</source>
+      <translation>Şifreleme parolası</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="50"/>
-        <source>Cipher</source>
-        <translation>Şifreleme algoritması</translation>
+      <location filename="../dbsqlitemc.cpp" line="50"/>
+      <source>Cipher</source>
+      <translation>Şifreleme algoritması</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="51"/>
-        <source>Cipher determines encryption algorithm used to encrypt the database.</source>
-        <translation>Şifreleme algoritması, veritabanını şifrelemek için kullanılan şifreleme algoritmasını belirler.</translation>
+      <location filename="../dbsqlitemc.cpp" line="51"/>
+      <source>Cipher determines encryption algorithm used to encrypt the database.</source>
+      <translation>Şifreleme algoritması, veritabanını şifrelemek için kullanılan şifreleme algoritmasını belirler.</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="61"/>
-        <source>Cipher configuration (optional)</source>
-        <translation>Şifreleme algoritması yapılandırması (isteğe bağlı)</translation>
+      <location filename="../dbsqlitemc.cpp" line="61"/>
+      <source>Cipher configuration (optional)</source>
+      <translation>Şifreleme algoritması yapılandırması (isteğe bağlı)</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="62"/>
-        <source>PRAGMA statements to customize SQLite3 Multiple Ciphers configuration, such as KDF iterations, legacy mode, etc.
+      <location filename="../dbsqlitemc.cpp" line="62"/>
+      <source>PRAGMA statements to customize SQLite3 Multiple Ciphers configuration, such as KDF iterations, legacy mode, etc.
 They will be executed upon each opening of the database.
 See documentation for SQLite3 Multiple Ciphers for details.</source>
-        <translation>KDF yinelemeleri, eski mod vb. gibi SQLite3 Multiple Ciphers yapılandırmasını özelleştirmek için PRAGMA deyimleri.
+      <translation>KDF yinelemeleri, eski mod vb. gibi SQLite3 Multiple Ciphers yapılandırmasını özelleştirmek için PRAGMA deyimleri.
 Veritabanının her açılışında yürütülürler.
 Ayrıntılar için SQLite3 Multiple Ciphers belgelerine bakın.</translation>
     </message>
-</context>
+  </context>
 </TS>
