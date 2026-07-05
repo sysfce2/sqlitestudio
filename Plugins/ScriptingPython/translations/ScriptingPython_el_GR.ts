5ba04d3c7e43442fe8e67b10a9c55e70032d3db5
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 20:52:38 +0200

    New translations scriptingpython.ts (Greek)
    [ci skip]

--- Plugins/ScriptingPython/translations/ScriptingPython_el_GR.ts
+++ Plugins/ScriptingPython/translations/ScriptingPython_el_GR.ts
@@ -1,50 +1,50 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="el" sourcelanguage="en">
-<context>
+  <context>
     <name>QObject</name>
     <message>
-        <location filename="../scriptingpython.cpp" line="730"/>
-        <source>Invalid use of %1 function. Expected %2 arguments, but got %3.</source>
-        <translation>Μη έγκυρη χρήση της συνάρτησης %1. Αναμένονταν %2 ορίσματα, αλλά λήφθηκαν %3.</translation>
+      <location filename="../scriptingpython.cpp" line="730"/>
+      <source>Invalid use of %1 function. Expected %2 arguments, but got %3.</source>
+      <translation>Μη έγκυρη χρήση της συνάρτησης %1. Αναμένονταν %2 ορίσματα, αλλά λήφθηκαν %3.</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="739"/>
-        <source>Unknown error from function %1.</source>
-        <translation>Άγνωστο σφάλμα από τη συνάρτηση %1.</translation>
+      <location filename="../scriptingpython.cpp" line="739"/>
+      <source>Unknown error from function %1.</source>
+      <translation>Άγνωστο σφάλμα από τη συνάρτηση %1.</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="780"/>
-        <source>Could not calculate string representation of the Python object passed as argument to the function %1.</source>
-        <translation>Δεν ήταν δυνατός ο υπολογισμός της αναπαράστασης συμβολοσειράς του αντικειμένου Python που μεταβιβάστηκε ως όρισμα στη συνάρτηση %1.</translation>
+      <location filename="../scriptingpython.cpp" line="780"/>
+      <source>Could not calculate string representation of the Python object passed as argument to the function %1.</source>
+      <translation>Δεν ήταν δυνατός ο υπολογισμός της αναπαράστασης συμβολοσειράς του αντικειμένου Python που μεταβιβάστηκε ως όρισμα στη συνάρτηση %1.</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="802"/>
-        <source>Could not find execution context for function %1. This is a bug of Python plugin. Please report it.</source>
-        <translation>Δεν ήταν δυνατή η εύρεση πλαισίου εκτέλεσης για τη συνάρτηση %1. Αυτό είναι σφάλμα του πρόσθετου Python. Παρακαλούμε αναφέρετέ το.</translation>
+      <location filename="../scriptingpython.cpp" line="802"/>
+      <source>Could not find execution context for function %1. This is a bug of Python plugin. Please report it.</source>
+      <translation>Δεν ήταν δυνατή η εύρεση πλαισίου εκτέλεσης για τη συνάρτηση %1. Αυτό είναι σφάλμα του πρόσθετου Python. Παρακαλούμε αναφέρετέ το.</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="827"/>
-        <source>Error from Python function %1: %2</source>
-        <translation>Σφάλμα από τη συνάρτηση Python %1: %2</translation>
+      <location filename="../scriptingpython.cpp" line="827"/>
+      <source>Error from Python function %1: %2</source>
+      <translation>Σφάλμα από τη συνάρτηση Python %1: %2</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>ScriptingPython</name>
     <message>
-        <location filename="../scriptingpython.ui" line="20"/>
-        <source>Python library path</source>
-        <translation>Διαδρομή βιβλιοθήκης Python</translation>
+      <location filename="../scriptingpython.ui" line="20"/>
+      <source>Python library path</source>
+      <translation>Διαδρομή βιβλιοθήκης Python</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.ui" line="29"/>
-        <source>Choose a Python library file</source>
-        <translation>Επιλέξτε αρχείο βιβλιοθήκης Python</translation>
+      <location filename="../scriptingpython.ui" line="29"/>
+      <source>Choose a Python library file</source>
+      <translation>Επιλέξτε αρχείο βιβλιοθήκης Python</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="370"/>
-        <source>The plugin is not configured properly.</source>
-        <translation>Το πρόσθετο δεν έχει ρυθμιστεί σωστά.</translation>
+      <location filename="../scriptingpython.cpp" line="370"/>
+      <source>The plugin is not configured properly.</source>
+      <translation>Το πρόσθετο δεν έχει ρυθμιστεί σωστά.</translation>
     </message>
-</context>
+  </context>
 </TS>
