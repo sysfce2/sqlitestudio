21272036805a2235ee0b88af97fcdfc0938c8815
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 20:44:54 +0200

    New translations dbsqlitemc.ts (Persian)
    [ci skip]

--- Plugins/DbSQLite3MC/translations/DbSqliteMc_fa_IR.ts
+++ Plugins/DbSQLite3MC/translations/DbSqliteMc_fa_IR.ts
@@ -1,46 +1,46 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="fa" sourcelanguage="en">
-<context>
+  <context>
     <name>DbSqliteMc</name>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="42"/>
-        <source>Password (key)</source>
-        <translation>گذرواژه (کلید)</translation>
+      <location filename="../dbsqlitemc.cpp" line="42"/>
+      <source>Password (key)</source>
+      <translation>گذرواژه (کلید)</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="43"/>
-        <source>Leave empty to create or connect to decrypted database.</source>
-        <translation>برای ایجاد یا اتصال به پایگاه‌داده رمزگشایی‌شده، خالی بگذارید.</translation>
+      <location filename="../dbsqlitemc.cpp" line="43"/>
+      <source>Leave empty to create or connect to decrypted database.</source>
+      <translation>برای ایجاد یا اتصال به پایگاه‌داده رمزگشایی‌شده، خالی بگذارید.</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="44"/>
-        <source>Encryption password</source>
-        <translation>گذرواژه رمزنگاری</translation>
+      <location filename="../dbsqlitemc.cpp" line="44"/>
+      <source>Encryption password</source>
+      <translation>گذرواژه رمزنگاری</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="50"/>
-        <source>Cipher</source>
-        <translation>رمز</translation>
+      <location filename="../dbsqlitemc.cpp" line="50"/>
+      <source>Cipher</source>
+      <translation>رمز</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="51"/>
-        <source>Cipher determines encryption algorithm used to encrypt the database.</source>
-        <translation>رمز، الگوریتم رمزنگاری مورد استفاده برای رمزنگاری پایگاه‌داده را تعیین می‌کند.</translation>
+      <location filename="../dbsqlitemc.cpp" line="51"/>
+      <source>Cipher determines encryption algorithm used to encrypt the database.</source>
+      <translation>رمز، الگوریتم رمزنگاری مورد استفاده برای رمزنگاری پایگاه‌داده را تعیین می‌کند.</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="61"/>
-        <source>Cipher configuration (optional)</source>
-        <translation>پیکربندی رمز (اختیاری)</translation>
+      <location filename="../dbsqlitemc.cpp" line="61"/>
+      <source>Cipher configuration (optional)</source>
+      <translation>پیکربندی رمز (اختیاری)</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="62"/>
-        <source>PRAGMA statements to customize SQLite3 Multiple Ciphers configuration, such as KDF iterations, legacy mode, etc.
+      <location filename="../dbsqlitemc.cpp" line="62"/>
+      <source>PRAGMA statements to customize SQLite3 Multiple Ciphers configuration, such as KDF iterations, legacy mode, etc.
 They will be executed upon each opening of the database.
 See documentation for SQLite3 Multiple Ciphers for details.</source>
-        <translation>دستورهای PRAGMA برای سفارشی‌سازی پیکربندی SQLite3 Multiple Ciphers، مانند تکرارهای KDF، حالت میراثی و غیره.
+      <translation>دستورهای PRAGMA برای سفارشی‌سازی پیکربندی SQLite3 Multiple Ciphers، مانند تکرارهای KDF، حالت میراثی و غیره.
 آن‌ها هنگام هر بار باز شدن پایگاه‌داده اجرا خواهند شد.
 برای جزئیات، مستندات SQLite3 Multiple Ciphers را ببینید.</translation>
     </message>
-</context>
+  </context>
 </TS>
