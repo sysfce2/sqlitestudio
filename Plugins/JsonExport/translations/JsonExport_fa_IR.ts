e39ef6a073d3bb930d317cd0864bb3dd7a169bd1
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 23:04:08 +0200

    New translations jsonexport.ts (Persian)
    [ci skip]

--- Plugins/JsonExport/translations/JsonExport_fa_IR.ts
+++ Plugins/JsonExport/translations/JsonExport_fa_IR.ts
@@ -1,22 +1,22 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="fa" sourcelanguage="en">
-<context>
+  <context>
     <name>JsonExportConfig</name>
     <message>
-        <location filename="../jsonexport.ui" line="20"/>
-        <source>Output format</source>
-        <translation>قالب خروجی</translation>
+      <location filename="../jsonexport.ui" line="20"/>
+      <source>Output format</source>
+      <translation>قالب خروجی</translation>
     </message>
     <message>
-        <location filename="../jsonexport.ui" line="26"/>
-        <source>Format document (new lines, indentation)</source>
-        <translation>قالب‌بندی سند (خطوط جدید، تورفتگی)</translation>
+      <location filename="../jsonexport.ui" line="26"/>
+      <source>Format document (new lines, indentation)</source>
+      <translation>قالب‌بندی سند (خطوط جدید، تورفتگی)</translation>
     </message>
     <message>
-        <location filename="../jsonexport.ui" line="42"/>
-        <source>Compress (everything in one line)</source>
-        <translation>فشرده‌سازی (همه‌چیز در یک خط)</translation>
+      <location filename="../jsonexport.ui" line="42"/>
+      <source>Compress (everything in one line)</source>
+      <translation>فشرده‌سازی (همه‌چیز در یک خط)</translation>
     </message>
-</context>
+  </context>
 </TS>
