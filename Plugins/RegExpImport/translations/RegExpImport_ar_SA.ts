49d89f519f2f1a938037702d9e3357f25be5be48
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 22:53:02 +0200

    New translations regexpimport.ts (Arabic)
    [ci skip]

--- Plugins/RegExpImport/translations/RegExpImport_ar_SA.ts
+++ Plugins/RegExpImport/translations/RegExpImport_ar_SA.ts
@@ -1,86 +1,86 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="ar" sourcelanguage="en">
-<context>
+  <context>
     <name>RegExpImport</name>
     <message>
-        <location filename="../regexpimport.cpp" line="37"/>
-        <source>Text files (*.txt);;All files (*)</source>
-        <translation>ملفات نصية (*.txt);;كل الملفات (*)</translation>
+      <location filename="../regexpimport.cpp" line="37"/>
+      <source>Text files (*.txt);;All files (*)</source>
+      <translation>ملفات نصية (*.txt);;كل الملفات (*)</translation>
     </message>
     <message>
-        <location filename="../regexpimport.cpp" line="53"/>
-        <source>Cannot read file %1</source>
-        <translation>لا يمكن قراءة الملف %1</translation>
+      <location filename="../regexpimport.cpp" line="53"/>
+      <source>Cannot read file %1</source>
+      <translation>لا يمكن قراءة الملف %1</translation>
     </message>
     <message>
-        <location filename="../regexpimport.cpp" line="161"/>
-        <source>Enter the regular expression pattern.</source>
-        <translation>أدخل نمط التعبير النمطي.</translation>
+      <location filename="../regexpimport.cpp" line="161"/>
+      <source>Enter the regular expression pattern.</source>
+      <translation>أدخل نمط التعبير النمطي.</translation>
     </message>
     <message>
-        <location filename="../regexpimport.cpp" line="169"/>
-        <source>Invalid pattern: %1</source>
-        <translation>نمط غير صالح: %1</translation>
+      <location filename="../regexpimport.cpp" line="169"/>
+      <source>Invalid pattern: %1</source>
+      <translation>نمط غير صالح: %1</translation>
     </message>
     <message>
-        <location filename="../regexpimport.cpp" line="189"/>
-        <source>Requested capture index %1 is out of range.</source>
-        <translation>فهرس الالتقاط المطلوب %1 خارج النطاق.</translation>
+      <location filename="../regexpimport.cpp" line="189"/>
+      <source>Requested capture index %1 is out of range.</source>
+      <translation>فهرس الالتقاط المطلوب %1 خارج النطاق.</translation>
     </message>
     <message>
-        <location filename="../regexpimport.cpp" line="196"/>
-        <source>&lt;p&gt;Requested capture group name &apos;%1&apos;, but it&apos;s not defined in the pattern: &lt;pre&gt;%2&lt;/pre&gt;&lt;/p&gt;</source>
-        <translation>&lt;p&gt;تم طلب اسم مجموعة الالتقاط &apos;%1&apos;، لكنّه غير معرّف (it&apos;s not defined) في النمط: &lt;pre&gt;%2&lt;/pre&gt;&lt;/p&gt;</translation>
+      <location filename="../regexpimport.cpp" line="196"/>
+      <source>&lt;p&gt;Requested capture group name &apos;%1&apos;, but it&apos;s not defined in the pattern: &lt;pre&gt;%2&lt;/pre&gt;&lt;/p&gt;</source>
+      <translation>&lt;p&gt;تم طلب اسم مجموعة الالتقاط &apos;%1&apos;، لكنّه غير معرّف (it&apos;s not defined) في النمط: &lt;pre&gt;%2&lt;/pre&gt;&lt;/p&gt;</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>RegExpImportConfig</name>
     <message>
-        <location filename="../regexpimport.ui" line="20"/>
-        <source>Capture groups</source>
-        <translation>مجموعات الالتقاط</translation>
+      <location filename="../regexpimport.ui" line="20"/>
+      <source>Capture groups</source>
+      <translation>مجموعات الالتقاط</translation>
     </message>
     <message>
-        <location filename="../regexpimport.ui" line="26"/>
-        <source>Treat all RegExp capture groups as columns</source>
-        <translation>التعامل مع كل مجموعات التقاط RegExp كأعمدة</translation>
+      <location filename="../regexpimport.ui" line="26"/>
+      <source>Treat all RegExp capture groups as columns</source>
+      <translation>التعامل مع كل مجموعات التقاط RegExp كأعمدة</translation>
     </message>
     <message>
-        <location filename="../regexpimport.ui" line="39"/>
-        <source>Import only following groups:</source>
-        <translation>استورد المجموعات التالية فقط:</translation>
+      <location filename="../regexpimport.ui" line="39"/>
+      <source>Import only following groups:</source>
+      <translation>استورد المجموعات التالية فقط:</translation>
     </message>
     <message>
-        <location filename="../regexpimport.ui" line="52"/>
-        <source>&lt;p&gt;Enter comma separated list of capture group indexes. The 0 index refers to the entire matched string.&lt;/p&gt;
+      <location filename="../regexpimport.ui" line="52"/>
+      <source>&lt;p&gt;Enter comma separated list of capture group indexes. The 0 index refers to the entire matched string.&lt;/p&gt;
 &lt;p&gt;If you used named groups in the pattern, you can use names instead of indexes. You can mix indexes and names in this list.&lt;/p&gt;</source>
-        <translation>&lt;p&gt;أدخل قائمة مفصولة بفواصل من فهارس مجموعات الالتقاط. يشير الفهرس 0 إلى السلسلة المطابقة بالكامل.&lt;/p&gt;
+      <translation>&lt;p&gt;أدخل قائمة مفصولة بفواصل من فهارس مجموعات الالتقاط. يشير الفهرس 0 إلى السلسلة المطابقة بالكامل.&lt;/p&gt;
 &lt;p&gt;إذا استخدمت مجموعات مسماة في النمط، فيمكنك استخدام الأسماء بدلًا من الفهارس. يمكنك مزج الفهارس والأسماء في هذه القائمة.&lt;/p&gt;</translation>
     </message>
     <message>
-        <location filename="../regexpimport.ui" line="56"/>
-        <source>Example: 1, 3, 4</source>
-        <translation>مثال: 1، 3، 4</translation>
+      <location filename="../regexpimport.ui" line="56"/>
+      <source>Example: 1, 3, 4</source>
+      <translation>مثال: 1، 3، 4</translation>
     </message>
     <message>
-        <location filename="../regexpimport.ui" line="69"/>
-        <source>Pattern:</source>
-        <translation>النمط:</translation>
+      <location filename="../regexpimport.ui" line="69"/>
+      <source>Pattern:</source>
+      <translation>النمط:</translation>
     </message>
     <message>
-        <location filename="../regexpimport.ui" line="76"/>
-        <source>&lt;p&gt;Use Regular Expression groups to enclose parts of the expression that you want to import. If you want to use a group, that you don&apos;t want to import, then use &quot;import only following groups&quot; option below.
+      <location filename="../regexpimport.ui" line="76"/>
+      <source>&lt;p&gt;Use Regular Expression groups to enclose parts of the expression that you want to import. If you want to use a group, that you don&apos;t want to import, then use &quot;import only following groups&quot; option below.
 
 You can use named groups and refer to them in group list below. To name a group use: &lt;pre&gt;(?&amp;lt;myGroupName&amp;gt;\s+\d+\s+)&lt;/pre&gt;&lt;/p&gt;</source>
-        <translation>&lt;p&gt;استخدم مجموعات التعبير النمطي لإحاطة أجزاء التعبير التي تريد استيرادها. إذا أردت استخدام مجموعة لا تريد استيرادها (don&apos;t)، فاستخدم خيار &quot;استيراد المجموعات التالية فقط&quot; أدناه.
+      <translation>&lt;p&gt;استخدم مجموعات التعبير النمطي لإحاطة أجزاء التعبير التي تريد استيرادها. إذا أردت استخدام مجموعة لا تريد استيرادها (don&apos;t)، فاستخدم خيار &quot;استيراد المجموعات التالية فقط&quot; أدناه.
 
 يمكنك استخدام المجموعات المسماة والإشارة إليها في قائمة المجموعات أدناه. لتسمية مجموعة، استخدم: &lt;pre&gt;(?&amp;lt;myGroupName&amp;gt;\s+\d+\s+)&lt;/pre&gt;&lt;/p&gt;</translation>
     </message>
     <message>
-        <location filename="../regexpimport.ui" line="81"/>
-        <source>Example: (\d+)\s+((\d+)\w+)\s+(\w+)</source>
-        <translation>مثال: (\d+)\s+((\d+)\w+)\s+(\w+)</translation>
+      <location filename="../regexpimport.ui" line="81"/>
+      <source>Example: (\d+)\s+((\d+)\w+)\s+(\w+)</source>
+      <translation>مثال: (\d+)\s+((\d+)\w+)\s+(\w+)</translation>
     </message>
-</context>
+  </context>
 </TS>
