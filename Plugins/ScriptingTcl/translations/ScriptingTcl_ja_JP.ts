0cff1dd27690bd2ad9a6415763315bb4e18514c6
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 20:50:15 +0200

    New translations scriptingtcl.ts (Japanese)
    [ci skip]

--- Plugins/ScriptingTcl/translations/ScriptingTcl_ja_JP.ts
+++ Plugins/ScriptingTcl/translations/ScriptingTcl_ja_JP.ts
@@ -1,27 +1,27 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="ja" sourcelanguage="en">
-<context>
+  <context>
     <name>ScriptingTcl</name>
     <message>
-        <location filename="../scriptingtcl.cpp" line="428"/>
-        <source>No database available in current context, while called Tcl&apos;s &apos;%1&apos; command.</source>
-        <translation>現在のコンテキストで利用可能なデータベースがありません。Tcl&apos;s &apos;%1&apos; コマンドが呼び出されました。</translation>
+      <location filename="../scriptingtcl.cpp" line="428"/>
+      <source>No database available in current context, while called Tcl&apos;s &apos;%1&apos; command.</source>
+      <translation>現在のコンテキストで利用可能なデータベースがありません。Tcl&apos;s &apos;%1&apos; コマンドが呼び出されました。</translation>
     </message>
     <message>
-        <location filename="../scriptingtcl.cpp" line="450"/>
-        <source>Invalid &apos;%1&apos; command syntax. Should be: %2</source>
-        <translation>&apos;%1&apos; コマンドの構文が無効です。次の形式である必要があります: %2</translation>
+      <location filename="../scriptingtcl.cpp" line="450"/>
+      <source>Invalid &apos;%1&apos; command syntax. Should be: %2</source>
+      <translation>&apos;%1&apos; コマンドの構文が無効です。次の形式である必要があります: %2</translation>
     </message>
     <message>
-        <location filename="../scriptingtcl.cpp" line="462"/>
-        <source>Error from Tcl&apos;s &apos;%1&apos; command: %2</source>
-        <translation>Tcl&apos;s &apos;%1&apos; コマンドからのエラー: %2</translation>
+      <location filename="../scriptingtcl.cpp" line="462"/>
+      <source>Error from Tcl&apos;s &apos;%1&apos; command: %2</source>
+      <translation>Tcl&apos;s &apos;%1&apos; コマンドからのエラー: %2</translation>
     </message>
     <message>
-        <location filename="../scriptingtcl.cpp" line="593"/>
-        <source>Error from Tcl&apos;s&apos; &apos;%1&apos; command: %2</source>
-        <translation>Tcl&apos;s&apos; &apos;%1&apos; コマンドからのエラー: %2</translation>
+      <location filename="../scriptingtcl.cpp" line="593"/>
+      <source>Error from Tcl&apos;s&apos; &apos;%1&apos; command: %2</source>
+      <translation>Tcl&apos;s&apos; &apos;%1&apos; コマンドからのエラー: %2</translation>
     </message>
-</context>
+  </context>
 </TS>
