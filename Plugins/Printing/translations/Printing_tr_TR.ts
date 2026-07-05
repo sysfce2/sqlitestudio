91f653c5103770a9c1d1d70f101c1d80be34c166
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 23:01:07 +0200

    New translations printing.ts (Turkish)
    [ci skip]

--- Plugins/Printing/translations/Printing_tr_TR.ts
+++ Plugins/Printing/translations/Printing_tr_TR.ts
@@ -1,40 +1,40 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="tr" sourcelanguage="en">
-<context>
+  <context>
     <name>Printing</name>
     <message>
-        <location filename="../printing.cpp" line="36"/>
-        <source>Print data</source>
-        <translation>Veriyi yazdır</translation>
+      <location filename="../printing.cpp" line="36"/>
+      <source>Print data</source>
+      <translation>Veriyi yazdır</translation>
     </message>
     <message>
-        <location filename="../printing.cpp" line="38"/>
-        <source>Print query</source>
-        <translation>Sorguyu yazdır</translation>
+      <location filename="../printing.cpp" line="38"/>
+      <source>Print query</source>
+      <translation>Sorguyu yazdır</translation>
     </message>
     <message>
-        <location filename="../printing.cpp" line="75"/>
-        <source>No data to print.</source>
-        <translation>Yazdırılacak veri yok.</translation>
+      <location filename="../printing.cpp" line="75"/>
+      <source>No data to print.</source>
+      <translation>Yazdırılacak veri yok.</translation>
     </message>
     <message>
-        <location filename="../printing.cpp" line="83"/>
-        <source>Printing data.</source>
-        <translation>Veri yazdırılıyor.</translation>
+      <location filename="../printing.cpp" line="83"/>
+      <source>Printing data.</source>
+      <translation>Veri yazdırılıyor.</translation>
     </message>
     <message>
-        <location filename="../printing.cpp" line="109"/>
-        <source>Printing query.</source>
-        <translation>Sorgu yazdırılıyor.</translation>
+      <location filename="../printing.cpp" line="109"/>
+      <source>Printing query.</source>
+      <translation>Sorgu yazdırılıyor.</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>PrintingExport</name>
     <message>
-        <location filename="../printingexport.cpp" line="27"/>
-        <source>Printing</source>
-        <translation>Yazdırılıyor</translation>
+      <location filename="../printingexport.cpp" line="27"/>
+      <source>Printing</source>
+      <translation>Yazdırılıyor</translation>
     </message>
-</context>
+  </context>
 </TS>
