c86787569a807395b9ce89172d04ef1496d77a54
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 13:20:21 +0200

    New translations scriptingpython.ts (Danish)
    [ci skip]

--- Plugins/ScriptingPython/translations/ScriptingPython_da_DK.ts
+++ Plugins/ScriptingPython/translations/ScriptingPython_da_DK.ts
@@ -1,50 +1,50 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="da" sourcelanguage="en">
-<context>
+  <context>
     <name>QObject</name>
     <message>
-        <location filename="../scriptingpython.cpp" line="730"/>
-        <source>Invalid use of %1 function. Expected %2 arguments, but got %3.</source>
-        <translation>Ugyldig brug af funktionen %1. Forventede %2 argumenter, men fik %3.</translation>
+      <location filename="../scriptingpython.cpp" line="730"/>
+      <source>Invalid use of %1 function. Expected %2 arguments, but got %3.</source>
+      <translation>Ugyldig brug af funktionen %1. Forventede %2 argumenter, men fik %3.</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="739"/>
-        <source>Unknown error from function %1.</source>
-        <translation>Ukendt fejl fra funktionen %1.</translation>
+      <location filename="../scriptingpython.cpp" line="739"/>
+      <source>Unknown error from function %1.</source>
+      <translation>Ukendt fejl fra funktionen %1.</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="780"/>
-        <source>Could not calculate string representation of the Python object passed as argument to the function %1.</source>
-        <translation>Kunne ikke beregne strengrepræsentationen af Python-objektet, der blev videregivet som argument til funktionen %1.</translation>
+      <location filename="../scriptingpython.cpp" line="780"/>
+      <source>Could not calculate string representation of the Python object passed as argument to the function %1.</source>
+      <translation>Kunne ikke beregne strengrepræsentationen af Python-objektet, der blev videregivet som argument til funktionen %1.</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="802"/>
-        <source>Could not find execution context for function %1. This is a bug of Python plugin. Please report it.</source>
-        <translation>Kunne ikke finde udførelseskontekst for funktionen %1. Dette er en fejl i Python-pluginet. Rapportér det venligst.</translation>
+      <location filename="../scriptingpython.cpp" line="802"/>
+      <source>Could not find execution context for function %1. This is a bug of Python plugin. Please report it.</source>
+      <translation>Kunne ikke finde udførelseskontekst for funktionen %1. Dette er en fejl i Python-pluginet. Rapportér det venligst.</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="827"/>
-        <source>Error from Python function %1: %2</source>
-        <translation>Fejl fra Python-funktionen %1: %2</translation>
+      <location filename="../scriptingpython.cpp" line="827"/>
+      <source>Error from Python function %1: %2</source>
+      <translation>Fejl fra Python-funktionen %1: %2</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>ScriptingPython</name>
     <message>
-        <location filename="../scriptingpython.ui" line="20"/>
-        <source>Python library path</source>
-        <translation>Sti til Python-bibliotek</translation>
+      <location filename="../scriptingpython.ui" line="20"/>
+      <source>Python library path</source>
+      <translation>Sti til Python-bibliotek</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.ui" line="29"/>
-        <source>Choose a Python library file</source>
-        <translation>Vælg en Python-biblioteksfil</translation>
+      <location filename="../scriptingpython.ui" line="29"/>
+      <source>Choose a Python library file</source>
+      <translation>Vælg en Python-biblioteksfil</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="370"/>
-        <source>The plugin is not configured properly.</source>
-        <translation>Pluginet er ikke konfigureret korrekt.</translation>
+      <location filename="../scriptingpython.cpp" line="370"/>
+      <source>The plugin is not configured properly.</source>
+      <translation>Pluginet er ikke konfigureret korrekt.</translation>
     </message>
-</context>
+  </context>
 </TS>
