3a3f90ffc1ad454855a42a7a145da196a4a361e6
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 20:51:45 +0200

    New translations xmlexport.ts (Arabic)
    [ci skip]

--- Plugins/XmlExport/translations/XmlExport_ar_SA.ts
+++ Plugins/XmlExport/translations/XmlExport_ar_SA.ts
@@ -1,70 +1,70 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="ar" sourcelanguage="en">
-<context>
+  <context>
     <name>XmlExport</name>
     <message>
-        <location filename="../xmlexport.cpp" line="37"/>
-        <source>Enter the namespace to use (for example: http://my.namespace.org)</source>
-        <translation>أدخل مساحة الأسماء المراد استخدامها (على سبيل المثال: http://my.namespace.org)</translation>
+      <location filename="../xmlexport.cpp" line="37"/>
+      <source>Enter the namespace to use (for example: http://my.namespace.org)</source>
+      <translation>أدخل مساحة الأسماء المراد استخدامها (على سبيل المثال: http://my.namespace.org)</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>XmlExportConfig</name>
     <message>
-        <location filename="../XmlExport.ui" line="20"/>
-        <source>Output format</source>
-        <translation>تنسيق الإخراج</translation>
+      <location filename="../XmlExport.ui" line="20"/>
+      <source>Output format</source>
+      <translation>تنسيق الإخراج</translation>
     </message>
     <message>
-        <location filename="../XmlExport.ui" line="26"/>
-        <source>Format document (new lines, indentation)</source>
-        <translation>تنسيق المستند (أسطر جديدة، مسافات بادئة)</translation>
+      <location filename="../XmlExport.ui" line="26"/>
+      <source>Format document (new lines, indentation)</source>
+      <translation>تنسيق المستند (أسطر جديدة، مسافات بادئة)</translation>
     </message>
     <message>
-        <location filename="../XmlExport.ui" line="42"/>
-        <source>Compress (everything in one line)</source>
-        <translation>ضغط (كل شيء في سطر واحد)</translation>
+      <location filename="../XmlExport.ui" line="42"/>
+      <source>Compress (everything in one line)</source>
+      <translation>ضغط (كل شيء في سطر واحد)</translation>
     </message>
     <message>
-        <location filename="../XmlExport.ui" line="58"/>
-        <source>Special characters escaping</source>
-        <translation>إفلات الأحرف الخاصة</translation>
+      <location filename="../XmlExport.ui" line="58"/>
+      <source>Special characters escaping</source>
+      <translation>إفلات الأحرف الخاصة</translation>
     </message>
     <message>
-        <location filename="../XmlExport.ui" line="64"/>
-        <source>&lt;p&gt;Ampersands will be used for shorter values and CDATA will be used for larger values. This applies only to values that require character escaping. Other values will be exported as they are.&lt;/p&gt;</source>
-        <translation>&lt;p&gt;سيتم استخدام علامات العطف للقيم الأقصر، وسيتم استخدام CDATA للقيم الأكبر. ينطبق هذا فقط على القيم التي تتطلب إفلات الأحرف. سيتم تصدير القيم الأخرى كما هي.&lt;/p&gt;</translation>
+      <location filename="../XmlExport.ui" line="64"/>
+      <source>&lt;p&gt;Ampersands will be used for shorter values and CDATA will be used for larger values. This applies only to values that require character escaping. Other values will be exported as they are.&lt;/p&gt;</source>
+      <translation>&lt;p&gt;سيتم استخدام علامات العطف للقيم الأقصر، وسيتم استخدام CDATA للقيم الأكبر. ينطبق هذا فقط على القيم التي تتطلب إفلات الأحرف. سيتم تصدير القيم الأخرى كما هي.&lt;/p&gt;</translation>
     </message>
     <message>
-        <location filename="../XmlExport.ui" line="67"/>
-        <source>Use CDATA and ampersands</source>
-        <translation>استخدام CDATA وعلامات العطف</translation>
+      <location filename="../XmlExport.ui" line="67"/>
+      <source>Use CDATA and ampersands</source>
+      <translation>استخدام CDATA وعلامات العطف</translation>
     </message>
     <message>
-        <location filename="../XmlExport.ui" line="83"/>
-        <source>&lt;p&gt;Every value requiring character escepe will be enclosed in CDATA block.&lt;/p&gt;</source>
-        <translation>&lt;p&gt;سيتم تضمين كل قيمة تتطلب إفلات الأحرف داخل كتلة CDATA.&lt;/p&gt;</translation>
+      <location filename="../XmlExport.ui" line="83"/>
+      <source>&lt;p&gt;Every value requiring character escepe will be enclosed in CDATA block.&lt;/p&gt;</source>
+      <translation>&lt;p&gt;سيتم تضمين كل قيمة تتطلب إفلات الأحرف داخل كتلة CDATA.&lt;/p&gt;</translation>
     </message>
     <message>
-        <location filename="../XmlExport.ui" line="86"/>
-        <source>Always use CDATA</source>
-        <translation>استخدام CDATA دائمًا</translation>
+      <location filename="../XmlExport.ui" line="86"/>
+      <source>Always use CDATA</source>
+      <translation>استخدام CDATA دائمًا</translation>
     </message>
     <message>
-        <location filename="../XmlExport.ui" line="99"/>
-        <source>&lt;p&gt;Every character that require esceping will be replaced with its ampersand escape sequence. No CDATA blocks will be used.&lt;/p&gt;</source>
-        <translation>&lt;p&gt;سيتم استبدال كل حرف يتطلب الإفلات بتسلسل الإفلات بعلامة العطف المقابل له. لن تُستخدم كتل CDATA.&lt;/p&gt;</translation>
+      <location filename="../XmlExport.ui" line="99"/>
+      <source>&lt;p&gt;Every character that require esceping will be replaced with its ampersand escape sequence. No CDATA blocks will be used.&lt;/p&gt;</source>
+      <translation>&lt;p&gt;سيتم استبدال كل حرف يتطلب الإفلات بتسلسل الإفلات بعلامة العطف المقابل له. لن تُستخدم كتل CDATA.&lt;/p&gt;</translation>
     </message>
     <message>
-        <location filename="../XmlExport.ui" line="102"/>
-        <source>Always use ampersand</source>
-        <translation>استخدام علامة العطف دائمًا</translation>
+      <location filename="../XmlExport.ui" line="102"/>
+      <source>Always use ampersand</source>
+      <translation>استخدام علامة العطف دائمًا</translation>
     </message>
     <message>
-        <location filename="../XmlExport.ui" line="118"/>
-        <source>Define XML namespace</source>
-        <translation>تعريف مساحة أسماء XML</translation>
+      <location filename="../XmlExport.ui" line="118"/>
+      <source>Define XML namespace</source>
+      <translation>تعريف مساحة أسماء XML</translation>
     </message>
-</context>
+  </context>
 </TS>
