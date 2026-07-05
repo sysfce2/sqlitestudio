de28fd593e39dc220a8fb7297fc8a33370c0ad02
Paweł Salawa <pawelsalawa@gmail.com>
2026-07-05 22:53:09 +0200

    New translations scriptingpython.ts (Arabic)
    [ci skip]

--- Plugins/ScriptingPython/translations/ScriptingPython_ar_SA.ts
+++ Plugins/ScriptingPython/translations/ScriptingPython_ar_SA.ts
@@ -1,50 +1,50 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="ar" sourcelanguage="en">
-<context>
+  <context>
     <name>QObject</name>
     <message>
-        <location filename="../scriptingpython.cpp" line="730"/>
-        <source>Invalid use of %1 function. Expected %2 arguments, but got %3.</source>
-        <translation>استخدام غير صالح للدالة %1. كان المتوقع %2 من الوسائط، لكن تم الحصول على %3.</translation>
+      <location filename="../scriptingpython.cpp" line="730"/>
+      <source>Invalid use of %1 function. Expected %2 arguments, but got %3.</source>
+      <translation>استخدام غير صالح للدالة %1. كان المتوقع %2 من الوسائط، لكن تم الحصول على %3.</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="739"/>
-        <source>Unknown error from function %1.</source>
-        <translation>خطأ غير معروف من الدالة %1.</translation>
+      <location filename="../scriptingpython.cpp" line="739"/>
+      <source>Unknown error from function %1.</source>
+      <translation>خطأ غير معروف من الدالة %1.</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="780"/>
-        <source>Could not calculate string representation of the Python object passed as argument to the function %1.</source>
-        <translation>تعذّر حساب التمثيل النصي لكائن Python المُمرَّر كوسيطة إلى الدالة %1.</translation>
+      <location filename="../scriptingpython.cpp" line="780"/>
+      <source>Could not calculate string representation of the Python object passed as argument to the function %1.</source>
+      <translation>تعذّر حساب التمثيل النصي لكائن Python المُمرَّر كوسيطة إلى الدالة %1.</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="802"/>
-        <source>Could not find execution context for function %1. This is a bug of Python plugin. Please report it.</source>
-        <translation>تعذّر العثور على سياق التنفيذ للدالة %1. هذا خطأ في إضافة Python. يُرجى الإبلاغ عنه.</translation>
+      <location filename="../scriptingpython.cpp" line="802"/>
+      <source>Could not find execution context for function %1. This is a bug of Python plugin. Please report it.</source>
+      <translation>تعذّر العثور على سياق التنفيذ للدالة %1. هذا خطأ في إضافة Python. يُرجى الإبلاغ عنه.</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="827"/>
-        <source>Error from Python function %1: %2</source>
-        <translation>خطأ من دالة Python %1: %2</translation>
+      <location filename="../scriptingpython.cpp" line="827"/>
+      <source>Error from Python function %1: %2</source>
+      <translation>خطأ من دالة Python %1: %2</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>ScriptingPython</name>
     <message>
-        <location filename="../scriptingpython.ui" line="20"/>
-        <source>Python library path</source>
-        <translation>مسار مكتبة Python</translation>
+      <location filename="../scriptingpython.ui" line="20"/>
+      <source>Python library path</source>
+      <translation>مسار مكتبة Python</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.ui" line="29"/>
-        <source>Choose a Python library file</source>
-        <translation>اختر ملف مكتبة Python</translation>
+      <location filename="../scriptingpython.ui" line="29"/>
+      <source>Choose a Python library file</source>
+      <translation>اختر ملف مكتبة Python</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="370"/>
-        <source>The plugin is not configured properly.</source>
-        <translation>لم تتم تهيئة الإضافة بشكل صحيح.</translation>
+      <location filename="../scriptingpython.cpp" line="370"/>
+      <source>The plugin is not configured properly.</source>
+      <translation>لم تتم تهيئة الإضافة بشكل صحيح.</translation>
     </message>
-</context>
+  </context>
 </TS>
